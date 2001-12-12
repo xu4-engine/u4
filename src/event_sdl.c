a2bda1d713a7f8503e96e0c08728666a7d9191ce
Andrew Taylor <andrewtaylor@users.sourceforge.net>
2001-12-12 00:04:48 +0000

    initial revision

---
+++ src/event_sdl.c
@@ -0,0 +1,54 @@
+/*
+ * $Id$
+ */
+
+#include <stdlib.h>
+#include <SDL/SDL.h>
+
+#include "u4.h"
+#include "screen.h"
+#include "event.h"
+#include "context.h"
+
+void eventHandlerMain() {
+    eventHandlerPushKeyHandler(&keyHandlerNormal);
+
+    screenMessage("\020");
+    while (1) {
+        int processed = 0;
+        SDL_Event event;
+
+        SDL_WaitEvent(&event);
+
+        switch (event.type) {
+        case SDL_KEYDOWN: {
+            int key;
+
+            if (event.key.keysym.sym >= SDLK_a &&
+                event.key.keysym.sym <= SDLK_z)
+                key = event.key.keysym.sym - SDLK_a + 'a';
+            else if (event.key.keysym.sym == SDLK_UP)
+                key = U4_UP;
+            else if (event.key.keysym.sym == SDLK_DOWN)
+                key = U4_DOWN;
+            else if (event.key.keysym.sym == SDLK_LEFT)
+                key = U4_LEFT;
+            else if (event.key.keysym.sym == SDLK_RIGHT)
+                key = U4_RIGHT;
+            else
+                key = event.key.keysym.sym;
+
+            processed = (*eventHandlerGetKeyHandler())(key);
+
+            if (processed) {
+                screenUpdate(c);
+                screenForceRedraw();
+            }
+            break;
+        }
+        case SDL_QUIT:
+            exit(0);
+            break;
+        }
+    }
+}
