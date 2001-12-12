8bbf20e0c09a3350803c12774a59a4f2576852f9
Andrew Taylor <andrewtaylor@users.sourceforge.net>
2001-12-12 00:00:31 +0000

    removed state, use forward decl for Map struct

--- src/context.h
+++ src/context.h
@@ -6,12 +6,12 @@
 #define CONTEXT_H
 
 struct _SaveGame;
+struct _Map;
 
 typedef struct _Context {
     struct _SaveGame *saveGame;
     struct _Context *parent;
-    const Map *map;
-    State state;
+    const struct _Map *map;
     int line, col;
 } Context;
 
