d3c9637175c8e5715623b9259b1c3af154d1770f
Andrew Taylor <andrewtaylor@users.sourceforge.net>
2001-12-20 04:18:24 +0000

    added talker field for current talker

--- src/context.h
+++ src/context.h
@@ -7,11 +7,13 @@
 
 struct _SaveGame;
 struct _Map;
+struct _Person;
 
 typedef struct _Context {
     struct _SaveGame *saveGame;
     struct _Context *parent;
     const struct _Map *map;
+    const struct _Person *talker;
     int line, col;
 } Context;
 
