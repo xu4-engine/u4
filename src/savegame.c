bb41db52e90c4805df9dc0f95d8a2b2d41911cd8
Andrew Taylor <andrewtaylor@users.sourceforge.net>
2002-02-27 16:57:27 +0000

    moved io functions into own file

--- src/savegame.c
+++ src/savegame.c
@@ -6,50 +6,7 @@
 #include <string.h>
 
 #include "savegame.h"
-
-static int writeInt(unsigned int i, FILE *f) {
-    if (fputc(i & 0xff, f) == EOF ||
-        fputc((i >> 8) & 0xff, f) == EOF ||
-        fputc((i >> 16) & 0xff, f) == EOF ||
-        fputc((i >> 24) & 0xff, f) == EOF)
-        return 0;
-    return 1;
-}
-
-static int writeShort(unsigned short s, FILE *f) {
-    if (fputc(s & 0xff, f) == EOF ||
-        fputc((s >> 8) & 0xff, f) == EOF)
-        return 0;
-    return 1;
-}
-
-static int writeChar(unsigned char c, FILE *f) {
-    if (fputc(c, f) == EOF)
-        return 0;
-    return 1;
-}
-
-static int readInt(unsigned int *i, FILE *f) {
-    *i = fgetc(f);
-    *i |= (fgetc(f) << 8);
-    *i |= (fgetc(f) << 16);
-    *i |= (fgetc(f) << 24);
-    
-    return 1;
-}
-
-static int readShort(unsigned short *s, FILE *f) {
-    *s = fgetc(f);
-    *s |= (fgetc(f) << 8);
-
-    return 1;
-}
-
-static int readChar(unsigned char *c, FILE *f) {
-    *c = fgetc(f);
-
-    return 1;
-}
+#include "io.h"
 
 int saveGameWrite(const SaveGame *save, FILE *f) {
     int i;
