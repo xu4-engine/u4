0bc6d2210853501db949728f61955468878cc61a
Andrew Taylor <andrewtaylor@users.sourceforge.net>
2002-04-07 23:56:24 +0000

    load .con areas at startup

--- src/u4.c
+++ src/u4.c
@@ -30,11 +30,36 @@ extern Map paws_map;
 extern Map vesper_map;
 extern Map den_map;
 extern Map cove_map;
+
 extern Map empath_map;
 extern Map lycaeum_map;
 extern Map serpent_map;
 extern Map lcb_1_map;
 extern Map lcb_2_map;
+
+extern Map brick_map;
+extern Map bridge_map;
+extern Map brush_map;
+extern Map camp_map;
+extern Map dng0_map;
+extern Map dng1_map;
+extern Map dng2_map;
+extern Map dng3_map;
+extern Map dng4_map;
+extern Map dng5_map;
+extern Map dng6_map;
+extern Map dungeon_map;
+extern Map forest_map;
+extern Map grass_map;
+extern Map hill_map;
+extern Map inn_map;
+extern Map marsh_map;
+extern Map shipsea_map;
+extern Map shipship_map;
+extern Map shipshor_map;
+extern Map shore_map;
+extern Map shorship_map;
+extern Map shrine_map;
 
 Map *cities[] = {
     &britain_map, &yew_map,
@@ -46,6 +71,21 @@ Map *cities[] = {
     &empath_map, &lycaeum_map,
     &serpent_map, &lcb_1_map,
     &lcb_2_map
+};
+
+Map *areas[] = {
+    &brick_map, &bridge_map,
+    &brush_map, &camp_map,
+    &dng0_map, &dng1_map,
+    &dng2_map, &dng3_map,
+    &dng4_map, &dng5_map,
+    &dng6_map, &dungeon_map,
+    &forest_map, &grass_map,
+    &hill_map, &inn_map,
+    &marsh_map, &shipsea_map,
+    &shipship_map, &shipshor_map,
+    &shore_map, &shorship_map,
+    &shrine_map
 };
 
 Context *c;
@@ -68,7 +108,13 @@ int main(int argc, char *argv[]) {
         fclose(ult);
         fclose(tlk);
     }
-    
+
+    for (i = 0; i < sizeof(areas) / sizeof(areas[0]); i++) {
+        FILE *con;
+        con = fopen(areas[i]->ult_fname, "r");
+        mapReadCon(areas[i], con, 1);
+        fclose(con);
+    }
 
     c = (Context *) malloc(sizeof(Context));
     c->saveGame = (SaveGame *) malloc(sizeof(SaveGame));
