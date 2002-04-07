3d7f0ccad4c5aa8fb8ed147eb0d7054497103e1a
Andrew Taylor <andrewtaylor@users.sourceforge.net>
2002-04-07 23:56:57 +0000

    initial revision

---
+++ src/maps/areas.c
@@ -0,0 +1,353 @@
+/*
+ * $Id$
+ */
+
+#include <stddef.h>
+
+#include "../u4.h"
+#include "../map.h"
+
+Map brick_map = {
+    "brick", /* name */
+    CON_WIDTH, CON_HEIGHT, /* width, height */
+    5, 5, /* startx, starty */
+    BORDER_FIXED, /* border_behavior */
+    0, /* n_portals */
+    NULL, /* portals */
+    0, /* n_persons */
+    NULL, /* persons */
+    0, /* flags */
+    NULL, /* data */
+    "./ultima4/brick.con", /* ult_fname */
+    NULL /* tlk_fname */
+};
+
+Map bridge_map = {
+    "bridge", /* name */
+    CON_WIDTH, CON_HEIGHT, /* width, height */
+    5, 5, /* startx, starty */
+    BORDER_FIXED, /* border_behavior */
+    0, /* n_portals */
+    NULL, /* portals */
+    0, /* n_persons */
+    NULL, /* persons */
+    0, /* flags */
+    NULL, /* data */
+    "./ultima4/bridge.con", /* ult_fname */
+    NULL /* tlk_fname */
+};
+
+Map brush_map = {
+    "brush", /* name */
+    CON_WIDTH, CON_HEIGHT, /* width, height */
+    5, 5, /* startx, starty */
+    BORDER_FIXED, /* border_behavior */
+    0, /* n_portals */
+    NULL, /* portals */
+    0, /* n_persons */
+    NULL, /* persons */
+    0, /* flags */
+    NULL, /* data */
+    "./ultima4/brush.con", /* ult_fname */
+    NULL /* tlk_fname */
+};
+
+Map camp_map = {
+    "camp", /* name */
+    CON_WIDTH, CON_HEIGHT, /* width, height */
+    5, 5, /* startx, starty */
+    BORDER_FIXED, /* border_behavior */
+    0, /* n_portals */
+    NULL, /* portals */
+    0, /* n_persons */
+    NULL, /* persons */
+    0, /* flags */
+    NULL, /* data */
+    "./ultima4/camp.con", /* ult_fname */
+    NULL /* tlk_fname */
+};
+
+Map dng0_map = {
+    "dng0", /* name */
+    CON_WIDTH, CON_HEIGHT, /* width, height */
+    5, 5, /* startx, starty */
+    BORDER_FIXED, /* border_behavior */
+    0, /* n_portals */
+    NULL, /* portals */
+    0, /* n_persons */
+    NULL, /* persons */
+    0, /* flags */
+    NULL, /* data */
+    "./ultima4/dng0.con", /* ult_fname */
+    NULL /* tlk_fname */
+};
+
+Map dng1_map = {
+    "dng1", /* name */
+    CON_WIDTH, CON_HEIGHT, /* width, height */
+    5, 5, /* startx, starty */
+    BORDER_FIXED, /* border_behavior */
+    0, /* n_portals */
+    NULL, /* portals */
+    0, /* n_persons */
+    NULL, /* persons */
+    0, /* flags */
+    NULL, /* data */
+    "./ultima4/dng1.con", /* ult_fname */
+    NULL /* tlk_fname */
+};
+
+Map dng2_map = {
+    "dng2", /* name */
+    CON_WIDTH, CON_HEIGHT, /* width, height */
+    5, 5, /* startx, starty */
+    BORDER_FIXED, /* border_behavior */
+    0, /* n_portals */
+    NULL, /* portals */
+    0, /* n_persons */
+    NULL, /* persons */
+    0, /* flags */
+    NULL, /* data */
+    "./ultima4/dng2.con", /* ult_fname */
+    NULL /* tlk_fname */
+};
+
+Map dng3_map = {
+    "dng3", /* name */
+    CON_WIDTH, CON_HEIGHT, /* width, height */
+    5, 5, /* startx, starty */
+    BORDER_FIXED, /* border_behavior */
+    0, /* n_portals */
+    NULL, /* portals */
+    0, /* n_persons */
+    NULL, /* persons */
+    0, /* flags */
+    NULL, /* data */
+    "./ultima4/dng3.con", /* ult_fname */
+    NULL /* tlk_fname */
+};
+
+Map dng4_map = {
+    "dng4", /* name */
+    CON_WIDTH, CON_HEIGHT, /* width, height */
+    5, 5, /* startx, starty */
+    BORDER_FIXED, /* border_behavior */
+    0, /* n_portals */
+    NULL, /* portals */
+    0, /* n_persons */
+    NULL, /* persons */
+    0, /* flags */
+    NULL, /* data */
+    "./ultima4/dng4.con", /* ult_fname */
+    NULL /* tlk_fname */
+};
+
+Map dng5_map = {
+    "dng5", /* name */
+    CON_WIDTH, CON_HEIGHT, /* width, height */
+    5, 5, /* startx, starty */
+    BORDER_FIXED, /* border_behavior */
+    0, /* n_portals */
+    NULL, /* portals */
+    0, /* n_persons */
+    NULL, /* persons */
+    0, /* flags */
+    NULL, /* data */
+    "./ultima4/dng5.con", /* ult_fname */
+    NULL /* tlk_fname */
+};
+
+Map dng6_map = {
+    "dng6", /* name */
+    CON_WIDTH, CON_HEIGHT, /* width, height */
+    5, 5, /* startx, starty */
+    BORDER_FIXED, /* border_behavior */
+    0, /* n_portals */
+    NULL, /* portals */
+    0, /* n_persons */
+    NULL, /* persons */
+    0, /* flags */
+    NULL, /* data */
+    "./ultima4/dng6.con", /* ult_fname */
+    NULL /* tlk_fname */
+};
+
+Map dungeon_map = {
+    "dungeon", /* name */
+    CON_WIDTH, CON_HEIGHT, /* width, height */
+    5, 5, /* startx, starty */
+    BORDER_FIXED, /* border_behavior */
+    0, /* n_portals */
+    NULL, /* portals */
+    0, /* n_persons */
+    NULL, /* persons */
+    0, /* flags */
+    NULL, /* data */
+    "./ultima4/dungeon.con", /* ult_fname */
+    NULL /* tlk_fname */
+};
+
+Map forest_map = {
+    "forest", /* name */
+    CON_WIDTH, CON_HEIGHT, /* width, height */
+    5, 5, /* startx, starty */
+    BORDER_FIXED, /* border_behavior */
+    0, /* n_portals */
+    NULL, /* portals */
+    0, /* n_persons */
+    NULL, /* persons */
+    0, /* flags */
+    NULL, /* data */
+    "./ultima4/forest.con", /* ult_fname */
+    NULL /* tlk_fname */
+};
+
+Map grass_map = {
+    "grass", /* name */
+    CON_WIDTH, CON_HEIGHT, /* width, height */
+    5, 5, /* startx, starty */
+    BORDER_FIXED, /* border_behavior */
+    0, /* n_portals */
+    NULL, /* portals */
+    0, /* n_persons */
+    NULL, /* persons */
+    0, /* flags */
+    NULL, /* data */
+    "./ultima4/grass.con", /* ult_fname */
+    NULL /* tlk_fname */
+};
+
+Map hill_map = {
+    "hill", /* name */
+    CON_WIDTH, CON_HEIGHT, /* width, height */
+    5, 5, /* startx, starty */
+    BORDER_FIXED, /* border_behavior */
+    0, /* n_portals */
+    NULL, /* portals */
+    0, /* n_persons */
+    NULL, /* persons */
+    0, /* flags */
+    NULL, /* data */
+    "./ultima4/hill.con", /* ult_fname */
+    NULL /* tlk_fname */
+};
+
+Map inn_map = {
+    "inn", /* name */
+    CON_WIDTH, CON_HEIGHT, /* width, height */
+    5, 5, /* startx, starty */
+    BORDER_FIXED, /* border_behavior */
+    0, /* n_portals */
+    NULL, /* portals */
+    0, /* n_persons */
+    NULL, /* persons */
+    0, /* flags */
+    NULL, /* data */
+    "./ultima4/inn.con", /* ult_fname */
+    NULL /* tlk_fname */
+};
+
+Map marsh_map = {
+    "marsh", /* name */
+    CON_WIDTH, CON_HEIGHT, /* width, height */
+    5, 5, /* startx, starty */
+    BORDER_FIXED, /* border_behavior */
+    0, /* n_portals */
+    NULL, /* portals */
+    0, /* n_persons */
+    NULL, /* persons */
+    0, /* flags */
+    NULL, /* data */
+    "./ultima4/marsh.con", /* ult_fname */
+    NULL /* tlk_fname */
+};
+
+Map shipsea_map = {
+    "shipsea", /* name */
+    CON_WIDTH, CON_HEIGHT, /* width, height */
+    5, 5, /* startx, starty */
+    BORDER_FIXED, /* border_behavior */
+    0, /* n_portals */
+    NULL, /* portals */
+    0, /* n_persons */
+    NULL, /* persons */
+    0, /* flags */
+    NULL, /* data */
+    "./ultima4/shipsea.con", /* ult_fname */
+    NULL /* tlk_fname */
+};
+
+Map shipship_map = {
+    "shipship", /* name */
+    CON_WIDTH, CON_HEIGHT, /* width, height */
+    5, 5, /* startx, starty */
+    BORDER_FIXED, /* border_behavior */
+    0, /* n_portals */
+    NULL, /* portals */
+    0, /* n_persons */
+    NULL, /* persons */
+    0, /* flags */
+    NULL, /* data */
+    "./ultima4/shipship.con", /* ult_fname */
+    NULL /* tlk_fname */
+};
+
+Map shipshor_map = {
+    "shipshor", /* name */
+    CON_WIDTH, CON_HEIGHT, /* width, height */
+    5, 5, /* startx, starty */
+    BORDER_FIXED, /* border_behavior */
+    0, /* n_portals */
+    NULL, /* portals */
+    0, /* n_persons */
+    NULL, /* persons */
+    0, /* flags */
+    NULL, /* data */
+    "./ultima4/shipshor.con", /* ult_fname */
+    NULL /* tlk_fname */
+};
+
+Map shore_map = {
+    "shore", /* name */
+    CON_WIDTH, CON_HEIGHT, /* width, height */
+    5, 5, /* startx, starty */
+    BORDER_FIXED, /* border_behavior */
+    0, /* n_portals */
+    NULL, /* portals */
+    0, /* n_persons */
+    NULL, /* persons */
+    0, /* flags */
+    NULL, /* data */
+    "./ultima4/shore.con", /* ult_fname */
+    NULL /* tlk_fname */
+};
+
+Map shorship_map = {
+    "shorship", /* name */
+    CON_WIDTH, CON_HEIGHT, /* width, height */
+    5, 5, /* startx, starty */
+    BORDER_FIXED, /* border_behavior */
+    0, /* n_portals */
+    NULL, /* portals */
+    0, /* n_persons */
+    NULL, /* persons */
+    0, /* flags */
+    NULL, /* data */
+    "./ultima4/shorship.con", /* ult_fname */
+    NULL /* tlk_fname */
+};
+
+Map shrine_map = {
+    "shrine", /* name */
+    CON_WIDTH, CON_HEIGHT, /* width, height */
+    5, 5, /* startx, starty */
+    BORDER_FIXED, /* border_behavior */
+    0, /* n_portals */
+    NULL, /* portals */
+    0, /* n_persons */
+    NULL, /* persons */
+    0, /* flags */
+    NULL, /* data */
+    "./ultima4/shrine.con", /* ult_fname */
+    NULL /* tlk_fname */
+};
