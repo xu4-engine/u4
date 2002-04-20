6f88b13982ee8a4d6cb411a5cd56b817aec5fc59
Andrew Taylor <andrewtaylor@users.sourceforge.net>
2002-04-20 22:26:23 +0000

    added tileIsAnimated function

--- src/ttype.c
+++ src/ttype.c
@@ -4,6 +4,7 @@
 #define MASK_FLYABLE  0x10
 #define MASK_OPAQUE   0x20
 #define MASK_LAVA     0x40
+#define MASK_ANIMATED 0x80
 
 #define WALKABLE 0x00
 #define SLOW 0x01
@@ -13,7 +14,7 @@
 /* tile values 0-31 */
 static const char _ttype_info1[] = {
 /* sea,  water, shallows, swamp, grass, brush, forest, hills, mountain, */
-   0x0b, 0x0b,  0x0b,     0x00,  0x00,  0x01,  0x02,   0x02,  0x03,
+   0x8b, 0x8b,  0x8b,     0x00,  0x00,  0x01,  0x02,   0x02,  0x03,
 /* dungeon, city, castle, town, lcb1, lcb2, lcb3, wship, nship, eship, sship */
    0x00,    0x00, 0x00,   0x00, 0x03, 0x00, 0x03, 0x00,  0x00,  0x00,  0x00,
 /* whorse, ehorse, hex,  bridge, baloon, nbridge, sbridge, uladder, dladder */
@@ -27,9 +28,9 @@ static const char _ttype_info2[] = {
 /* corpse, stonewall, ldoor, door, chest, anhk, brick, wood */
    0x00,   0x03,      0x03,  0x03, 0x00,  0x03, 0x00,  0x03,
 /* mgate0, mgate1, mgate2, mgate3, field0, field1, field2, field3 */
-   0x00,   0x00,   0x00,   0x00,   0x00,   0x00,   0x00,   0x00,
+   0x00,   0x00,   0x00,   0x00,   0x80,   0x80,   0x80,   0x80,
 /* solid, sdoor, altar, roast, lava, stone, orb1, orb2 */
-   0x03,  0x00,  0x00,  0x03,  0x40, 0x00,  0x00, 0x00
+   0x03,  0x00,  0x00,  0x03,  0xc0, 0x00,  0x00, 0x00
 };
 
 
@@ -81,6 +82,16 @@ int islockeddoor(unsigned char tile) {
     return tile == 58;
 }
 
-int canTalkOver(unsigned char tile) {
+int tileCanTalkOver(unsigned char tile) {
     return tile >= 96 && tile <= 122;
+}
+
+int tileIsAnimated(unsigned char tile) {
+    if (tile < 32)
+	return (_ttype_info1[tile] & MASK_ANIMATED) != 0;
+    else if (tile < 56)
+	return 0;
+    else if (tile < 79)
+	return (_ttype_info2[tile - 56] & MASK_ANIMATED) != 0;
+    return 0;
 }
--- src/ttype.h
+++ src/ttype.h
@@ -10,6 +10,7 @@ int isslow(unsigned char tile);
 int isvslow(unsigned char tile);
 int isdoor(unsigned char tile);
 int islockeddoor(unsigned char tile);
-int canTalkOver(unsigned char tile);
+int tileCanTalkOver(unsigned char tile);
+int tileIsAnimated(unsigned char tile);
 
 #endif
