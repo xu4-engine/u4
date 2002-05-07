6af0c4284d4426e4ffc9540bdf45d9885337c006
Andrew Taylor <andrewtaylor@users.sourceforge.net>
2002-05-07 03:48:07 +0000

    continued implementation

--- src/intro.c
+++ src/intro.c
@@ -14,24 +14,42 @@
 
 typedef enum {
     INTRO_MAP,
-    INTRO_MENU
+    INTRO_MENU,
+    INTRO_INIT_NAME,
+    INTRO_INIT_SEX,
 } IntroMode;
 
 IntroMode mode = INTRO_MAP;
+char buffer[16];
+
+int introHandleName(const char *message);
 
 int introKeyHandler(int key, void *data) {
+    ReadBufferActionInfo *info;
     int valid = 1;
 
-    if (mode == INTRO_MAP) {
+    switch (mode) {
+
+    case INTRO_MAP:
         mode = INTRO_MENU;
         introUpdateScreen();
         return 1;
-    }
 
-    else if (mode == INTRO_MENU) {
+    case INTRO_MENU:
         switch (key) {
         case 'i':
-            printf("FIXME\n");
+            mode = INTRO_INIT_NAME;
+            introUpdateScreen();
+            screenSetCursorPos(12, 20);
+            screenEnableCursor();
+            screenForceRedraw();
+            info = (ReadBufferActionInfo *) malloc(sizeof(ReadBufferActionInfo));
+            info->handleBuffer = &introHandleName;
+            info->buffer = buffer;
+            info->bufferLen = 16;
+            info->screenX = 12;
+            info->screenY = 20;
+            eventHandlerPushKeyHandlerData(&keyHandlerReadBuffer, info);
             break;
         case 'j':
             eventHandlerSetExitFlag(1);
@@ -44,7 +62,19 @@ int introKeyHandler(int key, void *data) {
             valid = 0;
             break;
         }
+        break;
+
+    case INTRO_INIT_NAME:
+        mode = INTRO_MENU;
+        introUpdateScreen();
+        return 1;
+
+    case INTRO_INIT_SEX:
+        mode = INTRO_MENU;
+        introUpdateScreen();
+        return 1;
     }
+
 
     return valid || keyHandlerDefault(key, NULL);
 }
@@ -60,17 +90,19 @@ void introUpdateScreen() {
         { 4, 4, 4, 4, 1, 1, 1, 1, 4, 4, 8, 8, 1, 1, 1, 1, 1, 6, 6 }
     };
 
-    screenDrawBackground(BKGD_INTRO);
 
-    if (mode == INTRO_MAP) {
+    switch (mode) {
+    case INTRO_MAP:
+        screenDrawBackground(BKGD_INTRO);
         for (y = 0; y < (sizeof(map) / sizeof(map[0])); y++) {
             for (x = 0; x < (sizeof(map[0]) / sizeof(map[0][0])); x++) {
                 screenShowTile(map[y][x], x, y + 6);
             }
         }
-    }
+        break;
 
-    else if (mode == INTRO_MENU) {
+    case INTRO_MENU:
+        screenDrawBackground(BKGD_INTRO);
         screenTextAt(2, 14, "In another world, in a time to come.");
         screenTextAt(15, 16, "Options:");
         screenTextAt(11, 17, "Return to the view");
@@ -78,10 +110,29 @@ void introUpdateScreen() {
         screenTextAt(11, 19, "Initiate New Game");
         screenTextAt(3, 21, "xu4 is free software, see COPYING");
         screenTextAt(5, 22, "\011 Copyright 1987 Lord British");
-    }
+        break;
 
-    else
+    case INTRO_INIT_NAME:
+        screenDrawBackground(BKGD_INTRO);
+        screenTextAt(4, 16, "By what name shalt thou be known");
+        screenTextAt(4, 17, "in this world and time?");
+        break;
+
+    case INTRO_INIT_SEX:
+        screenDrawBackground(BKGD_INTRO);
+        screenTextAt(4, 16, "Art thou Male or Female?");
+        break;
+
+    default:
         assert(0);
+    }
 
     screenForceRedraw();
 }
+
+int introHandleName(const char *message) {
+    mode = INTRO_INIT_SEX;
+    printf("%s\n", message);
+    return 1;
+}
+
