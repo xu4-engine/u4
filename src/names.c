bd6aa4c3822ee427437e646e076976915f6e46b4
Andrew Taylor <andrewtaylor@users.sourceforge.net>
2002-05-17 01:26:31 +0000

    initial revision

---
+++ src/names.c
@@ -0,0 +1,90 @@
+/*
+ * $Id$
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "names.h"
+
+const char *getClassName(ClassType klass) {
+    switch (klass) {
+    case CLASS_MAGE:
+        return "Mage";
+    case CLASS_BARD:
+        return "Bard";
+    case CLASS_FIGHTER:
+        return "Fighter";
+    case CLASS_DRUID:
+        return "Druid";
+    case CLASS_TINKER:
+        return "Tinker";
+    case CLASS_PALADIN:
+        return "Paladin";
+    case CLASS_RANGER:
+        return "Ranger";
+    case CLASS_SHEPHERD:
+        return "Shepherd";
+    default:
+        return "???";
+    }
+}
+
+const char *getWeaponName(WeaponType weapon) {
+    static const char *weapNames[] = {
+        "Hands", "Staff", "Dagger",
+        "Sling", "Mace", "Axe",
+        "Sword", "Bow", "Crossbow",
+        "Flaming Oil", "Halberd", "Magic Axe",
+        "Magic Sword", "Magic Bow", "Magic Wand",
+        "Mystic Sword"
+    };
+
+    if (weapon < WEAP_MAX)
+        return weapNames[weapon - WEAP_HANDS];
+    else
+        return "???";
+}
+
+const char *getWeaponAbbrev(WeaponType weapon) {
+    static const char *weapAbbrevs[] = {
+        "HND", "STF", "DAG",
+        "SLN", "MAC", "AXE",
+        "SWD", "BOW", "XBO",
+        "OIL", "HAL", "+AX",
+        "+SW", "+BO", "WND",
+        "^SW"
+    };
+
+    if (weapon < WEAP_MAX)
+        return weapAbbrevs[weapon - WEAP_HANDS];
+    else
+        return "???";
+}
+
+const char *getArmorName(ArmorType armor) {
+    static const char *armorNames[] = {
+        "Skin", "Cloth", "Leather", 
+        "Chain Mail", "Plate Mail", 
+        "Magic Chain", "Magic Plate", "Mystic Robe"
+    };
+
+    if (armor < ARMR_MAX)
+        return armorNames[armor - ARMR_NONE];
+    else
+        return "???";
+}
+
+const char *getReagentName(Reagent reagent) {
+    static const char *reagentNames[] = {
+        "Sulfur Ash", "Ginseng", "Garlic", 
+        "Spider Silk", "Blood Moss", "Black Pearl", 
+        "Nightshade", "Mandrake"
+    };
+
+    if (reagent < REAG_MAX)
+        return reagentNames[reagent - REAG_ASH];
+    else
+        return "???";
+}
---
+++ src/names.h
@@ -0,0 +1,16 @@
+/*
+ * $Id$
+ */
+
+#ifndef NAMES_H
+#define NAMES_H
+
+#include "savegame.h"
+
+const char *getClassName(ClassType klass);
+const char *getWeaponName(WeaponType weapon);
+const char *getWeaponAbbrev(WeaponType weapon);
+const char *getArmorName(ArmorType armor);
+const char *getReagentName(Reagent reagent);
+
+#endif
