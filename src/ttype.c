4fb802f45f5c241a085b8aaaf50907919b454554
Andrew Taylor <andrewtaylor@users.sourceforge.net>
2002-08-06 02:19:05 +0000

    removed redundant defines: bug 591241

--- src/ttype.c
+++ src/ttype.c
@@ -13,8 +13,6 @@
 #define MASK_OPAQUE    0x0020
 #define MASK_SAILABLE  0x0040
 #define MASK_ANIMATED  0x0080
-#define MASK_ANIMATED  0x0080
-#define MASK_OPAQUE    0x0100
 #define MASK_UNFLYABLE 0x0200
 #define MASK_SHIP      0x0400
 #define MASK_HORSE     0x0800
