3fba1112bcc5c12a09f61469849a034e2cc2cb9f
Andrew Taylor <andrewtaylor@users.sourceforge.net>
2002-11-05 04:32:45 +0000

    initial revision

---
+++ src/monster.c
@@ -0,0 +1,70 @@
+/*
+ * $Id$
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "monster.h"
+
+#define UNKNOWN 0
+
+/*
+    Bard         9
+    Beggar      13
+    Bull        11
+    Child       10
+    Druid       10
+    Fighter      7
+    Guard       13
+    Horse        9
+    Jester       9
+    Mage         8
+    Merchant     9
+    Paladin      4
+    Ranger       3
+    Shepherd     9
+    Tinker       9
+    Villager    13
+*/
+
+
+static const Monster monsters[] = {
+    { PIRATE_TILE,      UNKNOWN,       "Pirate Ship",  UNKNOWN },
+    { NIXIE_TILE,       SEAHORSE_TILE, "Nixie",        5 },
+    { GIANT_SQUID_TILE, UNKNOWN,       "Giant Squid",  9 },
+    { SEA_SERPENT_TILE, UNKNOWN,       "Sea Serpent",  9 },
+    { SEAHORSE_TILE,    UNKNOWN,       "Seahorse",     7 },
+    { WHIRLPOOL_TILE,   UNKNOWN,       "Whirlpool",    UNKNOWN },
+    { STORM_TILE,       UNKNOWN,       "Storm",        UNKNOWN },
+    { RAT_TILE,         UNKNOWN,       "Rat",          4 },
+    { BAT_TILE,         UNKNOWN,       "Bat",          4 },
+    { GIANT_SPIDER_TILE, UNKNOWN,      "Giant Spider", 5 },
+    { GHOST_TILE,       UNKNOWN,       "Ghost",        6 },
+    { SLIME_TILE,       UNKNOWN,       "Slime",        4 },
+    { TROLL_TILE,       ETTIN_TILE,    "Troll",        7 },
+    { GREMLIN_TILE,     UNKNOWN,       "Gremlin",      4 },
+    { MIMIC_TILE,       UNKNOWN,       "Mimic",        13 },
+    { REAPER_TILE,      UNKNOWN,       "Reaper",       16 },
+    { INSECT_SWARM_TILE, UNKNOWN,      "Insect Swarm", 4 },
+    { GAZER_TILE,       UNKNOWN,       "Gazer",        16 },
+    { PHANTOM_TILE,     UNKNOWN,       "Phantom",      9 },
+    { ORC_TILE,         TROLL_TILE,    "Orc",          6 },
+    { SKELETON_TILE,    MAGE_TILE,     "Skeleton",     4 },
+    { ROGUE_TILE,       UNKNOWN,       "Rogue",        6 },
+    { PYTHON_TILE,      UNKNOWN,       "Python",       4 },
+    { ETTIN_TILE,       UNKNOWN,       "Ettin",        8 },
+    { HEADLESS_TILE,    GAZER_TILE,    "Headless",     5 },
+    { CYCLOPS_TILE,     UNKNOWN,       "Cyclops",      9 },
+    { WISP_TILE,        UNKNOWN,       "Wisp",         5 },
+    { MAGE_TILE,        UNKNOWN,       "Mage",         12 },
+    { LICH_TILE,        UNKNOWN,       "Lich",         13 },
+    { LAVA_LIZARD_TILE, UNKNOWN,       "Lava Lizard",  7 },
+    { ZORN_TILE,        UNKNOWN,       "Zorn",         16 },
+    { DAEMON_TILE,      UNKNOWN,       "Daemon",       8 },
+    { HYDRA_TILE,       UNKNOWN,       "Hydra",        14 },
+    { DRAGON_TILE,      UNKNOWN,       "Dragon",       15 },
+    { BALRON_TILE,      UNKNOWN,       "Balron",       16 }
+};
+
---
+++ src/monster.h
@@ -0,0 +1,51 @@
+/*
+ * $Id$
+ */
+
+#ifndef MONSTER_H
+#define MONSTER_H
+
+#define PIRATE_TILE 128
+#define NIXIE_TILE 132
+#define GIANT_SQUID_TILE 134
+#define SEA_SERPENT_TILE 136
+#define SEAHORSE_TILE 138
+#define WHIRLPOOL_TILE 140
+#define STORM_TILE 142
+#define RAT_TILE 144
+#define BAT_TILE 148
+#define GIANT_SPIDER_TILE 152
+#define GHOST_TILE 156
+#define SLIME_TILE 160
+#define TROLL_TILE 164
+#define GREMLIN_TILE 168
+#define MIMIC_TILE 172
+#define REAPER_TILE 176
+#define INSECT_SWARM_TILE 180
+#define GAZER_TILE 184
+#define PHANTOM_TILE 188
+#define ORC_TILE 192
+#define SKELETON_TILE 196
+#define ROGUE_TILE 200
+#define PYTHON_TILE 204
+#define ETTIN_TILE 208
+#define HEADLESS_TILE 212
+#define CYCLOPS_TILE 216
+#define WISP_TILE 220
+#define MAGE_TILE 224
+#define LICH_TILE 228
+#define LAVA_LIZARD_TILE 232
+#define ZORN_TILE 236
+#define DAEMON_TILE 240
+#define HYDRA_TILE 244
+#define DRAGON_TILE 248
+#define BALRON_TILE 252
+
+typedef struct _Monster {
+    unsigned char tile;
+    unsigned char leader;
+    const char *name;
+    unsigned short xp;
+} Monster;
+
+#endif
