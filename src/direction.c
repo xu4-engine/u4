439b7caab4fa37873af815b97b720ccf4b9c5372
Andrew Taylor <andrewtaylor@users.sourceforge.net>
2002-12-03 19:52:24 +0000

    add some basic path finding: needs work

--- src/direction.c
+++ src/direction.c
@@ -2,7 +2,7 @@
  * $Id$
  */
 
-#include <stddef.h>
+#include <stdlib.h>
 #include <assert.h>
 
 #include "direction.h"
@@ -12,6 +12,8 @@
  */
 void dirMove(Direction dir, int *x, int *y) {
     switch (dir) {
+    case DIR_NONE:
+        break;
     case DIR_WEST:
         (*x)--;
         break;
@@ -34,6 +36,8 @@ void dirMove(Direction dir, int *x, int *y) {
  */
 Direction dirReverse(Direction dir) {
     switch (dir) {
+    case DIR_NONE:
+        return DIR_NONE;
     case DIR_WEST:
         return DIR_EAST;
     case DIR_NORTH:
@@ -46,5 +50,34 @@ Direction dirReverse(Direction dir) {
         assert(0);              /* shouldn't happen */
     }
 
-    return (Direction) 0;
+    return DIR_NONE;
+}
+
+/**
+ * Finds the appropriate direction to travel to get from one point to
+ * another.  Quite possibly the worst path finding algorithm ever
+ * conceived; causes things to get stuck behind obstacles easily.
+ */
+Direction dirFindPath(int from_x, int from_y, int to_x, int to_y, int valid_directions_mask) {
+    Direction vert_dir, horiz_dir;
+    int dx, dy;
+
+    dx = from_x - to_x;
+    dy = from_y - to_y;
+
+    vert_dir = (dy < 0) ? DIR_SOUTH : (dy > 0) ? DIR_NORTH : DIR_NONE;
+    horiz_dir = (dx < 0) ? DIR_EAST : (dx > 0) ? DIR_WEST : DIR_NONE;
+
+    /* restrict direction to the valid directions specified */
+    if (!DIR_IN_MASK(vert_dir, valid_directions_mask))
+        vert_dir = DIR_NONE;
+    if (!DIR_IN_MASK(horiz_dir, valid_directions_mask))
+        horiz_dir = DIR_NONE;
+
+    if (vert_dir == DIR_NONE)
+        return horiz_dir;
+    else if (horiz_dir == DIR_NONE)
+        return vert_dir;
+    else
+        return (rand() % 2) ? vert_dir : horiz_dir;
 }
--- src/direction.h
+++ src/direction.h
@@ -6,13 +6,24 @@
 #define DIRECTION_H
 
 typedef enum {
+    DIR_NONE,
     DIR_WEST,
     DIR_NORTH,
     DIR_EAST,
     DIR_SOUTH
 } Direction;
 
+#define MASK_DIR_WEST (1 << DIR_WEST)
+#define MASK_DIR_NORTH (1 << DIR_NORTH)
+#define MASK_DIR_EAST (1 << DIR_EAST)
+#define MASK_DIR_SOUTH (1 << DIR_SOUTH)
+#define MASK_DIR_ALL (MASK_DIR_WEST | MASK_DIR_NORTH | MASK_DIR_EAST | MASK_DIR_SOUTH)
+
+#define DIR_IN_MASK(dir,mask) ((1 << (dir)) & (mask))
+#define DIR_ADD_TO_MASK(dir,mask) ((1 << (dir)) | (mask))
+
 void dirMove(Direction dir, int *x, int *y);
 Direction dirReverse(Direction dir);
+Direction dirFindPath(int from_x, int from_y, int to_x, int to_y, int valid_directions_mask);
 
 #endif
