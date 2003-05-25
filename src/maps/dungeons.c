68f8c27b544e5d4d070c43ad2b7ce75008c218b0
Andrew Taylor <andrewtaylor@users.sourceforge.net>
2003-05-25 23:23:16 +0000

    initial revision

---
+++ src/maps/dungeons.c
@@ -0,0 +1,129 @@
+/*
+ * $Id$
+ */
+
+#include <stddef.h>
+
+#include "../u4.h"
+#include "../map.h"
+
+Map deceit_map = {
+    17,
+    "deceit.dng", /* fname */
+    MAP_DUNGEON, /* type */
+    DNG_WIDTH, DNG_HEIGHT, 8, /* width, height, levels */
+    0, 0, 0, /* startx, starty, startlevel */
+    BORDER_WRAP, /* border_behavior */
+    0, /* n_portals */
+    NULL, /* portals */
+    FIRST_PERSON, /* flags */
+    MUSIC_DUNGEON, /* music */
+    NULL, /* data */
+    { NULL } /* dungeon */
+};
+
+Map despise_map = {
+    18,
+    "despise.dng", /* fname */
+    MAP_DUNGEON, /* type */
+    DNG_WIDTH, DNG_HEIGHT, 8, /* width, height, levels */
+    0, 0, 0, /* startx, starty, startlevel */
+    BORDER_WRAP, /* border_behavior */
+    0, /* n_portals */
+    NULL, /* portals */
+    FIRST_PERSON, /* flags */
+    MUSIC_DUNGEON, /* music */
+    NULL, /* data */
+    { NULL } /* dungeon */
+};
+
+Map destard_map = {
+    19,
+    "destard.dng", /* fname */
+    MAP_DUNGEON, /* type */
+    DNG_WIDTH, DNG_HEIGHT, 8, /* width, height, levels */
+    0, 0, 0, /* startx, starty, startlevel */
+    BORDER_WRAP, /* border_behavior */
+    0, /* n_portals */
+    NULL, /* portals */
+    FIRST_PERSON, /* flags */
+    MUSIC_DUNGEON, /* music */
+    NULL, /* data */
+    { NULL } /* dungeon */
+};
+
+Map wrong_map = {
+    20,
+    "wrong.dng", /* fname */
+    MAP_DUNGEON, /* type */
+    DNG_WIDTH, DNG_HEIGHT, 8, /* width, height, levels */
+    0, 0, 0, /* startx, starty, startlevel */
+    BORDER_WRAP, /* border_behavior */
+    0, /* n_portals */
+    NULL, /* portals */
+    FIRST_PERSON, /* flags */
+    MUSIC_DUNGEON, /* music */
+    NULL, /* data */
+    { NULL } /* dungeon */
+};
+
+Map covetous_map = {
+    21,
+    "covetous.dng", /* fname */
+    MAP_DUNGEON, /* type */
+    DNG_WIDTH, DNG_HEIGHT, 8, /* width, height, levels */
+    0, 0, 0, /* startx, starty, startlevel */
+    BORDER_WRAP, /* border_behavior */
+    0, /* n_portals */
+    NULL, /* portals */
+    FIRST_PERSON, /* flags */
+    MUSIC_DUNGEON, /* music */
+    NULL, /* data */
+    { NULL } /* dungeon */
+};
+
+Map shame_map = {
+    22,
+    "shame.dng", /* fname */
+    MAP_DUNGEON, /* type */
+    DNG_WIDTH, DNG_HEIGHT, 8, /* width, height, levels */
+    0, 0, 0, /* startx, starty, startlevel */
+    BORDER_WRAP, /* border_behavior */
+    0, /* n_portals */
+    NULL, /* portals */
+    FIRST_PERSON, /* flags */
+    MUSIC_DUNGEON, /* music */
+    NULL, /* data */
+    { NULL } /* dungeon */
+};
+
+Map hythloth_map = {
+    23,
+    "hythloth.dng", /* fname */
+    MAP_DUNGEON, /* type */
+    DNG_WIDTH, DNG_HEIGHT, 8, /* width, height, levels */
+    0, 0, 0, /* startx, starty, startlevel */
+    BORDER_WRAP, /* border_behavior */
+    0, /* n_portals */
+    NULL, /* portals */
+    FIRST_PERSON, /* flags */
+    MUSIC_DUNGEON, /* music */
+    NULL, /* data */
+    { NULL } /* dungeon */
+};
+
+Map abyss_map = {
+    24,
+    "abyss.dng", /* fname */
+    MAP_DUNGEON, /* type */
+    DNG_WIDTH, DNG_HEIGHT, 8, /* width, height, levels */
+    0, 0, 0, /* startx, starty, startlevel */
+    BORDER_WRAP, /* border_behavior */
+    0, /* n_portals */
+    NULL, /* portals */
+    FIRST_PERSON, /* flags */
+    MUSIC_DUNGEON, /* music */
+    NULL, /* data */
+    { NULL } /* dungeon */
+};
+
