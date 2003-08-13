3355a850c5bf2288964be5f31610889fb72f3f93
Doug Day <dougday@users.sourceforge.net>
2003-08-13 06:31:43 +0000

    Fixed bug #785810 - Only magic weapons should work in the Abyss

--- src/combat.c
+++ src/combat.c
@@ -480,7 +480,7 @@ int combatAttackAtCoord(int x, int y, int distance, void *data) {
     
         /* Did the weapon miss? */
         if ((c->location->map->id == 24 && !weaponIsMagic(weapon)) || /* non-magical weapon in the Abyss */
-            !playerAttackHit(&c->saveGame->players[focus])) {        /* player naturally missed */
+            !playerAttackHit(&c->saveGame->players[focus])) {         /* player naturally missed */
             screenMessage("Missed!\n");
         
             /* show the 'miss' tile */
