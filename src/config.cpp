0da9fa41424fb4fa06872384339108f73368626d
Andrew Taylor <andrewtaylor@users.sourceforge.net>
2004-05-23 05:09:52 +0000

    initial revision

---
+++ src/config.cpp
@@ -0,0 +1,192 @@
+/*
+ * $Id$
+ */
+
+#include <cstdlib>
+#include <cctype>
+
+#include <libxml/xmlmemory.h>
+#include <libxml/parser.h>
+#include <libxml/valid.h>
+#include <libxml/xmlIO.h>
+#include <libxml/xinclude.h>
+#include <libxml/xpath.h>
+
+#include "config.h"
+#include "error.h"
+#include "settings.h"
+#include "u4file.h"
+
+using namespace std;
+
+extern bool verbose;
+Config *Config::instance = NULL;
+
+const Config *Config::getInstance() {
+    if (!instance) {
+        xmlRegisterInputCallbacks(&xmlFileMatch, &fileOpen, xmlFileRead, xmlFileClose);
+        instance = new Config;
+    }
+    return instance;
+}
+
+ConfigElement Config::getElement(const string &path) const {
+    xmlXPathContextPtr context;
+    xmlXPathObjectPtr result;
+
+    context = xmlXPathNewContext(doc);
+    result = xmlXPathEvalExpression(reinterpret_cast<const xmlChar *>(path.c_str()), context);
+    if(xmlXPathNodeSetIsEmpty(result->nodesetval))
+        errorFatal("no match for xpath %s\n", path.c_str());
+
+    xmlXPathFreeContext(context);
+
+    if (result->nodesetval->nodeNr > 1)
+        errorWarning("more than one match for xpath %s\n", path.c_str());
+
+    xmlNodePtr node = result->nodesetval->nodeTab[0];
+    xmlXPathFreeObject(result);
+
+    return ConfigElement(node);
+}
+
+Config::Config() {
+    doc = xmlParseFile("config.xml");
+    if (!doc)
+        errorFatal("error parsing config.xml");
+
+    xmlXIncludeProcess(doc);
+
+    if (settings.validateXml && doc->intSubset) {
+        string errorMessage;
+        xmlValidCtxt cvp;
+
+        if (verbose)
+            printf("validating config.xml\n");
+
+        cvp.userData = &errorMessage;
+        cvp.error = &accumError;
+
+        if (!xmlValidateDocument(&cvp, doc))
+            errorFatal("xml parse error:\n%s", errorMessage.c_str());        
+    }
+}
+
+
+void *Config::fileOpen(const char *filename) {
+    void *result;
+    string pathname(u4find_conf(filename));
+
+    if (pathname.empty())
+        return NULL;
+    result = xmlFileOpen(pathname.c_str());
+
+    if (verbose)
+        printf("xml parser opened %s: %s\n", pathname.c_str(), result ? "success" : "failed");
+
+    return result;
+}
+
+void Config::accumError(void *l, const char *fmt, ...) {
+    string* errorMessage = (string *) l;
+    char buffer[1000];
+    va_list args;
+
+    va_start(args, fmt);
+    vsnprintf(buffer, sizeof(buffer), fmt, args);
+    va_end(args);
+
+    errorMessage->append(buffer);
+}
+
+ConfigElement::ConfigElement(xmlNodePtr xmlNode) : node(xmlNode), name(reinterpret_cast<const char *>(xmlNode->name)) {
+}
+
+ConfigElement::ConfigElement(const ConfigElement &e) : node(e.node), name(e.name) {
+}
+
+ConfigElement::~ConfigElement() {
+}
+
+ConfigElement &ConfigElement::operator=(const ConfigElement &e) {
+    if (&e != this) {
+        node = e.node;
+        name = e.name;
+    }
+    return *this;
+}
+
+string ConfigElement::getString(const string &name) const {
+    xmlChar *prop = xmlGetProp(node, reinterpret_cast<const xmlChar *>(name.c_str()));
+    if (!prop)
+        return "";
+
+    string result(reinterpret_cast<const char *>(prop));
+    free(prop);
+    
+    return result;
+}
+
+int ConfigElement::getInt(const string &name) const {
+    long result;
+    xmlChar *prop;
+
+    prop = xmlGetProp(node, reinterpret_cast<const xmlChar *>(name.c_str()));
+    if (!prop)
+        return 0;
+
+    result = strtol(reinterpret_cast<const char *>(prop), NULL, 0);
+    xmlFree(prop);
+
+    return static_cast<int>(result);
+}
+
+bool ConfigElement::getBool(const string &name) const {
+    int result;
+
+    xmlChar *prop = xmlGetProp(node, reinterpret_cast<const xmlChar *>(name.c_str()));
+    if (!prop)
+        return false;
+
+    if (xmlStrcmp(prop, reinterpret_cast<const xmlChar *>("true")) == 0)
+        result = true;
+    else
+        result = false;
+
+    xmlFree(prop);
+
+    return result;
+}
+
+int ConfigElement::getEnum(const string &name, const char *enumValues[]) const {
+    int result = -1, i;
+    xmlChar *prop;
+
+    prop = xmlGetProp(node, reinterpret_cast<const xmlChar *>(name.c_str()));
+    if (!prop)
+        return 0;
+
+    for (i = 0; enumValues[i]; i++) {
+        if (xmlStrcmp(prop, (const xmlChar *) enumValues[i]) == 0)
+        result = i;
+    }
+
+    if (result == -1)
+        errorFatal("invalid enum value for %s: %s", name.c_str(), prop);
+
+    xmlFree(prop);
+
+    return result;
+}
+
+vector<ConfigElement> ConfigElement::getChildren() const {
+    vector<ConfigElement> result;
+
+    for (xmlNodePtr child = node->children; child; child = child->next) {
+        if (child->type == XML_ELEMENT_NODE)
+            result.push_back(ConfigElement(child));
+    }
+
+    return result;
+}
+
---
+++ src/config.h
@@ -0,0 +1,61 @@
+/*
+ * $Id$
+ */
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include "vc6.h" // Fixes things if you're using VC6, does nothing if otherwise
+#include <string>
+#include <vector>
+#include <libxml/xmlmemory.h>
+
+class ConfigElement;
+
+/**
+ * Singleton class that manages the XML configuration tree.
+ */
+class Config {
+public:
+    static const Config *getInstance();
+
+    ConfigElement getElement(const std::string &path) const;
+
+private:
+    Config();
+    static void *fileOpen(const char *filename);
+    static void accumError(void *l, const char *fmt, ...);
+
+    static Config *instance;
+    xmlDocPtr doc;
+};
+
+/**
+ * A single configuration element in the config tree.  Right now, a
+ * thin wrapper around the XML DOM element.
+ */
+class ConfigElement {
+public:
+    ConfigElement(xmlNodePtr xmlNode);
+    ConfigElement(const ConfigElement &e);
+    ~ConfigElement();
+
+    ConfigElement &operator=(const ConfigElement &e);
+
+    const std::string getName() const { return name; }
+
+    std::string getString(const std::string &name) const;
+    int getInt(const std::string &name) const;
+    bool getBool(const std::string &name) const;
+    int getEnum(const std::string &name, const char *enumValues[]) const;
+
+    std::vector<ConfigElement> getChildren() const;
+
+    xmlNodePtr getNode() const { return node; }
+
+private:
+    xmlNodePtr node;
+    std::string name;
+};
+
+#endif /* CONFIG_H */
