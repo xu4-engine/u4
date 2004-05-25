4474ea2c126bf8ae748ad3c32d57a58c1292363c
Steve Saunders <steven-j-s@users.sourceforge.net>
2004-05-25 00:40:44 +0000

    OS X version won't compile without the extern 'C'

--- src/error.h
+++ src/error.h
@@ -5,6 +5,10 @@
 #ifndef ERROR_H
 #define ERROR_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #if __GNUC__
 #define PRINTF_LIKE(x,y)  __attribute__ ((format (printf, (x), (y))))
 #else
@@ -13,5 +17,9 @@
 
 void errorFatal(const char *fmt, ...) PRINTF_LIKE(1, 2);
 void errorWarning(const char *fmt, ...) PRINTF_LIKE(1, 2);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
