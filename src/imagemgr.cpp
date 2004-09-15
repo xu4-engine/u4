d956ab91c72ea03566f699d95a26098b28700488
Andrew Taylor <andrewtaylor@users.sourceforge.net>
2004-09-15 04:27:40 +0000

    initial revision

---
+++ src/imagemgr.cpp
@@ -0,0 +1,477 @@
+/*
+ * $Id$
+ */
+
+#include "vc6.h" // Fixes things if you're using VC6, does nothing if otherwise
+
+#include <vector>
+
+#include "config.h"
+#include "debug.h"
+#include "error.h"
+#include "image.h"
+#include "imageloader.h"
+#include "imagemgr.h"
+#include "intro.h"
+#include "settings.h"
+#include "u4file.h"
+
+using std::map;
+using std::string;
+using std::vector;
+
+Image *screenScale(Image *src, int scale, int n, int filter);
+
+struct ImageSet {
+    string name;
+    string location;
+    string extends;
+    map<string, ImageInfo *> info;
+};
+
+ImageMgr *ImageMgr::instance = NULL;
+
+ImageMgr *ImageMgr::getInstance() {
+    if (instance == NULL)
+        instance = new ImageMgr();
+    return instance;
+}
+
+ImageMgr::ImageMgr() {
+    logger = new Debug("debug/imagemgr.txt", "ImageMgr");
+    TRACE(*logger, "creating ImageMgr");
+
+    const Config *config = Config::getInstance();
+    vector<ConfigElement> graphicsConf = config->getElement("/config/graphics").getChildren();
+    for (std::vector<ConfigElement>::iterator conf = graphicsConf.begin(); conf != graphicsConf.end(); conf++) {
+        if (conf->getName() == "imageset") {
+            ImageSet *set = loadImageSetFromConf(*conf);
+            imageSets[set->name] = set;
+        }
+    }
+
+    imageSetNames.clear();
+    for (std::map<string, ImageSet *>::const_iterator set = imageSets.begin(); set != imageSets.end(); set++)
+        imageSetNames.push_back(set->first);
+}
+
+ImageSet *ImageMgr::loadImageSetFromConf(const ConfigElement &conf) {
+    ImageSet *set;
+
+    set = new ImageSet;
+    set->name = conf.getString("name");
+    set->location = conf.getString("location");
+    set->extends = conf.getString("extends");
+
+    vector<ConfigElement> children = conf.getChildren();
+    for (vector<ConfigElement>::iterator i = children.begin(); i != children.end(); i++) {
+        if (i->getName() == "image") {
+            ImageInfo *info = loadImageInfoFromConf(*i);
+            set->info[info->name] = info;
+        }
+    }
+
+    return set;
+}
+
+ImageInfo *ImageMgr::loadImageInfoFromConf(const ConfigElement &conf) {
+    ImageInfo *info;
+    static const char *fixupEnumStrings[] = { "none", "intro", "introExtended", "abyss", "abacus", "dungns", NULL };
+
+    info = new ImageInfo;
+    info->name = conf.getString("name");
+    info->filename = conf.getString("filename");
+    info->width = conf.getInt("width");
+    info->height = conf.getInt("height");
+    info->depth = conf.getInt("depth");
+    info->prescale = conf.getInt("prescale");
+    info->filetype = conf.getString("filetype");
+    info->tiles = conf.getInt("tiles");
+    info->introOnly = conf.getBool("introOnly");
+    info->transparentIndex = conf.getInt("transparentIndex", -1);
+
+    info->xu4Graphic = conf.getBool("xu4Graphic");
+    info->fixup = static_cast<ImageFixup>(conf.getEnum("fixup", fixupEnumStrings));
+    info->image = NULL;
+
+    vector<ConfigElement> children = conf.getChildren();
+    for (vector<ConfigElement>::iterator i = children.begin(); i != children.end(); i++) {
+        if (i->getName() == "subimage") {
+            SubImage *subimage = loadSubImageFromConf(info, *i);            
+            info->subImages[subimage->name] = subimage;
+        }
+    }
+
+    return info;
+}
+
+SubImage *ImageMgr::loadSubImageFromConf(const ImageInfo *info, const ConfigElement &conf) {
+    SubImage *subimage;
+    static int x = 0,
+               y = 0,
+               last_width = 0,
+               last_height = 0;    
+
+    subimage = new SubImage;
+    subimage->name = conf.getString("name");    
+    subimage->width = conf.getInt("width");
+    subimage->height = conf.getInt("height");
+    subimage->srcImageName = info->name;
+    if (conf.exists("x") && conf.exists("y")) {
+        x = subimage->x = conf.getInt("x");
+        y = subimage->y = conf.getInt("y");
+    }
+    else {
+        // Automatically increment our position through the base image
+        x += last_width;
+        if (x >= last_width) {
+            x = 0;
+            y += last_height;
+        }
+
+        subimage->x = x;
+        subimage->y = y;
+    }
+
+    // "remember" the width and height of this subimage
+    last_width = subimage->width;
+    last_height = subimage->height;
+
+    return subimage;
+}
+
+void ImageMgr::fixupIntro(Image *im, int prescale) {
+    const unsigned char *sigData;
+    int i, x, y;
+
+    sigData = intro->getSigData();
+
+    /* -----------------------------------------------------------------------------
+     * copy "present" to new location between "Origin Systems, Inc." and "Ultima IV"
+     * ----------------------------------------------------------------------------- */
+
+    im->drawSubRectOn(im, 
+                      136 * prescale,
+                      33 * prescale,
+                      136 * prescale,
+                      0 * prescale,
+                      55 * prescale,
+                      5 * prescale);
+
+    /* ----------------------------
+     * erase the original "present"
+     * ---------------------------- */
+
+    im->fillRect(136 * prescale, 0 * prescale, 55 * prescale, 5 * prescale, 0, 0, 0);
+
+    /* -----------------------------
+     * draw "Lord British" signature
+     * ----------------------------- */
+    i = 0;
+    while (sigData[i] != 0) {
+        /*  (x/y) are unscaled coordinates, i.e. in 320x200  */
+        x = sigData[i] + 0x14;
+        y = 0xBF - sigData[i+1];
+        im->fillRect(x * prescale, y * prescale, prescale, prescale, 0, 255, 255); /* cyan */
+        im->fillRect((x + 1) * prescale, y * prescale, prescale, prescale, 0, 255, 255); /* cyan */
+        i += 2;
+    }
+
+    /* --------------------------------------------------------------
+     * draw the red line between "Origin Systems, Inc." and "present"
+     * -------------------------------------------------------------- */
+    /* we're still working with an unscaled surface */
+    for (i = 86; i < 239; i++)
+        im->fillRect(i * prescale, 31 * prescale, prescale, prescale, 128, 0, 0); /* red */
+}
+
+void ImageMgr::fixupIntroExtended(Image *im, int prescale) {
+    fixupIntro(im, prescale);
+
+    im->drawSubRectOn(im, 
+                      0 * prescale,
+                      10 * prescale,
+                      0 * prescale,
+                      95 * prescale,
+                      320 * prescale,
+                      50 * prescale);
+
+    im->drawSubRectOn(im, 
+                      0 * prescale,
+                      60 * prescale,
+                      0 * prescale, 
+                      105 * prescale,
+                      320 * prescale,
+                      45 * prescale);
+}
+
+void ImageMgr::fixupAbyssVision(Image *im, int prescale) {
+    static unsigned int *data = NULL;
+
+    /*
+     * Each VGA vision components must be XORed with all the previous
+     * vision components to get the actual image.
+     */
+    if (data != NULL) {
+        for (int y = 0; y < im->height(); y++) {
+            for (int x = 0; x < im->width(); x++) {
+                unsigned int index;
+                im->getPixelIndex(x, y, index);
+                index ^= data[y * im->width() + x];
+                im->putPixelIndex(x, y, index);
+            }
+        }
+    } else {
+        data = new unsigned int[im->width() * im->height()];
+    }
+
+    for (int y = 0; y < im->height(); y++) {
+        for (int x = 0; x < im->width(); x++) {
+            unsigned int index;
+            im->getPixelIndex(x, y, index);
+            data[y * im->width() + x] = index;
+        }
+    }
+}
+
+void ImageMgr::fixupAbacus(Image *im, int prescale) {
+
+    /* 
+     * surround each bead with a row green pixels to avoid artifacts
+     * when scaling
+     */
+
+    im->fillRect(7 * prescale, 186 * prescale, prescale, 14 * prescale, 0, 255, 80); /* green */
+    im->fillRect(16 * prescale, 186 * prescale, prescale, 14 * prescale, 0, 255, 80); /* green */
+    im->fillRect(8 * prescale, 186 * prescale, prescale * 8, prescale, 0, 255, 80); /* green */
+    im->fillRect(8 * prescale, 199 * prescale, prescale * 8, prescale, 0, 255, 80); /* green */
+
+    im->fillRect(23 * prescale, 186 * prescale, prescale, 14 * prescale, 0, 255, 80); /* green */
+    im->fillRect(32 * prescale, 186 * prescale, prescale, 14 * prescale, 0, 255, 80); /* green */
+    im->fillRect(24 * prescale, 186 * prescale, prescale * 8, prescale, 0, 255, 80); /* green */
+    im->fillRect(24 * prescale, 199 * prescale, prescale * 8, prescale, 0, 255, 80); /* green */
+}
+
+/**
+ * Swap blue and green for the dungeon walls when facing north or
+ * south.
+ */
+void ImageMgr::fixupDungNS(Image *im, int prescale) {
+    for (int y = 0; y < im->height(); y++) {
+        for (int x = 0; x < im->width(); x++) {
+            unsigned int index;
+            im->getPixelIndex(x, y, index);
+            if (index == 1)
+                im->putPixelIndex(x, y, 2);
+            else if (index == 2)
+                im->putPixelIndex(x, y, 1);
+        }
+    }
+}
+
+/**
+ * Returns information for the given image set.
+ */
+ImageSet *ImageMgr::getSet(const string &setname) {
+    std::map<string, ImageSet *>::iterator i = imageSets.find(setname);
+    if (i != imageSets.end())
+        return i->second;
+    else
+        return NULL;
+}
+
+/**
+ * Returns image information for the current image set.
+ */
+ImageInfo *ImageMgr::getInfo(const string &name) {
+    string setname;
+
+    if (!u4isUpgradeAvailable())
+        setname = "EGA";
+    else
+        setname = settings.videoType;
+
+    return getInfoFromSet(name, setname);
+}
+
+/**
+ * Returns information for the given image set.
+ */
+ImageInfo *ImageMgr::getInfoFromSet(const string &name, const string &setname) {
+    ImageSet *set;
+    
+    set = getSet(setname);
+    if (!set)
+        return NULL;
+
+    /* if the image set contains the image we want, we are done */
+    std::map<string, ImageInfo *>::iterator i = set->info.find(name);
+    if (i != set->info.end())
+        return i->second;
+
+    /* otherwise if this image set extends another, check the base image set */
+    if (set->extends != "")
+        return getInfoFromSet(name, set->extends);
+
+    return NULL;
+}
+
+/**
+ * Load in a background image from a ".ega" file.
+ */
+ImageInfo *ImageMgr::get(const string &name) {
+    ImageInfo *info = getInfo(name);
+    if (!info)
+        return NULL;
+
+    /* return if already loaded */
+    if (info->image != NULL)
+        return info;
+
+    /*
+     * If the u4 VGA upgrade is installed (i.e. setup has been run and
+     * the u4dos files have been renamed), we need to use VGA names
+     * for EGA and vice versa, but *only* when the upgrade file has a
+     * .old extention.  The charset and tiles have a .vga extention
+     * and are not renamed in the upgrade installation process
+     */
+    string filename = info->filename;
+    if (u4isUpgradeInstalled() && getInfoFromSet(name, "VGA")->filename.find(".old") != string::npos) {
+        if (settings.videoType == "EGA")
+            filename = getInfoFromSet(name, "VGA")->filename;
+        else
+            filename = getInfoFromSet(name, "EGA")->filename;
+    }
+
+    if (filename == "")
+        return NULL;
+
+    U4FILE *file = NULL;
+    if (info->xu4Graphic) {
+        string pathname(u4find_graphics(filename));
+
+        if (!pathname.empty())
+            file = u4fopen_stdio(pathname);
+    } 
+    else {
+        file = u4fopen(filename);
+    }
+    
+    Image *unscaled = NULL;
+    if (file) {
+        ImageLoader *loader = ImageLoader::getLoader(info->filetype);
+        if (loader == NULL)
+            errorFatal("can't load image of type \"%s\"", info->filetype.c_str());
+        loader->setDimensions(info->width, info->height, info->depth);
+        unscaled = loader->load(file);
+        u4fclose(file);
+
+    }
+    if (unscaled == NULL)
+        return NULL;
+
+    if (info->transparentIndex != -1)
+        unscaled->setTransparentIndex(info->transparentIndex);
+
+    if (info->prescale == 0)
+        info->prescale = 1;
+
+    /*
+     * fixup the image before scaling it
+     */
+    switch (info->fixup) {
+    case FIXUP_NONE:
+        break;
+    case FIXUP_INTRO:
+        fixupIntro(unscaled, info->prescale);
+        break;
+    case FIXUP_INTRO_EXTENDED:
+        fixupIntroExtended(unscaled, info->prescale);
+        break;
+    case FIXUP_ABYSS:
+        fixupAbyssVision(unscaled, info->prescale);
+        break;
+    case FIXUP_ABACUS:
+        fixupAbacus(unscaled, info->prescale);
+        break;
+    case FIXUP_DUNGNS:
+        fixupDungNS(unscaled, info->prescale);
+        break;
+    }
+
+    int imageScale = settings.scale;
+    if (info->prescale != 0) {
+        if ((settings.scale % info->prescale) != 0)
+            errorFatal("image %s is prescaled to an incompatible size: %d\n", filename.c_str(), info->prescale);
+        imageScale /= info->prescale;
+    }
+        
+    info->image = screenScale(unscaled, imageScale, info->tiles, 1);
+
+    delete unscaled;
+    return info;
+}
+
+/**
+ * Returns information for the given image set.
+ */
+SubImage *ImageMgr::getSubImage(const string &name) {
+    string setname;
+
+    if (!u4isUpgradeAvailable())
+        setname = "EGA";
+    else
+        setname = settings.videoType;
+
+    ImageSet *set = getSet(setname);
+
+    while (set != NULL) {
+        for (std::map<string, ImageInfo *>::iterator i = set->info.begin(); i != set->info.end(); i++) {
+            ImageInfo *info = (ImageInfo *) i->second;
+            std::map<string, SubImage *>::iterator j = info->subImages.find(name);
+            if (j != info->subImages.end())
+                return j->second;
+        }
+
+        set = getSet(set->extends);
+    }
+        
+    return NULL;
+}
+
+/**
+ * Free up any background images used only in the animations.
+ */
+void ImageMgr::freeIntroBackgrounds() {
+    for (std::map<string, ImageSet *>::iterator i = imageSets.begin(); i != imageSets.end(); i++) {
+        ImageSet *set = i->second;
+        for (std::map<string, ImageInfo *>::iterator j = set->info.begin(); j != set->info.end(); j++) {
+            ImageInfo *info = j->second;
+            if (info->image != NULL && info->introOnly) {
+                delete info->image;
+                info->image = NULL;
+            }
+        }
+    }
+}
+
+/**
+ * Free up all images
+ */
+void ImageMgr::freeAll() {
+    for (std::map<string, ImageSet *>::iterator i = imageSets.begin(); i != imageSets.end(); i++) {
+        ImageSet *set = i->second;
+        for (std::map<string, ImageInfo *>::iterator j = set->info.begin(); j != set->info.end(); j++) {
+            ImageInfo *info = j->second;
+            if (info->image != NULL) {
+                delete info->image;                
+                info->image = NULL;
+            }
+        }
+    }
+}
+
+const vector<string> &ImageMgr::getSetNames() {
+    return imageSetNames;
+}
+
---
+++ src/imagemgr.h
@@ -0,0 +1,130 @@
+/*
+ * $Id$
+ */
+
+#ifndef IMAGEMGR_H
+#define IMAGEMGR_H
+
+#include <map>
+#include <string>
+#include <vector>
+
+#include "image.h"
+
+class ConfigElement;
+class Debug;
+class ImageSet;
+
+/*
+ * The image manager is responsible for loading and keeping track of
+ * the various images.
+ */
+
+#define BKGD_SHAPES "tiles"
+#define BKGD_CHARSET "charset"
+#define BKGD_BORDERS "borders"
+#define BKGD_INTRO "title"
+#define BKGD_INTRO_EXTENDED "titlex"
+#define BKGD_TREE "tree"
+#define BKGD_PORTAL "portal"
+#define BKGD_OUTSIDE "outside"
+#define BKGD_INSIDE "inside"
+#define BKGD_WAGON "wagon"
+#define BKGD_GYPSY "gypsy"
+#define BKGD_ABACUS "abacus"
+#define BKGD_HONCOM "honcom"
+#define BKGD_VALJUS "valjus"
+#define BKGD_SACHONOR "sachonor"
+#define BKGD_SPIRHUM "spirhum"
+#define BKGD_ANIMATE "beasties"
+#define BKGD_KEY "key"
+#define BKGD_HONESTY "honesty"
+#define BKGD_COMPASSN "compassn"
+#define BKGD_VALOR "valor"
+#define BKGD_JUSTICE "justice"
+#define BKGD_SACRIFIC "sacrific"
+#define BKGD_HONOR "honor"
+#define BKGD_SPIRIT "spirit"
+#define BKGD_HUMILITY "humility"
+#define BKGD_TRUTH "truth"
+#define BKGD_LOVE "love"
+#define BKGD_COURAGE "courage"
+#define BKGD_STONCRCL "stoncrcl"
+#define BKGD_RUNE_INF "rune0"
+#define BKGD_SHRINE_HON "rune1"
+#define BKGD_SHRINE_COM "rune2"
+#define BKGD_SHRINE_VAL "rune3"
+#define BKGD_SHRINE_JUS "rune4"
+#define BKGD_SHRINE_SAC "rune5"
+#define BKGD_SHRINE_HNR "rune6"
+#define BKGD_SHRINE_SPI "rune7"
+#define BKGD_SHRINE_HUM "rune8"
+#define BKGD_GEMTILES "gemtiles"
+
+enum ImageFixup {
+    FIXUP_NONE,
+    FIXUP_INTRO,
+    FIXUP_INTRO_EXTENDED,
+    FIXUP_ABYSS,
+    FIXUP_ABACUS,
+    FIXUP_DUNGNS
+};
+
+/**
+ * Image meta info.
+ */
+struct ImageInfo {
+    std::string name;
+    std::string filename;
+    int width, height, depth;
+    int prescale;
+    std::string filetype;
+    int tiles;                  /**< used to scale the without bleeding colors between adjacent tiles */
+    bool introOnly;             /**< whether can be freed after the intro */
+    int transparentIndex;       /**< color index to consider transparent */
+    bool xu4Graphic;            /**< an original xu4 graphic not part of u4dos or the VGA upgrade */
+    ImageFixup fixup;           /**< a routine to do miscellaneous fixes to the image */
+    Image *image;               /**< the image we're describing */
+    std::map<std::string, SubImage *> subImages;
+};
+
+/**
+ * The image manager singleton that keeps track of all the images.
+ */
+class ImageMgr {
+public:
+    static ImageMgr *getInstance();
+
+    ImageInfo *get(const std::string &name);
+    SubImage *ImageMgr::getSubImage(const std::string &name);
+    void freeIntroBackgrounds();
+    void freeAll();
+    const std::vector<std::string> &getSetNames();
+
+private:
+    ImageMgr();
+
+    ImageSet *loadImageSetFromConf(const ConfigElement &conf);
+    ImageInfo *loadImageInfoFromConf(const ConfigElement &conf);
+    SubImage *loadSubImageFromConf(const ImageInfo *info, const ConfigElement &conf);
+
+    ImageSet *getSet(const std::string &setname);
+    ImageInfo *getInfo(const std::string &name);
+    ImageInfo *getInfoFromSet(const std::string &name, const std::string &setname);
+
+    void fixupIntro(Image *im, int prescale);
+    void fixupIntroExtended(Image *im, int prescale);
+    void fixupAbyssVision(Image *im, int prescale);
+    void fixupAbacus(Image *im, int prescale);
+    void fixupDungNS(Image *im, int prescale);
+
+    static ImageMgr *instance;
+    std::map<std::string, ImageSet *> imageSets;
+    std::vector<std::string> imageSetNames;
+
+    Debug *logger;
+};
+
+#define imageMgr (ImageMgr::getInstance())
+
+#endif /* IMAGEMGR_H */
