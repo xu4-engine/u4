f437a5ce5a99bbbec07ff6fcb4648c055e2ef1d7
Andrew Taylor <andrewtaylor@users.sourceforge.net>
2004-12-07 01:58:17 +0000

    updated version number

--- src/u4.h
+++ src/u4.h
@@ -11,7 +11,7 @@ extern "C" {
 
 /* Microsoft VC port */
 #if defined(_MSC_VER)
-    #define VERSION "1.0beta1"
+    #define VERSION "1.1cvs"
 #endif
 
 /* info for loading city data from *.ult and *.tlk */
