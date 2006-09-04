5749fbdbb13ffadbce19216014c4fb7fcbfac113
Michael Ryan <solus@users.sourceforge.net>
2006-09-04 10:23:09 +0000

    added a pragma for Warning C4996) // Disable "'stricmp' was declared deprecated" compiler warning (MSVC8)

--- src/vc6.h
+++ src/vc6.h
@@ -6,6 +6,9 @@
 #   pragma warning(disable:4503) // Disable "decorated name length exceeded, name was trancated" compiler warning
 #   pragma warning(disable:4786) // Disable "symbol truncated to 255 characters" compiler warning
 #   pragma warning(disable:4800) // Disable "conversion from int to bool" compiler performance warning
+
+// VC8:
+#   pragma warning(disable:4996) // Disable "'stricmp' was declared deprecated" compiler warning
 #endif
 
 #endif
