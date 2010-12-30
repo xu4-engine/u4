1f46aa39527e7ea4adf431705bdf3e964703ce11
Kirben <kirben@users.sourceforge.net>
2010-12-30 13:03:01 +0000

    Update version number to use svn suffix too.

--- src/u4.h
+++ src/u4.h
@@ -11,7 +11,7 @@ extern "C" {
 
 /* Microsoft VC port */
 #if defined(_MSC_VER)
-    #define VERSION "1.1cvs"
+    #define VERSION "1.1svn"
 #endif
 
 /* info for loading city data from *.ult and *.tlk */
