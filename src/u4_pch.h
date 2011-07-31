0b91d3b165683ca158e64e4f6972178ba3fc556e
Trenton Schulz <twschulz@users.sourceforge.net>
2011-07-31 19:19:20 +0000

    Add the precompile header file.
    Sorry about this. I thought that this file already existed, but I apparently forgot to add it.

---
+++ src/u4_pch.h
@@ -0,0 +1,43 @@
+#ifdef __cplusplus
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <list>
+#include <map>
+#include <string>
+#include <vector>
+#include "annotation.h"
+#include "conversation.h"
+#include "config.h"
+#include "debug.h"
+#include "types.h"
+#include "u4.h"
+#include "u4file.h"
+#include "utils.h"
+
+//#include "combat.h"
+//#include "context.h"
+//#include "creature.h"
+/*
+#include "direction.h"
+#include "dungeon.h"
+#include "error.h"
+#include "event.h"
+#include "game.h"
+#include "image.h"
+#include "imagemgr.h"
+#include "location.h"
+#include "map.h"
+#include "mapmgr.h"
+#include "names.h"
+#include "object.h"
+#include "person.h"
+#include "player.h"
+#include "portal.h"
+#include "savegame.h"
+#include "screen.h"
+#include "settings.h"
+#include "stats.h"
+#include "tileset.h"
+*/
+#endif
