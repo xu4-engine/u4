xu4-engine/u4

commit e7c475c27dd99c7f20ec7841a4f94d6b3e105c25

Fix memory leaks in City, Dialog, Map & Weapon.

src/map.cpp
@@ -259,6 +259,7 @@ Map::Map() {
 Map::~Map() {
     for (PortalList::iterator i = portals.begin(); i != portals.end(); i++)
         delete *i;
+    clearObjects();
     delete annotations;
 }
 
@@ -566,6 +567,8 @@ void Map::resetObjectAnimations() {
  * Removes all objects from the given map
  */
 void Map::clearObjects() {
+    for (ObjectDeque::iterator o = objects.begin(); o != objects.end(); o++)
+        delete *o;
     objects.clear();
 }
 

src/city.cpp
@@ -27,7 +27,11 @@ City::~City() {
         delete *i;
     for (PersonRoleList::iterator j = personroles.begin(); j != personroles.end(); j++)
         delete *j;
-    for (std::vector<Dialogue *>::iterator k = extraDialogues.begin(); k != extraDialogues.end(); k++)
+
+    std::vector<Dialogue *>::iterator k;
+    for (k = dialogueStore.begin(); k != dialogueStore.end(); k++)
+        delete *k;
+    for (k = extraDialogues.begin(); k != extraDialogues.end(); k++)
         delete *k;
 }
 

src/city.h
@@ -42,6 +42,7 @@ public:
     PersonList persons;
     string tlk_fname;
     PersonRoleList personroles;
+    std::vector<Dialogue *> dialogueStore;  // Only used to delete Dialogues.
     std::vector<Dialogue *> extraDialogues;
 };
 

src/conversation.cpp
@@ -96,22 +96,6 @@ const vector<ResponsePart> &DynamicResponse::getParts() const {
 }
 
 /*
- * Dialogue::Question class
- */
-Dialogue::Question::Question(const string &txt, Response *yes, Response *no) :
-    text(txt), yesresp(yes->addref()), noresp(no->addref()) {}
-
-string Dialogue::Question::getText() {
-    return text;
-}
-
-Response *Dialogue::Question::getResponse(bool yes) {
-    if (yes)
-        return yesresp;
-    return noresp;
-}
-
-/*
  * Dialogue::Keyword class
  */
 Dialogue::Keyword::Keyword(const string &kw, Response *resp) :
@@ -150,14 +134,16 @@ bool Dialogue::Keyword::operator==(const string &kw) const {
 Dialogue::Dialogue()
     : intro(NULL)
     , longIntro(NULL)
-    , defaultAnswer(NULL)
-    , question(NULL) {
+    , defaultAnswer(NULL) {
 }
 
 Dialogue::~Dialogue() {
     for (KeywordMap::iterator i = keywords.begin(); i != keywords.end(); i++) {
         delete i->second;
     }
+    delete intro;
+    delete longIntro;
+    delete defaultAnswer;
 }
 
 void Dialogue::addKeyword(const string &kw, Response *response) {

src/conversation.h
@@ -107,14 +107,24 @@ public:
      */
     class Question {
     public:
-        Question(const string &txt, Response *yes, Response *no);
+        Question() : yes(NULL), no(NULL) {}
+        ~Question() {
+            delete yes;
+            delete no;
+        }
+        void assign(const string &txt, Response *resYes, Response *resNo) {
+            text = txt;
+            yes  = resYes;
+            no   = resNo;
+        }
+        Response* getResponse(bool getYes) const {
+            return getYes ? yes : no;
+        }
 
-        string getText();
-        Response *getResponse(bool yes);
+        string text;
 
     private:
-        string text;
-        Response *yesresp, *noresp;
+        Response *yes, *no;
     };
 
     /**
@@ -161,10 +171,10 @@ public:
     Response *getIntro(bool familiar = false)       {return intro;}
     Response *getLongIntro(bool familiar = false)   {return longIntro;}
     Response *getDefaultAnswer()                    {return defaultAnswer;}
-    Dialogue::Question *getQuestion()               {return question;}
+    const Dialogue::Question* getQuestion() const   {return &question;}
 
     /*
-     * Getters
+     * Setters
      */
     void setName(const string &n)       {name           = n;}
     void setPronoun(const string &pn)   {pronoun        = pn;}
@@ -173,7 +183,9 @@ public:
     void setLongIntro(Response *i)      {longIntro      = i;}
     void setDefaultAnswer(Response *a)  {defaultAnswer  = a;}
     void setTurnAwayProb(int prob)      {turnAwayProb   = prob;}
-    void setQuestion(Question *q)       {question       = q;}
+    void setQuestion(const string &txt, Response *yes, Response *no) {
+        question.assign(txt, yes, no);
+    }
     void addKeyword(const string &kw, Response *response);
 
     const ResponsePart &getAction() const;
@@ -196,7 +208,7 @@ private:
         int turnAwayProb;
         int attackProb;
     };
-    Question *question;
+    Question question;
 };
 
 /**
@@ -253,7 +265,7 @@ public:
     string playerInput;         /**< A string holding the text the player inputs */
     list<string> reply;         /**< What the talker says */
     class Script *script;       /**< A script that this person follows during the conversation (may be NULL) */
-    Dialogue::Question *question; /**< The current question the player is being asked */
+    const Dialogue::Question *question; /**< The current question the player is being asked */
     int quant;                  /**< For vendor transactions */
     int player;                 /**< For vendor transactions */
     int price;                  /**< For vendor transactions */

src/dialogueloader_tlk.cpp
@@ -92,7 +92,7 @@ Dialogue* U4TlkDialogueLoader::load(void *source) {
         yes->add(ResponsePart::BRAGGED);
         no->add(ResponsePart::HUMBLE);
     }
-    dlg->setQuestion(new Dialogue::Question(strings[7], yes, no));
+    dlg->setQuestion(strings[7], yes, no);
 
     // one of the following four keywords triggers the speaker's question
     Response *job = new Response(string("\n") + strings[3]);

src/maploader.cpp
@@ -189,9 +189,10 @@ bool CityMapLoader::load(Map *map) {
          * to the city; Isaac the ghost in Skara Brae is handled like
          * this
          */
-        if (!found) {
+        if (!found)
             city->extraDialogues.push_back(dialogues[i]);
-        }
+        else
+            city->dialogueStore.push_back(dialogues[i]);
     }
 
     /*

src/person.cpp
@@ -468,7 +468,7 @@ string Person::talkerGetQuestionResponse(Conversation *cnv, const char *answer) 
 
     if (ans == 'y' || ans == 'n') {
         valid = true;
-        yes = ans == 'y';
+        yes = (ans == 'y');
     }
 
     if (!valid) {
@@ -522,7 +522,7 @@ string Person::lordBritishGetQuestionResponse(Conversation *cnv, const char *ans
 }
 
 string Person::getQuestion(Conversation *cnv) {
-    return "\n" + cnv->question->getText() + "\n\nYou say: ";
+    return "\n" + cnv->question->text + "\n\nYou say: ";
 }
 
 /**

commit 526648fa61142a703229690e4f9df33ed3d7bd02

map.cpp: Fix segfault when exiting combat (broken in commit e7c475c2).
Avoid deleting the PartyMembers which are owned by Party.

src/map.cpp
@@ -565,8 +565,10 @@ void Map::resetObjectAnimations() {
  * Removes all objects from the given map
  */
 void Map::clearObjects() {
-    for (ObjectDeque::iterator o = objects.begin(); o != objects.end(); o++)
-        delete *o;
+    for (ObjectDeque::iterator o = objects.begin(); o != objects.end(); o++) {
+        if (! isPartyMember(*o))
+            delete *o;
+    }
     objects.clear();
 }
 