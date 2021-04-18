xu4-engine/u4

commit 7bcf8987d2f91fd2ed05f353a35f17f6968bd9d8

Cleanup CLI argument parsing.
 - Replace global useProfile & profileName with Settings::profile.

src/u4.cpp
@@ -34,8 +34,6 @@
 
 bool verbose = false;
 bool quit = false;
-bool useProfile = false;
-string profileName = "";
 
 //#define ENABLE_PERF
 #include "support/performance.h"
@@ -43,9 +41,106 @@ PERF_OBJ(perf, "debug/performance.txt")
 #define PSTART      PERF_START(perf)
 #define PEND(msg)   PERF_END(perf,msg)
 
-using namespace std;
+
+enum OptionsFlag {
+    OPT_FULLSCREEN = 1,
+    OPT_NO_INTRO   = 2,
+    OPT_NO_AUDIO   = 4,
+    OPT_VERBOSE    = 8
+};
+
+struct Options {
+    uint16_t flags;
+    uint16_t used;
+    uint32_t scale;
+    const char* filter;
+    const char* profile;
+};
+
+#define strEqual(A,B)       (strcmp(A,B) == 0)
+#define strEqualAlt(A,B,C)  (strEqual(A,B) || strEqual(A,C))
+
+/*
+ * Return non-zero if the program should continue.
+ */
+int parseOptions(Options* opt, int argc, char** argv) {
+    int i;
+    for (i = 0; i < argc; i++) {
+        if (strEqual(argv[i], "--filter"))
+        {
+            if (++i >= argc)
+                goto missing_value;
+            opt->filter = argv[i];
+        }
+        else if (strEqualAlt(argv[i], "-s", "--scale"))
+        {
+            if (++i >= argc)
+                goto missing_value;
+            opt->scale = strtoul(argv[i], NULL, 0);
+        }
+        else if (strEqualAlt(argv[i], "-p", "--profile"))
+        {
+            if (++i >= argc)
+                goto missing_value;
+            opt->profile = argv[i];
+        }
+        else if (strEqualAlt(argv[i], "-i", "--skip-intro"))
+        {
+            opt->flags |= OPT_NO_INTRO;
+            opt->used  |= OPT_NO_INTRO;
+        }
+        else if (strEqualAlt(argv[i], "-v", "--verbose"))
+        {
+            opt->flags |= OPT_VERBOSE;
+            opt->used  |= OPT_VERBOSE;
+        }
+        else if (strEqualAlt(argv[i], "-f", "--fullscreen"))
+        {
+            opt->flags |= OPT_FULLSCREEN;
+            opt->used  |= OPT_FULLSCREEN;
+        }
+        else if (strEqualAlt(argv[i], "-q", "--quiet"))
+        {
+            opt->flags |= OPT_NO_AUDIO;
+            opt->used  |= OPT_NO_AUDIO;
+        }
+        else if (strEqualAlt(argv[i], "-h", "--help"))
+        {
+            printf("xu4: Ultima IV Recreated\n"
+                   "v%s (%s)\n\n", VERSION, __DATE__ );
+            printf(
+            "Options:\n"
+            "      --filter <string>   Specify display filtering options.\n"
+            "  -f, --fullscreen        Run in fullscreen mode.\n"
+            "  -h, --help              Print this message and quit.\n"
+            "  -i, --skip-intro        Skip the intro. and load the last saved game.\n"
+            "  -p <string>,\n"
+            "      --profile <string>  Use another set of settings and save files.\n"
+            "  -q, --quiet             Disable audio.\n"
+            "  -s <int>,\n"
+            "      --scale <int>       Specify scaling factor (1-5).\n"
+            "  -v, --verbose           Enable verbose console output.\n"
+
+            "\nFilters: point, 2xBi, 2xSaI, Scale2x\n"
+            "\nHomepage: http://xu4.sourceforge.com\n");
+
+            return 0;
+        }
+        else {
+            errorFatal("Unrecognized argument: %s\n\n"
+                   "Use --help for a list of supported arguments.", argv[i]);
+            return 0;
+        }
+    }
+    return 1;
+
+missing_value:
+    errorFatal("%s requires a value. See --help for more detail.", argv[i-1]);
+    return 0;
+}
 
 
+Options opt;
 
 int main(int argc, char *argv[]) {
     Debug::initGlobal("debug/global.txt");
@@ -53,6 +148,11 @@ int main(int argc, char *argv[]) {
 #if defined(MACOSX)
     osxInit(argv[0]);
 #endif
+
+    /* Parse arguments before setup in case the user only wants some help. */
+    memset(&opt, 0, sizeof opt);
+    if (! parseOptions(&opt, argc-1, argv+1))
+        return 0;
 
     if (!u4fsetup())
     {
@@ -65,119 +165,16 @@ int main(int argc, char *argv[]) {
                     "\n\nVisit the XU4 website for additional information.\n\thttp://xu4.sourceforge.net/");
     }
 
-    unsigned int i;
-    int skipIntro = 0;
-    bool enableAudio = true;
-
-
-    /*
-     * if the -p or -profile arguments are passed to the application,
-     * they need to be identified before the settings are initialized.
-     */
-    for (i = 1; i < (unsigned int)argc; i++) {
-        if (((strcmp(argv[i], "-p") == 0)
-          || (strcmp(argv[i], "--profile") == 0))
-                && (unsigned int)argc > i + 1) {
-            // when grabbing the profile name:
-            // 1. trim leading whitespace
-            // 2. truncate the string at 20 characters
-            // 3. then strip any trailing whitespace
-            profileName = argv[i+1];
-            profileName = profileName.erase(0,profileName.find_first_not_of(' '));
-            profileName.resize(20, ' ');
-            profileName = profileName.erase(profileName.find_last_not_of(' ')+1);
-
-            // verify that profileName is valid, otherwise do not use the profile
-            if (!profileName.empty()) {
-                useProfile = true;
-            }
-            i++;
-            break;
-        }
-    }
-
     /* initialize the settings */
-    settings.init(useProfile, profileName);
+    settings.init(opt.profile);
 
     /* update the settings based upon command-line arguments */
-    for (i = 1; i < (unsigned int)argc; i++) {
-        if (strcmp(argv[i], "--filter") == 0)
-        {
-            if ((unsigned int)argc > i + 1)
-            {
-                settings.filter = argv[i+1];
-                i++;
-            }
-            else
-                errorFatal("%s is invalid alone: Requires a string for input. See --help for more detail.\n", argv[i]);
-        }
-        else if (strcmp(argv[i], "-s") == 0
-              || strcmp(argv[i], "--scale") == 0)
-        {
-            if ((unsigned int)argc > i + 1)
-            {
-                settings.scale = strtoul(argv[i+1], NULL, 0);
-                i++;
-            }
-            else
-                errorFatal("%s is invalid alone: Requires a number for input. See --help for more detail.\n", argv[i]);
-        }
-        else if ( strcmp(argv[i], "-p") == 0
-               || strcmp(argv[i], "--profile") == 0)
-        {
-            // do nothing
-            if ((unsigned int)argc > i + 1)
-                i++;
-            else
-                errorFatal("%s is invalid alone: Requires a string as input. See --help for more detail.\n", argv[i]);
-
-        }
-        else if (strcmp(argv[i], "-i") == 0
-              || strcmp(argv[i], "--skip-intro") == 0)
-        {
-            skipIntro = 1;
-        }
-        else if (strcmp(argv[i], "-v") == 0
-              || strcmp(argv[i], "--verbose") == 0)
-        {
-            verbose = true;
-        }
-        else if (strcmp(argv[i], "-f") == 0
-              || strcmp(argv[i], "--fullscreen") == 0)
-        {
-            settings.fullscreen = 1;
-        }
-        else if (strcmp(argv[i], "-q") == 0
-              || strcmp(argv[i], "--quiet") == 0)
-        {
-            enableAudio = false;
-        }
-        else if (strcmp(argv[i], "-h") == 0
-              || strcmp(argv[i], "--help") == 0)
-        {
-            printf("xu4: Ultima IV Recreated\n"
-                   "v%s (%s)\n\n", VERSION, __DATE__ );
-            printf(
-            "Options:\n"
-            "      --filter <string>   Specify display filtering options.\n"
-            "  -f, --fullscreen        Run in fullscreen mode.\n"
-            "  -h, --help              Print this message and quit.\n"
-            "  -i, --skip-intro        Skip the intro. and load the last saved game.\n"
-            "  -p <string>,\n"
-            "      --profile <string>  Pass extra arguments to the program.\n"
-            "  -q, --quiet             Disable audio.\n"
-            "  -s <int>,\n"
-            "      --scale <int>       Specify scaling factor (1-5).\n"
-            "  -v, --verbose           Enable verbose console output.\n"
-
-            "\nFilters: point, 2xBi, 2xSaI, Scale2x\n"
-            "\nHomepage: http://xu4.sourceforge.com\n");
-
-            goto cleanup;
-        }
-        else
-            errorFatal("Unrecognized argument: %s\n\nUse --help for a list of supported arguments.", argv[i]);
-    }
+    if (opt.used & OPT_FULLSCREEN)
+        settings.fullscreen = (opt.flags & OPT_FULLSCREEN) ? true : false;
+    if (opt.scale)
+        settings.scale = opt.scale;
+    if (opt.filter)
+        settings.filter = opt.filter;
 
     xu4_srandom();
 
@@ -185,6 +182,7 @@ int main(int argc, char *argv[]) {
     configInit();
     screenInit();
     {
+    int skipIntro = opt.flags & OPT_NO_INTRO;
     ProgressBar pb((320/2) - (200/2), (200/2), 200, 10, 0, (skipIntro ? 4 : 6));
     pb.setBorderColor(240, 240, 240);
     pb.setColor(0, 0, 128);
@@ -195,7 +193,7 @@ int main(int argc, char *argv[]) {
     ++pb;
 
     PSTART
-    if (enableAudio)
+    if (! (opt.flags & OPT_NO_AUDIO))
         soundInit();
     PEND("soundInit")
     ++pb;
@@ -255,7 +253,6 @@ int main(int argc, char *argv[]) {
     screenDelete();
     configFree();
 
-cleanup:
     u4fcleanup();
     return 0;
 }

src/intro.cpp
@@ -33,8 +33,6 @@
 
 using namespace std;
 
-extern bool useProfile;
-extern string profileName;
 extern bool quit;
 
 IntroController *intro = NULL;
@@ -630,8 +628,11 @@ void IntroController::updateScreen() {
         drawMap();
         drawBeasties();
         // display the profile name if a local profile is being used
-        if (useProfile)
-            screenTextAt(40-profileName.length(), 24, "%s", profileName.c_str());
+        {
+        const string& pname = settings.profile;
+        if (! pname.empty())
+            screenTextAt(40-pname.length(), 24, "%s", pname.c_str());
+        }
         break;
 
     case INTRO_MENU:

src/settings.cpp
@@ -78,12 +78,17 @@ Settings::Settings() {
 /**
  * Initialize the settings.
  */
-void Settings::init(const bool useProfile, const string profileName) {
-    if (useProfile) {
+void Settings::init(const char* profileName) {
+    if (profileName && profileName[0]) {
         userPath = "./profiles/";
-        userPath += profileName.c_str();
+        userPath += profileName;
         userPath += "/";
+
+        profile = profileName;
+        if (profile.length() > 20)
+            errorFatal("Profile name must be no more than 20 characters.");
     } else {
+        profile.clear();
 
 #if defined(MACOSX)
             FSRef folder;

src/settings.h
@@ -156,7 +156,7 @@ class Settings : public SettingsData, public Observable<Settings *> {
 
 public:
     /* Methods */
-    void init(const bool useProfile, const string profileName);
+    void init(const char* profileName);
     static Settings &getInstance();
     void setData(const SettingsData &data);
     bool read();
@@ -164,6 +164,8 @@ public:
 
     const string &getUserPath();
     const char** getBattleDiffs();
+
+    string profile;
 
 private:
     Settings();

commit f763a3b04742d43449223805231dc372ef79e4e4

Fix verbose CLI option (broken in commit 7bcf8987).

src/u4.cpp
@@ -158,6 +158,9 @@ void servicesInit(XU4GameServices* gs, Options* opt) {
     if (opt->filter)
         gs->settings->filter = opt->filter;
 
+    if (opt->flags & OPT_VERBOSE)
+        verbose = true;
+
     Debug::initGlobal("debug/global.txt");
 
     xu4_srandom();