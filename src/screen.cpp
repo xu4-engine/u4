xu4-engine/u4

commit 9d1669e5715d08d50ade3ab1082b2d39d20468fd

Move global ImageMgr pointer to XU4GameServices.

src/screen.cpp
@@ -81,8 +81,6 @@ int screenLos[VIEWPORT_W][VIEWPORT_H];
 
 static const int BufferSize = 1024;
 
-ImageMgr* imageMgr = NULL;
-
 extern bool verbose;
 
 // Just extern the system functions here. That way people aren't tempted to call them as part of the public API.
@@ -98,9 +96,7 @@ static void screenInit_data(Settings& settings) {
     xu4.screenImage = Image::create(320 * settings.scale, 200 * settings.scale);
     xu4.screenImage->fill(RGBA(0, 0, 0, 255));
 
-    imageMgr = new ImageMgr;
-
-    charsetInfo = imageMgr->get(BKGD_CHARSET);
+    charsetInfo = xu4.imageMgr->get(BKGD_CHARSET);
     if (!charsetInfo)
         errorFatal("ERROR 1001: Unable to load the \"%s\" data file.\t\n\nIs %s installed?\n\nVisit the XU4 website for additional information.\n\thttp://xu4.sourceforge.net/", BKGD_CHARSET, settings.game.c_str());
 
@@ -151,9 +147,6 @@ static void screenDelete_data() {
     for (i = layouts.begin(); i != layouts.end(); i++)
         delete(*i);
     layouts.clear();
-
-    delete imageMgr;
-    imageMgr = NULL;
 
     delete xu4.screenImage;
     xu4.screenImage = NULL;
@@ -499,15 +492,15 @@ void screenUpdate(TileView *view, bool showmap, bool blackout) {
  * Draw an image or subimage on the screen.
  */
 void screenDrawImage(const string &name, int x, int y) {
-    ImageInfo *info = imageMgr->get(name);
+    ImageInfo *info = xu4.imageMgr->get(name);
     if (info) {
         info->image->draw(x, y);
         return;
     }
 
-    SubImage *subimage = imageMgr->getSubImage(name);
+    SubImage *subimage = xu4.imageMgr->getSubImage(name);
     if (subimage)
-        info = imageMgr->get(subimage->srcImageName);
+        info = xu4.imageMgr->get(subimage->srcImageName);
 
     if (info) {
         if (info) {
@@ -526,7 +519,7 @@ void screenDrawImage(const string &name, int x, int y) {
 void screenDrawImageInMapArea(const string &name) {
     ImageInfo *info;
 
-    info = imageMgr->get(name);
+    info = xu4.imageMgr->get(name);
     if (!info)
         errorFatal("ERROR 1004: Unable to load data files.\t\n\nIs %s installed?\n\nVisit the XU4 website for additional information.\n\thttp://xu4.sourceforge.net/", xu4.settings->game.c_str());
 
@@ -1174,7 +1167,7 @@ static void screenShowGemTile(Layout *layout, Map *map, MapTile &t, bool focus, 
     }
     else {
         if (gemTilesInfo == NULL) {
-            gemTilesInfo = imageMgr->get(BKGD_GEMTILES);
+            gemTilesInfo = xu4.imageMgr->get(BKGD_GEMTILES);
             if (!gemTilesInfo)
                 errorFatal("ERROR 1002: Unable to load the \"%s\" data file.\t\n\nIs %s installed?\n\nVisit the XU4 website for additional information.\n\thttp://xu4.sourceforge.net/", BKGD_GEMTILES, xu4.settings->game.c_str());
         }

src/u4.cpp
@@ -17,6 +17,7 @@
 #include "error.h"
 #include "event.h"
 #include "game.h"
+#include "imagemgr.h"
 #include "intro.h"
 #include "mapmgr.h"
 #include "person.h"
@@ -162,6 +163,7 @@ void servicesInit(XU4GameServices* gs, Options* opt) {
 
     xu4_srandom();
     gs->config = configInit();
+    gs->imageMgr = new ImageMgr;
     screenInit();
 
     if (! (opt->flags & OPT_NO_AUDIO))
@@ -188,6 +190,7 @@ void servicesFree(XU4GameServices* gs) {
     delete gs->eventHandler;
     soundDelete();
     screenDelete();
+    delete gs->imageMgr;
     configFree(gs->config);
     delete gs->settings;
     u4fcleanup();

src/dungeonview.cpp
@@ -410,7 +410,7 @@ void DungeonView::drawWall(int xoffset, int distance, Direction orientation, Dun
         return;
 
     int x = 0, y = 0;
-    SubImage *subimage = imageMgr->getSubImage(dngGraphicInfo[index].subimage);
+    SubImage *subimage = xu4.imageMgr->getSubImage(dngGraphicInfo[index].subimage);
     if (subimage) {
         x = subimage->x;
         y = subimage->y;

src/game.cpp
@@ -187,7 +187,7 @@ void GameController::initScreen()
 void GameController::initScreenWithoutReloadingState()
 {
     musicPlayLocale();
-    imageMgr->get(BKGD_BORDERS)->image->draw(0, 0);
+    xu4.imageMgr->get(BKGD_BORDERS)->image->draw(0, 0);
     c->stats->update(); /* draw the party stats */
 
     screenMessage("Press Alt-h for help\n");

src/imageview.cpp
@@ -20,15 +20,15 @@ ImageView::~ImageView() {
  * Draw the image at the optionally specified offset.
  */
 void ImageView::draw(const string &imageName, int x, int y) {
-    ImageInfo *info = imageMgr->get(imageName);
+    ImageInfo *info = xu4.imageMgr->get(imageName);
     if (info) {
         info->image->draw(SCALED(this->x + x), SCALED(this->y + y));
         return;
     }
 
-    SubImage *subimage = imageMgr->getSubImage(imageName);
+    SubImage *subimage = xu4.imageMgr->getSubImage(imageName);
     if (subimage) {
-        info = imageMgr->get(subimage->srcImageName);
+        info = xu4.imageMgr->get(subimage->srcImageName);
 
         if (info) {
             info->image->drawSubRect(SCALED(this->x + x), SCALED(this->y + y),

src/intro.cpp
@@ -354,7 +354,7 @@ void IntroController::deleteIntro() {
     delete [] objectStateTable;
     objectStateTable = NULL;
 
-    imageMgr->freeIntroBackgrounds();
+    xu4.imageMgr->freeIntroBackgrounds();
 }
 
 unsigned char *IntroController::getSigData() {
@@ -1585,7 +1585,7 @@ void IntroController::getTitleSourceData()
     // here, and elements of the image will be stored
     // individually.  Afterward, the BKGD_INTRO image
     // will be scaled appropriately.
-    ImageInfo *info = imageMgr->get(BKGD_INTRO, true);
+    ImageInfo *info = xu4.imageMgr->get(BKGD_INTRO, true);
     if (!info) {
         errorFatal("ERROR 1007: Unable to load the image \"%s\".\t\n\nIs %s installed?\n\nVisit the XU4 website for additional information.\n\thttp://xu4.sourceforge.net/", BKGD_INTRO, xu4.settings->game.c_str());
     }

src/textview.cpp
@@ -24,7 +24,7 @@ TextView::TextView(int x, int y, int columns, int rows) : View(x, y, columns * C
     this->cursorY = 0;
     this->cursorPhase = 0;
     if (charset == NULL)
-        charset = imageMgr->get(BKGD_CHARSET)->image;
+        charset = xu4.imageMgr->get(BKGD_CHARSET)->image;
     xu4.eventHandler->getTimer()->add(&cursorTimer, /*SCR_CYCLE_PER_SECOND*/4, this);
 }
 
@@ -34,7 +34,7 @@ TextView::~TextView() {
 
 void TextView::reinit() {
     View::reinit();
-    charset = imageMgr->get(BKGD_CHARSET)->image;
+    charset = xu4.imageMgr->get(BKGD_CHARSET)->image;
 }
 
 /**

src/tile.cpp
@@ -127,11 +127,11 @@ void Tile::loadImage() {
 
         SubImage *subimage = NULL;
 
-        ImageInfo *info = imageMgr->get(imageName);
+        ImageInfo *info = xu4.imageMgr->get(imageName);
         if (!info) {
-            subimage = imageMgr->getSubImage(imageName);
+            subimage = xu4.imageMgr->getSubImage(imageName);
             if (subimage)
-                info = imageMgr->get(subimage->srcImageName);
+                info = xu4.imageMgr->get(subimage->srcImageName);
         }
         if (!info) //IF still no info loaded
         {

commit 83eaebe73c87ef96fc1c9a6e41cac495ae76d41f

Fix the garbage characters shown after the video scale is changed.
This was broken in commit 9d1669e5 where the ImageMgr reconstruction
required by screenReInit() was optimized away.

src/screen.cpp
@@ -79,6 +79,8 @@ static void screenInit_data(Settings& settings) {
     xu4.screenImage = Image::create(320 * settings.scale, 200 * settings.scale);
     xu4.screenImage->fill(Image::black);
 
+    xu4.imageMgr = new ImageMgr;
+
     charsetInfo = xu4.imageMgr->get(BKGD_CHARSET);
     if (!charsetInfo)
         errorFatal("ERROR 1001: Unable to load the \"%s\" data file.\t\n\nIs %s installed?\n\nVisit the XU4 website for additional information.\n\thttp://xu4.sourceforge.net/", BKGD_CHARSET, settings.game.c_str());
@@ -136,6 +138,9 @@ static void screenDelete_data() {
 
     delete tileanims;
     tileanims = NULL;
+
+    delete xu4.imageMgr;
+    xu4.imageMgr = NULL;
 
     delete xu4.screenImage;
     xu4.screenImage = NULL;

src/u4.cpp
@@ -175,7 +175,6 @@ void servicesInit(XU4GameServices* gs, Options* opt) {
 
     xu4_srandom();
     gs->config = configInit();
-    gs->imageMgr = new ImageMgr;
     screenInit();
     Tile::initSymbols(gs->config);
 
@@ -193,7 +192,6 @@ void servicesFree(XU4GameServices* gs) {
     delete gs->eventHandler;
     soundDelete();
     screenDelete();
-    delete gs->imageMgr;
     configFree(gs->config);
     delete gs->settings;
     u4fcleanup();