xu4-engine/u4

commit 51f9838d72666fb117353d7810efc89d04eb4eff

Remove hard-coded TileIds from dungeon map id conversion.
Make dngMapToModule() & moduleToDngMap() members of UltimaSaveIds.

src/savegame.cpp
@@ -399,14 +399,35 @@ int saveGameMonstersRead(SaveGameMonsterRecord *monsterTable, FILE *f) {
 
 //--------------------------------------
 
-void UltimaSaveIds::alloc(int ucount, int mcount) {
-    // Use one block of memory for both tables.
-    size_t msize = ucount * sizeof(TileId);
+#include "config.h"
+#include "tile.h"
+#include "tileset.h"
+
+static const int dngTableLen = 16+4;    // DungeonToken + Magic fields
+
+void UltimaSaveIds::alloc(int ucount, int mcount,
+                          Config* cfg, const Tileset* tiles) {
+    // Use one block of memory for all tables.
+    size_t msize = (ucount + dngTableLen) * sizeof(TileId);
     moduleIdTable = (TileId*) malloc( msize + mcount );
+    moduleIdDngTable = moduleIdTable + ucount;
     ultimaIdTable = ((uint8_t*) moduleIdTable) + msize;
 
     uiCount = ucount;
     miCount = mcount;
+
+    // Fill moduleIdDngTable to match DNGMAP.SAV values.
+    Symbol dngMapSymbols[dngTableLen];
+    cfg->internSymbols(dngMapSymbols, dngTableLen,
+        "brick_floor up_ladder down_ladder up_down_ladder\n"
+        "chest ceiling_hole floor_hole magic_orb\n"
+        "ceiling_hole fountain poison_field dungeon_altar\n"
+        "dungeon_door dungeon_room secret_door brick_wall\n"
+        "poison_field energy_field fire_field sleep_field\n");
+    for (int i = 0; i < dngTableLen; ++i) {
+        const Tile* tile = tiles->getByName(dngMapSymbols[i]);
+        moduleIdDngTable[i] = tile ? tile->id : 0;
+    }
 }
 
 void UltimaSaveIds::free() {
@@ -453,4 +474,32 @@ uint8_t UltimaSaveIds::ultimaId(const MapTile& tile) const {
         return uid;
 
     return uidF;
+}
+
+/*
+ * Convert DNGMAP.SAV encoded map values to their matching module id.
+ */
+TileId UltimaSaveIds::dngMapToModule(uint8_t u4DngId) const {
+    int i = u4DngId >> 4;
+    if (i == 0xA)
+        i = 16 + (u4DngId & 3);         // Magic fields at end of table.
+    return moduleIdDngTable[i];
+}
+
+/*
+ * Convert module TileIds to DNGMAP.SAV encoded map values.
+ * NOTE: This does not handle traps (0x8?), fountains (0x9?), & rooms (0xD?)!
+ * Return zero for any non-dungeon or unhandled tiles.
+ */
+uint8_t UltimaSaveIds::moduleToDngMap(TileId modId) const {
+    int i;
+    for (i = 0; i < dngTableLen; ++i) {
+        if (modId == moduleIdDngTable[i]) {
+            if (i < 16)
+                return i << 4;
+            else
+                return 0xA0 + (i - 16); // Magic fields at end of table.
+        }
+    }
+    return 0;
 }

src/config.cpp
@@ -377,13 +377,13 @@ static void conf_tilesetLoad(ConfigXML* cfg, Tileset* ts, const ConfigElement& c
 }
 
 static void conf_ultimaSaveIds(ConfigXML* cfg, UltimaSaveIds* usaveIds, Tileset* ts, const ConfigElement &conf) {
+    vector<ConfigElement> children = conf.getChildren();
+    vector<ConfigElement>::iterator it;
     int frames;
     int uid = 0;
 
-    usaveIds->alloc(256, 135);
+    usaveIds->alloc(256, children.size(), cfg, ts);
 
-    vector<ConfigElement> children = conf.getChildren();
-    vector<ConfigElement>::iterator it;
     foreach (it, children) {
         if (it->getName() != "mapping")
             continue;

src/dungeon.cpp
@@ -6,6 +6,7 @@
 #include "dungeon.h"
 
 #include "annotation.h"
+#include "config.h"
 #include "context.h"
 #include "debug.h"
 #include "game.h"
@@ -17,6 +18,7 @@
 #include "stats.h"
 #include "tileset.h"
 #include "utils.h"
+#include "xu4.h"
 
 /**
  * Returns true if 'map' points to a dungeon map
@@ -333,11 +335,10 @@ static int u4DngMonster(CreatureId cid) {
     return 0;
 }
 
-extern int moduleToDngMap(TileId modId);
-
 uint8_t* Dungeon::fillRawMap() {
     uint32_t x, y, z;
     const MapTile* mt;
+    const UltimaSaveIds* usaveIds = xu4.config->usaveIds();
     uint8_t* dp = (uint8_t*) &rawMap.front();
     int uid, dngId;
 
@@ -354,7 +355,7 @@ uint8_t* Dungeon::fillRawMap() {
                 }
 
                 mt = getTileFromData(MapCoords(x, y, z));
-                dngId = moduleToDngMap(mt->id);
+                dngId = usaveIds->moduleToDngMap(mt->id);
                 //printf("KR %d,%d,%d %d => %d\n", x, y, z, mt->id, dngId);
 
                 // Add the creature to the tile

src/maploader.cpp
@@ -276,48 +276,6 @@ static void initDungeonRoom(Dungeon *dng, int room) {
     cmap->tileset = xu4.config->tileset();
 }
 
-static const uint8_t ultima4Dng_to_module[16] = {
-     46,  // 0x00 brick_floor
-     22,  // 0x10 up_ladder
-     23,  // 0x20 down_ladder
-    135,  // 0x30 up_down_ladder
-     44,  // 0x40 chest
-    136,  // 0x50 ceiling_hole
-    137,  // 0x60 floor_hole
-    138,  // 0x70 magic_orb
-    136,  // 0x80 (trap) ceiling_hole
-    139,  // 0x90 fountain
-     50,  // 0xA0 poison_field
-    142,  // 0xB0 dungeon_altar
-    141,  // 0xC0 dungeon_door
-    140,  // 0xD0 dungeon_room
-     55,  // 0xE0 secret_door
-     99   // 0xF0 brick_wall
-};
-
-static TileId dngMapToModule(int u4DngId) {
-    TileId mid = ultima4Dng_to_module[ u4DngId >> 4 ];
-    if (mid == 50)
-        mid += u4DngId & 0x3;   // Magic fields.
-    return mid;
-}
-
-// NOTE: This does not handle traps, fountains, & rooms!
-int moduleToDngMap(TileId modId) {
-    int i;
-    for (i = 0; i < 16; ++i) {
-        if (modId == ultima4Dng_to_module[i])
-            return i << 4;
-    }
-    // Magic fields.
-    switch (modId) {
-        case 51: return 0xA1;
-        case 52: return 0xA2;
-        case 53: return 0xA3;
-    }
-    return 0;
-}
-
 /**
  * Loads a dungeon map from the 'dng' file (and dngmap.sav if provided).
  */
@@ -347,7 +305,7 @@ static bool loadDungeonMap(Map *map, U4FILE *uf, FILE *sav) {
 
     for (i = 0; i < bytes; i++) {
         j = *rawMap++;
-        MapTile tile(dngMapToModule(j), 0);
+        MapTile tile(usaveIds->dngMapToModule(j), 0);
         dungeon->data.push_back(tile);
         //printf( "KR dng tile %d: %d => %d\n", i, j, tile.id);
     }

src/savegame.h
@@ -247,9 +247,13 @@ struct SaveGame {
 int saveGameMonstersWrite(const SaveGameMonsterRecord *monsterTable, FILE *f);
 int saveGameMonstersRead(SaveGameMonsterRecord *monsterTable, FILE *f);
 
+class Config;
+class Tileset;
+
 struct UltimaSaveIds {
     uint8_t* ultimaIdTable;
     TileId*  moduleIdTable;
+    TileId*  moduleIdDngTable;
     uint16_t uiCount;       // Number of entries in moduleIdTable.
     uint16_t miCount;       // Number of entries in ultimaIdTable.
 
@@ -257,7 +261,10 @@ struct UltimaSaveIds {
     MapTile moduleId(uint8_t uid) const;
     uint8_t ultimaId(const MapTile& tile) const;
 
-    void alloc(int ucount, int mcount);
+    TileId  dngMapToModule(uint8_t u4DngId) const;
+    uint8_t moduleToDngMap(TileId modId) const;
+
+    void alloc(int ucount, int mcount, Config* cfg, const Tileset* tiles);
     void addId(uint8_t uid, int frames, TileId);
     void free();
 };

commit b846312cb9a6854ec3837717e03706108c0fcdaf

Fix build of dumpsavegame utility.
This was broken in commit 51f9838d7 when UltimaSaveIds became dependent
on Config & Tileset.

project.b
@@ -143,7 +143,6 @@ if make_util [
 		console
 		include_from %src
 		sources [
-			%src/savegame.cpp
 			%src/util/dumpsavegame.cpp
 		]
 	]

src/Makefile.common
@@ -95,7 +95,7 @@ coord$(EXEEXT): util/coord.o
 dumpmap$(EXEEXT) : util/dumpmap.o
 	$(CC) $(CFLAGS) -o $@ $+
 
-dumpsavegame$(EXEEXT) : util/dumpsavegame.o savegame.o
+dumpsavegame$(EXEEXT) : util/dumpsavegame.o
 	$(CXX) $(CXXFLAGS) $(LDFLAGS) -o $@ $+
 
 tlkconv$(EXEEXT) : util/tlkconv.o

src/savegame.cpp
@@ -399,6 +399,7 @@ int saveGameMonstersRead(SaveGameMonsterRecord *monsterTable, FILE *f) {
 
 //--------------------------------------
 
+#ifndef SAVE_UTIL
 #include "config.h"
 #include "tile.h"
 #include "tileset.h"
@@ -503,3 +504,4 @@ uint8_t UltimaSaveIds::moduleToDngMap(TileId modId) const {
     }
     return 0;
 }
+#endif

src/util/dumpsavegame.cpp
@@ -4,7 +4,8 @@
 #include <cstdlib>
 #include <cstring>
 
-#include "savegame.h"
+#define SAVE_UTIL
+#include "savegame.cpp"
 
 #define EX_USAGE     64  /* command line usage error */
 #define EX_NOINPUT   66  /* cannot open input */