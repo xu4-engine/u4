xu4-engine/u4

commit e130fd0d4a05b6881fc61053018df0805a33cb4d

Map: The prevTile of objects is no longer modified during save.
 - Remove Map::resetObjectAnimations().
 - Remove Map::monsterTable.  This is now a temporary area of memory.
 - Make fillMonsterTable(), getTileFromData(), & tileAt() const methods.
 - Add const version of objectAt().

src/map.cpp
@@ -320,13 +320,13 @@ void Map::queryVisible(const Coords& center, int radius,
  * Returns the object at the given (x,y,z) coords, if one exists.
  * Otherwise, returns NULL.
  */
-Object *Map::objectAt(const Coords &coords) {
+const Object *Map::objectAt(const Coords &coords) const {
     /* FIXME: return a list instead of one object */
     ObjectDeque::const_iterator i;
-    Object *objAt = NULL;
+    const Object *objAt = NULL;
 
     for(i = objects.begin(); i != objects.end(); i++) {
-        Object *obj = *i;
+        const Object *obj = *i;
 
         if (obj->getCoords() == coords) {
             /* get the most visible object */
@@ -361,7 +361,7 @@ const Portal *Map::portalAt(const Coords &coords, int actionFlags) {
 /**
  * Returns the raw tile for the given (x,y,z) coords for the given map
  */
-MapTile *Map::getTileFromData(const Coords &coords) {
+const MapTile *Map::getTileFromData(const Coords &coords) const {
     static MapTile blank(0);
 
     if (MAP_IS_OOB(this, coords))
@@ -376,12 +376,12 @@ MapTile *Map::getTileFromData(const Coords &coords) {
  * annotations like moongates and attack icons are ignored.  Any walkable tiles
  * are taken into account (treasure chests, ships, balloon, etc.)
  */
-MapTile *Map::tileAt(const Coords &coords, int withObjects) {
+const MapTile *Map::tileAt(const Coords &coords, int withObjects) const {
     /* FIXME: this should return a list of tiles, with the most visible at the front */
-    MapTile *tile;
+    const MapTile *tile;
     std::list<Annotation *> a = annotations->ptrsToAllAt(coords);
     std::list<Annotation *>::iterator i;
-    Object *obj = objectAt(coords);
+    const Object *obj = objectAt(coords);
 
     tile = getTileFromData(coords);
 
@@ -404,7 +404,7 @@ MapTile *Map::tileAt(const Coords &coords, int withObjects) {
 }
 
 const Tile *Map::tileTypeAt(const Coords &coords, int withObjects) {
-    MapTile *tile = tileAt(coords, withObjects);
+    const MapTile *tile = tileAt(coords, withObjects);
     return tile->getTileType();
 }
 
@@ -595,21 +595,6 @@ Creature *Map::moveObjects(MapCoords avatar) {
     }
 
     return attacker;
-}
-
-/**
- * Resets object animations to a value that is acceptable for
- * savegame compatibility with u4dos.
- */
-void Map::resetObjectAnimations() {
-    ObjectDeque::iterator i;
-
-    for (i = objects.begin(); i != objects.end(); i++) {
-        Object *obj = *i;
-
-        if (obj->getType() == Object::CREATURE)
-            obj->setPrevTile(xu4.creatureMgr->getByTile(obj->getTile())->getTile());
-    }
 }
 
 /**
@@ -820,19 +805,18 @@ const MapCoords &Map::getLabel(const string &name) const {
     return i->second;
 }
 
-bool Map::fillMonsterTable() {
-    ObjectDeque::iterator current;
-    Object *obj;
-    ObjectDeque monsters;
-    ObjectDeque other_creatures;
-    ObjectDeque inanimate_objects;
+bool Map::fillMonsterTable(SaveGameMonsterRecord* table) const {
+    ObjectDeque::const_iterator current;
+    const Object *obj;
+    CObjectDeque monsters;
+    CObjectDeque other_creatures;
+    CObjectDeque inanimate_objects;
     Object empty;
 
     int nCreatures = 0;
     int nObjects = 0;
     int i;
 
-    memset(monsterTable, 0, MONSTERTABLE_SIZE * sizeof(SaveGameMonsterRecord));
 
     /**
      * First, categorize all the objects we have
@@ -841,14 +825,16 @@ bool Map::fillMonsterTable() {
         obj = *current;
 
         /* moving objects first */
-        if ((obj->getType() == Object::CREATURE) && (obj->getMovementBehavior() != MOVEMENT_FIXED)) {
-            Creature *c = dynamic_cast<Creature*>(obj);
+        if ((obj->getType() == Object::CREATURE) &&
+            (obj->getMovementBehavior() != MOVEMENT_FIXED)) {
+            const Creature *c = dynamic_cast<const Creature*>(obj);
             /* whirlpools and storms are separated from other moving objects */
             if (c->getId() == WHIRLPOOL_ID || c->getId() == STORM_ID)
                 monsters.push_back(obj);
-            else other_creatures.push_back(obj);
-        }
-        else inanimate_objects.push_back(obj);
+            else
+                other_creatures.push_back(obj);
+        } else
+            inanimate_objects.push_back(obj);
     }
 
     /**
@@ -884,14 +870,20 @@ bool Map::fillMonsterTable() {
      */
     for (i = 0; i < MONSTERTABLE_SIZE; i++) {
         Coords c = monsters[i]->getCoords(),
-               prevc = monsters[i]->getPrevCoords();
+           prevc = monsters[i]->getPrevCoords();
 
-        monsterTable[i].tile = translateToRawTileIndex(monsters[i]->getTile());
-        monsterTable[i].x = c.x;
-        monsterTable[i].y = c.y;
-        monsterTable[i].prevTile = translateToRawTileIndex(monsters[i]->getPrevTile());
-        monsterTable[i].prevx = prevc.x;
-        monsterTable[i].prevy = prevc.y;
+        // Reset animation to a value that is savegame compatible with u4dos.
+        MapTile prevTile = monsters[i]->getPrevTile();
+        prevTile.frame = 0;
+
+        table[i].tile = translateToRawTileIndex(monsters[i]->getTile());
+        table[i].x = c.x;
+        table[i].y = c.y;
+        table[i].prevTile = translateToRawTileIndex(prevTile);
+        table[i].prevx = prevc.x;
+        table[i].prevy = prevc.y;
+        table[i].unused1 =
+        table[i].unused2 = 0;
     }
 
     return true;
@@ -903,6 +895,6 @@ MapTile Map::translateFromRawTileIndex(int raw) const {
     return tilemap->translate(raw);
 }
 
-unsigned int Map::translateToRawTileIndex(MapTile &tile) const {
+unsigned int Map::translateToRawTileIndex(const MapTile &tile) const {
     return tilemap->untranslate(tile);
 }

src/cheat.cpp
@@ -277,7 +277,7 @@ bool CheatMenuController::keyPressed(int key) {
                 coords.move(readDir.waitFor(), c->location->map);
                 if (coords != c->location->coords) {
                     bool ok = false;
-                    MapTile *ground = c->location->map->tileAt(coords, WITHOUT_OBJECTS);
+                    const MapTile *ground = c->location->map->tileAt(coords, WITHOUT_OBJECTS);
 
                     screenMessage("%s\n", getDirectionName(readDir.getValue()));
 

src/dungeon.cpp
@@ -316,6 +316,6 @@ bool Dungeon::ladderDownAt(MapCoords coords) {
 }
 
 bool Dungeon::validTeleportLocation(MapCoords coords) {
-    MapTile *tile = tileAt(coords, WITH_OBJECTS);
+    const MapTile *tile = tileAt(coords, WITH_OBJECTS);
     return tokenForTile(*tile) == DUNGEON_CORRIDOR;
 }

src/game.cpp
@@ -99,7 +99,7 @@ void gameLordBritishCheckLevels(void);
 
 /* creature functions */
 void gameDestroyAllCreatures(void);
-void gameFixupObjects(Map *map);
+void gameFixupObjects(Map *map, const SaveGameMonsterRecord* table);
 void gameCreatureAttack(Creature *obj);
 
 /* Functions END */
@@ -199,10 +199,22 @@ void GameController::initScreenWithoutReloadingState()
 }
 
 
+class MonstersSav {
+public:
+    MonstersSav() {
+        table = new SaveGameMonsterRecord[MONSTERTABLE_SIZE];
+    }
+    ~MonstersSav() {
+        delete[] table;
+    }
+    SaveGameMonsterRecord* table;
+};
+
 void GameController::init() {
     Debug gameDbg("debug/game.txt", "Game");
-    FILE *saveGameFile, *monstersFile;
+    FILE *fp;
     const Settings& settings = *xu4.settings;
+    MonstersSav mons;
 
     TRACE(gameDbg, "gameInit() running.");
 
@@ -236,10 +248,10 @@ void GameController::init() {
     c->lastShip = NULL;
 
     /* load in the save game */
-    saveGameFile = fopen((settings.getUserPath() + PARTY_SAV).c_str(), "rb");
-    if (saveGameFile) {
-        c->saveGame->read(saveGameFile);
-        fclose(saveGameFile);
+    fp = fopen((settings.getUserPath() + PARTY_SAV).c_str(), "rb");
+    if (fp) {
+        c->saveGame->read(fp);
+        fclose(fp);
     } else
         errorFatal("no savegame found!");
 
@@ -274,7 +286,9 @@ void GameController::init() {
         c->location->coords = MapCoords(c->saveGame->x, c->saveGame->y, c->saveGame->dnglevel);
         c->location->prev->coords = MapCoords(c->saveGame->dngx, c->saveGame->dngy);
     }
-    else c->location->coords = MapCoords(c->saveGame->x, c->saveGame->y, (int)c->saveGame->dnglevel);
+    else
+        c->location->coords = MapCoords(c->saveGame->x, c->saveGame->y, (int)c->saveGame->dnglevel);
+
     c->saveGame->orientation = (Direction)(c->saveGame->orientation + DIR_WEST);
 
     /**
@@ -289,21 +303,21 @@ void GameController::init() {
     TRACE_LOCAL(gameDbg, "Loading monsters."); ++pb;
 
     /* load in monsters.sav */
-    monstersFile = fopen((settings.getUserPath() + MONSTERS_SAV).c_str(), "rb");
-    if (monstersFile) {
-        saveGameMonstersRead(c->location->map->monsterTable, monstersFile);
-        fclose(monstersFile);
+    fp = fopen((settings.getUserPath() + MONSTERS_SAV).c_str(), "rb");
+    if (fp) {
+        saveGameMonstersRead(mons.table, fp);
+        fclose(fp);
+        gameFixupObjects(c->location->map, mons.table);
     }
-    gameFixupObjects(c->location->map);
 
     /* we have previous creature information as well, load it! */
     if (c->location->prev) {
-        monstersFile = fopen((settings.getUserPath() + OUTMONST_SAV).c_str(), "rb");
-        if (monstersFile) {
-            saveGameMonstersRead(c->location->prev->map->monsterTable, monstersFile);
-            fclose(monstersFile);
+        fp = fopen((settings.getUserPath() + OUTMONST_SAV).c_str(), "rb");
+        if (fp) {
+            saveGameMonstersRead(mons.table, fp);
+            fclose(fp);
+            gameFixupObjects(c->location->prev->map, mons.table);
         }
-        gameFixupObjects(c->location->prev->map);
     }
 
     spellSetEffectCallback(&gameSpellEffect);
@@ -339,8 +353,9 @@ int gameSave(const char* userPath) {
     size_t userPathLen = sbuf.size();
     const char* basename;
     const Location* loc = c->location;
-    Map* map = loc->map;
+    const Map* map = loc->map;
     SaveGame save = *c->saveGame;
+    MonstersSav mons;
 
 #define openSaveFile(FN) \
     basename = FN; \
@@ -382,12 +397,8 @@ int gameSave(const char* userPath) {
 
 
     openSaveFile(MONSTERS_SAV);
-
-    /* fix creature animations so they are compatible with u4dos */
-    map->resetObjectAnimations();
-    map->fillMonsterTable(); /* fill the monster table so we can save it */
-
-    if (! saveGameMonstersWrite(map->monsterTable, fp))
+    map->fillMonsterTable(mons.table);
+    if (! saveGameMonstersWrite(mons.table, fp))
         goto write_error;
     fclose(fp);
 
@@ -428,13 +439,13 @@ int gameSave(const char* userPath) {
                 for (x = 0; x < map->width; x++) {
                     mt = map->getTileFromData(MapCoords(x, y, z));
                     int tile = moduleToDngMap(mt->id);
-                    Object *obj = map->objectAt(MapCoords(x, y, z));
+                    const Object *obj = map->objectAt(MapCoords(x, y, z));
 
                     /**
                      * Add the creature to the tile
                      */
                     if (obj && obj->getType() == Object::CREATURE) {
-                        const Creature *m = dynamic_cast<Creature*>(obj);
+                        const Creature *m = dynamic_cast<const Creature*>(obj);
                         DngCreatureIdMap::iterator m_id = id_map.find(m);
                         if (m_id != id_map.end())
                             tile |= m_id->second;
@@ -450,12 +461,8 @@ int gameSave(const char* userPath) {
 
 
         openSaveFile(OUTMONST_SAV);
-
-        /* fix creature animations so they are compatible with u4dos */
-        loc->prev->map->resetObjectAnimations();
-        loc->prev->map->fillMonsterTable(); /* fill the monster table so we can save it */
-
-        if (! saveGameMonstersWrite(loc->prev->map->monsterTable, fp))
+        loc->prev->map->fillMonsterTable(mons.table);
+        if (! saveGameMonstersWrite(mons.table, fp))
             goto write_error;
         fclose(fp);
     }
@@ -819,7 +826,7 @@ bool GameController::keyPressed(int key) {
     bool valid = true;
     int endTurn = 1;
     Object *obj;
-    MapTile *tile;
+    const MapTile *tile;
 
     /* Translate context-sensitive action key into a useful command */
     if (key == U4_ENTER && settings.enhancements && settings.enhancementsOptions.smartEnterKey) {
@@ -2221,7 +2228,7 @@ void GameController::avatarMoved(MoveEvent &event) {
             /* if shortcuts are enabled, try them! */
             if (xu4.settings->shortcutCommands) {
                 MapCoords new_coords = c->location->coords;
-                MapTile *tile;
+                const MapTile *tile;
 
                 new_coords.move(event.dir, c->location->map);
                 tile = c->location->map->tileAt(new_coords, WITH_OBJECTS);
@@ -2350,7 +2357,7 @@ void jimmy() {
  * tile.
  */
 bool jimmyAt(const Coords &coords) {
-    MapTile *tile = c->location->map->tileAt(coords, WITH_OBJECTS);
+    const MapTile *tile = c->location->map->tileAt(coords, WITH_OBJECTS);
 
     if (!tile->getTileType()->isLockedDoor())
         return false;
@@ -3123,13 +3130,14 @@ bool GameController::checkMoongates() {
  * Fixes objects initially loaded by saveGameMonstersRead,
  * and alters movement behavior accordingly to match the creature
  */
-void gameFixupObjects(Map *map) {
+void gameFixupObjects(Map *map, const SaveGameMonsterRecord* table) {
     int i;
     Object *obj;
+    const SaveGameMonsterRecord *monster;
 
     /* add stuff from the monster table to the map */
     for (i = 0; i < MONSTERTABLE_SIZE; i++) {
-        SaveGameMonsterRecord *monster = &map->monsterTable[i];
+        monster = table + i;
         if (monster->prevTile != 0) {
             Coords coords(monster->x, monster->y);
 
@@ -3167,7 +3175,7 @@ time_t gameTimeSinceLastCommand() {
  * Handles what happens when a creature attacks you
  */
 void gameCreatureAttack(Creature *m) {
-    Object *under;
+    const Object *under;
     const Tile *ground;
 
     screenMessage("\nAttacked by %s\n", m->getName().c_str());

src/map.h
@@ -99,10 +99,13 @@ public:
     void queryVisible(const Coords &coords, int radius,
                       void (*func)(const Coords*, VisualId, void*),
                       void* user) const;
-    class Object *objectAt(const Coords &coords);
+    const Object* objectAt(const Coords &coords) const;
+    Object* objectAt(const Coords &coords) {
+        return (Object*) static_cast<const Map*>(this)->objectAt(coords);
+    }
     const Portal *portalAt(const Coords &coords, int actionFlags);
-    MapTile* getTileFromData(const Coords &coords);
-    MapTile* tileAt(const Coords &coords, int withObjects);
+    const MapTile* getTileFromData(const Coords &coords) const;
+    const MapTile* tileAt(const Coords &coords, int withObjects) const;
     const Tile *tileTypeAt(const Coords &coords, int withObjects);
     bool isWorldMap();
     bool isEnclosed(const Coords &party);
@@ -113,7 +116,6 @@ public:
     ObjectDeque::iterator removeObject(ObjectDeque::iterator rem, bool deleteObject = true);
     void clearObjects();
     class Creature *moveObjects(MapCoords avatar);
-    void resetObjectAnimations();
     int getNumberOfCreatures();
     int getValidMoves(const MapCoords& from, MapTile transport);
     bool move(Object *obj, Direction d);
@@ -121,9 +123,9 @@ public:
     const MapCoords &getLabel(const string &name) const;
 
     // u4dos compatibility
-    bool fillMonsterTable();
+    bool fillMonsterTable(SaveGameMonsterRecord* table) const;
     MapTile translateFromRawTileIndex(int c) const;
-    unsigned int translateToRawTileIndex(MapTile &tile) const;
+    unsigned int translateToRawTileIndex(const MapTile &tile) const;
 
 public:
     MapId           id;
@@ -151,9 +153,6 @@ public:
     std::map<string, MapCoords> labels;
     Tileset        *tileset;
     TileMap        *tilemap;
-
-    // u4dos compatibility
-    SaveGameMonsterRecord monsterTable[MONSTERTABLE_SIZE];
 
 private:
     // disallow map copying: all maps should be created and accessed

src/movement.cpp
@@ -119,7 +119,7 @@ void moveAvatarInDungeon(MoveEvent &event) {
     Direction realDir = dirNormalize((Direction)c->saveGame->orientation, event.dir); /* get our real direction */
     int advancing = realDir == c->saveGame->orientation,
         retreating = realDir == dirReverse((Direction)c->saveGame->orientation);
-    MapTile *tile;
+    const MapTile *tile;
 
     /* we're not in a dungeon, failed! */
     ASSERT(c->location->context & CTX_DUNGEON, "moveAvatarInDungeon() called outside of dungeon, failed!");

src/object.h
@@ -10,8 +10,6 @@
 #include "tile.h"
 #include "types.h"
 
-typedef std::deque<class Object *> ObjectDeque;
-
 typedef enum {
     MOVEMENT_FIXED,
     MOVEMENT_WANDER,
@@ -41,7 +39,8 @@ public:
 
     // Methods
     MapTile& getTile()                      { return tile; }
-    MapTile& getPrevTile()                  { return prevTile; }
+    const MapTile& getTile() const          { return tile; }
+    const MapTile& getPrevTile() const      { return prevTile; }
     const Coords& getCoords() const         { return coords; }
     const Coords& getPrevCoords() const     { return prevCoords; }
     ObjectMovementBehavior getMovementBehavior() const    { return movement_behavior; }
@@ -83,5 +82,8 @@ protected:
 
     friend class Map;
 };
+
+typedef std::deque<Object *> ObjectDeque;
+typedef std::deque<const Object *> CObjectDeque;
 
 #endif

src/person.cpp
@@ -41,9 +41,9 @@ int chars_needed(const char *s, int columnmax, int linesdesired, int *real_lines
  * Returns true of the object that 'punknown' points
  * to is a person object
  */
-bool isPerson(Object *punknown) {
-    Person *p;
-    if ((p = dynamic_cast<Person*>(punknown)) != NULL)
+bool isPerson(const Object *punknown) {
+    const Person *p;
+    if ((p = dynamic_cast<const Person*>(punknown)) != NULL)
         return true;
     else
         return false;

commit 55af039220f3ea2c5429646a515a7043c5d6df08

map: Fix monster saving outdoors (broken in commit e130fd0d).
The copying of Object::prevTile from tile was eliminated so a zero previous
tile value was written to the .sav file.

src/map.cpp
@@ -876,16 +876,21 @@ void Map::fillMonsterTable(SaveGameMonsterRecord* table) const {
     /**
      * Fill in our monster table
      */
+    MapTile prevTile;
     const UltimaSaveIds* saveIds = xu4.config->usaveIds();
     for (i = 0; i < MONSTERTABLE_SIZE; i++) {
-        Coords c = monsters[i]->getCoords(),
-           prevc = monsters[i]->getPrevCoords();
+        obj = monsters[i];
+        Coords c = obj->getCoords(),
+           prevc = obj->getPrevCoords();
 
         // Reset animation to a value that is savegame compatible with u4dos.
-        MapTile prevTile = monsters[i]->getPrevTile();
-        prevTile.frame = 0;
+        if (obj->getType() == Object::CREATURE) {
+            prevTile = obj->getTile();
+            prevTile.frame = 0;
+        } else
+            prevTile = obj->getPrevTile();
 
-        table->tile = saveIds->ultimaId(monsters[i]->getTile());
+        table->tile = saveIds->ultimaId(obj->getTile());
         table->x = c.x;
         table->y = c.y;
         table->prevTile = saveIds->ultimaId(prevTile);