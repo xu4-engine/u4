xu4-engine/u4

commit baefcc7a0151408cf935be9dfad8e08d74714005

Consolidate all pausing of user interaction into EventHandler::wait_msecs().
 - Remove WaitController and EventHandler::sleep().
 - Remove EventHandler::wait_cycles() as it was only used in one place.
 - The Controller base class now consumes all key presses.

These changes also fix a few issues:
 - Fix spurious automatic turn Pass during scripted vendor healing
   (via gameSpellEffect -> EventHandler::sleep).
 - Eliminates any possible side effect in wait_msecs() from calling both
   EventHandler::run() with a WaitController and then EventHandler::sleep()
   with the GameController.
 - Global keys (e.g. Alt+X to quit) work during the wait.

src/event_allegro.cpp
@@ -163,27 +163,33 @@ static void handleKeyDownEvent(const ALLEGRO_EVENT* event, Controller *controlle
 /**
  * Delays program execution for the specified number of milliseconds.
  * This doesn't actually stop events, but it stops the user from interacting
- * While some important event happens (e.g., getting hit by a cannon ball or a spell effect).
+ * while some important event happens (e.g., getting hit by a cannon ball or
+ * a spell effect).
  *
- * This method is not expected to handle msec values less than one game frame.
+ * This method is not expected to handle msec values of less than the display
+ * refresh interval.
  */
-void EventHandler::sleep(unsigned int msec) {
+void EventHandler::wait_msecs(unsigned int msec) {
+    Controller waitCon;     // Base controller consumes key events.
     ALLEGRO_EVENT event;
+    EventHandler* eh = xu4.eventHandler;
     ScreenAllegro* sa = SA;
     uint32_t endTime = getTicks() + msec;
     bool sleeping = true;
     bool redraw = false;
 
     // Using getTicks() rather than an ALLEGRO_TIMER to avoid the overhead
-    // to create/destroy & regiseter/unregister it.
+    // to create/destroy & register/unregister it.
 
-    while (sleeping) {
+    eh->pushController(&waitCon);
+
+    while (sleeping && ! eh->ended) {
         do {
             al_wait_for_event(sa->queue, &event);
             switch (event.type) {
-            default:
-                // Discard any key & button events.
-                // TODO: But handle Alt+x Alt+q keys...
+            // Discard all key & button events but globals (e.g. Alt+x).
+            case ALLEGRO_EVENT_KEY_CHAR:
+                handleKeyDownEvent(&event, &waitCon, NULL);
                 break;
             case ALLEGRO_EVENT_MOUSE_AXES:
                 handleMouseMotionEvent(event.mouse.x, event.mouse.y);
@@ -197,13 +203,13 @@ void EventHandler::sleep(unsigned int msec) {
                 if (event.timer.source == sa->refreshTimer) {
                     redraw = true;
                 } else {
-                    xu4.eventHandler->getTimer()->tick();
+                    eh->getTimer()->tick();
                 }
                 if (getTicks() >= endTime)
                     sleeping = false;
                 break;
             case ALLEGRO_EVENT_DISPLAY_CLOSE:
-                xu4.eventHandler->quitGame();
+                eh->quitGame();
                 sleeping = false;
                 break;
             }
@@ -214,6 +220,8 @@ void EventHandler::sleep(unsigned int msec) {
             screenSwapBuffers();
         }
     }
+
+    eh->popController();
 }
 
 void EventHandler::run() {

src/event_sdl.cpp
@@ -217,23 +217,32 @@ static void handleKeyDownEvent(const SDL_Event &event, Controller *controller, u
 /**
  * Delays program execution for the specified number of milliseconds.
  * This doesn't actually stop events, but it stops the user from interacting
- * While some important event happens (e.g., getting hit by a cannon ball or a spell effect).
+ * while some important event happens (e.g., getting hit by a cannon ball or
+ * a spell effect).
  *
- * This method is not expected to handle msec values less than one game frame.
+ * This method is not expected to handle msec values of less than the display
+ * refresh interval.
  */
-void EventHandler::sleep(unsigned int msec) {
+void EventHandler::wait_msecs(unsigned int msec) {
+    Controller waitCon;     // Base controller consumes key events.
     SDL_Event event;
+    EventHandler* eh = xu4.eventHandler;
     uint32_t endTime = getTicks() + msec;
     bool sleeping = true;
     bool redraw = false;
 
-    while (sleeping) {
+    eh->pushController(&waitCon);
+
+    while (sleeping && ! eh->ended) {
         do {
             SDL_WaitEvent(&event);
             switch (event.type) {
             default:
                 break;
+            // Discard all key & button events but globals (e.g. Alt+x).
             case SDL_KEYDOWN:
+                handleKeyDownEvent(event, &waitCon, NULL);
+                break;
             case SDL_KEYUP:
             case SDL_MOUSEBUTTONDOWN:
             case SDL_MOUSEBUTTONUP:
@@ -243,19 +252,19 @@ void EventHandler::sleep(unsigned int msec) {
                 handleMouseMotionEvent(event);
                 break;
             case SDL_ACTIVEEVENT:
-                handleActiveEvent(event, xu4.eventHandler->updateScreen);
+                handleActiveEvent(event, eh->updateScreen);
                 break;
             case SDL_USEREVENT:
                 if (event.user.code == UC_ScreenRefresh) {
                     redraw = true;
                 } else if (event.user.code == UC_TimedEventMgr) {
-                    xu4.eventHandler->getTimer()->tick();
+                    eh->getTimer()->tick();
                 }
                 if (getTicks() >= endTime)
                     sleeping = false;
                 break;
             case SDL_QUIT:
-                xu4.eventHandler->quitGame();
+                eh->quitGame();
                 sleeping = false;
                 break;
             }
@@ -266,6 +275,8 @@ void EventHandler::sleep(unsigned int msec) {
             screenSwapBuffers();
         }
     }
+
+    eh->popController();
 }
 
 void EventHandler::run() {

src/codex.cpp
@@ -100,7 +100,7 @@ void codexStart() {
     gameSetViewMode(VIEW_CODEX);
 
     screenMessage("\n\n\n\nThere is a sudden darkness, and you find yourself alone in an empty chamber.\n");
-    EventHandler::sleep(4000);
+    EventHandler::wait_msecs(4000);
 
     /**
      * check to see if you have the 3-part key
@@ -114,7 +114,7 @@ void codexStart() {
     screenRedrawMapArea();
 
     screenMessage("\nYou use your key of Three Parts.\n");
-    EventHandler::sleep(3000);
+    EventHandler::wait_msecs(3000);
 
     screenMessage("\nA voice rings out:\n\"What is the Word of Passage?\"\n\n");
 
@@ -151,12 +151,12 @@ void codexEject(CodexEjectCode code) {
         break;
     case CODEX_EJECT_NO_FULL_PARTY:
       screenMessage("\nThou hast not proved thy leadership in all eight virtues.\n\n");
-      EventHandler::sleep(2000);
+      EventHandler::wait_msecs(2000);
       screenMessage("\nPassage is not granted.\n\n");
       break;
     case CODEX_EJECT_NO_FULL_AVATAR:
         screenMessage("\nThou art not ready.\n");
-        EventHandler::sleep(2000);
+        EventHandler::wait_msecs(2000);
         screenMessage("\nPassage is not granted.\n\n");
         break;
     case CODEX_EJECT_BAD_WOP:
@@ -183,7 +183,7 @@ void codexEject(CodexEjectCode code) {
         break;
     }
 
-    EventHandler::sleep(2000);
+    EventHandler::wait_msecs(2000);
 
     /* free memory associated with the Codex */
     codexDelete();
@@ -225,7 +225,7 @@ void codexHandleWOP(const string &word) {
     /* slight pause before continuing */
     screenMessage("\n");
     screenDisableCursor();
-    EventHandler::sleep(1000);
+    EventHandler::wait_msecs(1000);
 
     /* entered correctly */
     if (strcasecmp(word.c_str(), "veramocor") == 0) {
@@ -246,14 +246,14 @@ void codexHandleWOP(const string &word) {
         }
 
         screenMessage("\nPassage is granted.\n");
-        EventHandler::sleep(4000);
+        EventHandler::wait_msecs(4000);
 
         screenEraseMapArea();
         screenRedrawMapArea();
 
         /* Ask the Virtue questions */
         screenMessage("\n\nThe voice asks:\n");
-        EventHandler::sleep(2000);
+        EventHandler::wait_msecs(2000);
         screenMessage("\n%s\n\n", codexVirtueQuestions[0].c_str());
 
         codexHandleVirtues(gameGetInput());
@@ -291,7 +291,7 @@ void codexHandleVirtues(const string &virtue) {
     /* slight pause before continuing */
     screenMessage("\n");
     screenDisableCursor();
-    EventHandler::sleep(1000);
+    EventHandler::wait_msecs(1000);
 
     /* answered with the correct one of eight virtues */
     if ((current < VIRT_MAX) &&
@@ -303,15 +303,15 @@ void codexHandleVirtues(const string &virtue) {
         current++;
         tries = 1;
 
-        EventHandler::sleep(2000);
+        EventHandler::wait_msecs(2000);
 
         if (current == VIRT_MAX) {
             screenMessage("\nThou art well versed in the virtues of the Avatar.\n");
-            EventHandler::sleep(5000);
+            EventHandler::wait_msecs(5000);
         }
 
         screenMessage("\n\nThe voice asks:\n");
-        EventHandler::sleep(2000);
+        EventHandler::wait_msecs(2000);
         screenMessage("\n%s\n\n", codexVirtueQuestions[current].c_str());
 #ifdef IOS
         U4IOS::IOSConversationHelper::setIntroString((current != VIRT_MAX) ? "Which virtue?" : "Which principle?");
@@ -331,7 +331,7 @@ void codexHandleVirtues(const string &virtue) {
 
         if (current < VIRT_MAX+3) {
             screenMessage("\n\nThe voice asks:\n");
-            EventHandler::sleep(2000);
+            EventHandler::wait_msecs(2000);
             screenMessage("\n%s\n\n", codexVirtueQuestions[current].c_str());
 #ifdef IOS
             U4IOS::IOSConversationHelper::setIntroString("Which principle?");
@@ -340,10 +340,10 @@ void codexHandleVirtues(const string &virtue) {
         }
         else {
             screenMessage("\nThe ground rumbles beneath your feet.\n");
-            EventHandler::sleep(1000);
+            EventHandler::wait_msecs(1000);
             screenShake(10);
 
-            EventHandler::sleep(3000);
+            EventHandler::wait_msecs(3000);
             screenEnableCursor();
             screenMessage("\nAbove the din, the voice asks:\n\nIf all eight virtues of the Avatar combine into and are derived from the Three Principles of Truth, Love and Courage...");
 #ifdef IOS
@@ -396,10 +396,10 @@ void codexHandleInfinity(const string &answer) {
     /* slight pause before continuing */
     screenMessage("\n");
     screenDisableCursor();
-    EventHandler::sleep(1000);
+    EventHandler::wait_msecs(1000);
 
     if (strcasecmp(answer.c_str(), "infinity") == 0) {
-        EventHandler::sleep(2000);
+        EventHandler::wait_msecs(2000);
         screenShake(10);
 
         screenEnableCursor();
@@ -464,5 +464,5 @@ bool codexHandleEndgameAnyKey(int key, void *data) {
  */
 void codexImpureThoughts() {
     screenMessage("\nThy thoughts are not pure.\nI ask again.\n");
-    EventHandler::sleep(2000);
+    EventHandler::wait_msecs(2000);
 }

src/combat.cpp
@@ -716,8 +716,9 @@ void CombatController::finishTurn() {
                 /* reset the focus to the avatar and start the party's turn over again */
                 focus = 0;
 
+                /* give a slight pause in case party members are asleep for awhile */
                 gameUpdateScreen();
-                EventHandler::sleep(50); /* give a slight pause in case party members are asleep for awhile */
+                EventHandler::wait_msecs(50);
 
                 /* adjust moves */
                 c->party->endTurn();

src/controller.cpp
@@ -31,6 +31,14 @@ int Controller::getTimerInterval() {
 }
 
 /**
+ * The keyboard input handler for a controller.
+ * The base keyPressed method returns true (consumes all key events).
+ */
+bool Controller::keyPressed(int key) {
+    return true;
+}
+
+/**
  * The default timerFired handler for a controller.  By default,
  * timers are ignored, but subclasses can override this method and it
  * will be called every <interval> 1/4 seconds.

src/controller.h
@@ -16,14 +16,14 @@ public:
     virtual ~Controller();
 
     /* methods for interacting with event manager */
-    virtual bool  isCombatController() const { return false; }
     bool notifyKeyPressed(int key);
     int getTimerInterval();
     static void timerCallback(void *data);
 
     /* control methods subclasses may want to override */
-    virtual bool keyPressed(int key) = 0;
+    virtual bool keyPressed(int key);
     virtual void timerFired();
+    virtual bool isCombatController() const { return false; }
 
 private:
     int timerInterval;

src/death.cpp
@@ -51,7 +51,7 @@ const struct {
 
 #define N_MSGS (sizeof(deathMsgs) / sizeof(deathMsgs[0]))
 
-void deathStart(int delay) {
+void deathStart(int delaySeconds) {
     if (deathSequenceRunning)
         return;
 
@@ -62,15 +62,13 @@ void deathStart(int delay) {
     timerCount = 0;
     timerMsg = 0;
 
-    WaitController waitCtrl(delay * xu4.settings->gameCyclesPerSecond);
-    xu4.eventHandler->pushController(&waitCtrl);
-    waitCtrl.wait();
+    if (delaySeconds > 0)
+        EventHandler::wait_msecs(delaySeconds * 1000);
 
     gameSetViewMode(VIEW_DEAD);
-
-    xu4.eventHandler->pushKeyHandler(&KeyHandler::ignoreKeys);
     screenDisableCursor();
 
+    xu4.eventHandler->pushKeyHandler(&KeyHandler::ignoreKeys);
     xu4.eventHandler->getTimer()->add(&deathTimer, xu4.settings->gameCyclesPerSecond);
 }
 

src/death.h
@@ -5,6 +5,6 @@
 #ifndef DEATH_H
 #define DEATH_H
 
-void deathStart(int delay);
+void deathStart(int delaySeconds);
 
 #endif

commit 79a5a1433f9de22641e6d18bb8f861da4534d557

Fix crash when wait_msecs() is used inside TimedEvent::tick() code.
Remove push & pop of waitCon Controller to avoid needlessly adding a new
TimedEvent and thus corrupting the TimedEventMgr::events list in its
locked state.

The segfault would occur in combat after finishTurn() was called via the
GameController::timerFired() force pass timeout.

The bug was introduced by commit baefcc7a.

src/event_allegro.cpp
@@ -171,8 +171,6 @@ void EventHandler::wait_msecs(unsigned int msec) {
     // Using getTicks() rather than an ALLEGRO_TIMER to avoid the overhead
     // to create/destroy & register/unregister it.
 
-    eh->pushController(&waitCon);
-
     while (sleeping && ! eh->ended) {
         do {
             al_wait_for_event(sa->queue, &event);
@@ -210,8 +208,6 @@ void EventHandler::wait_msecs(unsigned int msec) {
             screenSwapBuffers();
         }
     }
-
-    eh->popController();
 }
 
 void EventHandler::run() {

src/event_sdl.cpp
@@ -223,8 +223,6 @@ void EventHandler::wait_msecs(unsigned int msec) {
     bool sleeping = true;
     bool redraw = false;
 
-    eh->pushController(&waitCon);
-
     while (sleeping && ! eh->ended) {
         do {
             SDL_WaitEvent(&event);
@@ -267,8 +265,6 @@ void EventHandler::wait_msecs(unsigned int msec) {
             screenSwapBuffers();
         }
     }
-
-    eh->popController();
 }
 
 void EventHandler::run() {