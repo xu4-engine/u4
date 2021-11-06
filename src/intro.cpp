xu4-engine/u4

commit afd308b77d6449f563fbb72862e0918ab2c5af77

Show the intro if --skip-intro is used without an existing saved game.
An error message is presented to the user in-game rather than exiting the
program.

Change SaveGame and errorMessage ownership to XU4GameServices. This
eliminates an extra SaveGame load when using Journey Onward, and allows
StagePlay to exit back to StageIntro with an error.

 - Add saveGameLoad() to consolidate some loading code.
 - Increase the intro error message display time to 3 seconds.

src/intro.cpp
@@ -302,6 +302,9 @@ bool IntroController::init() {
     beastieSub[0] = beastiesImg->subImageIndex[sym[0]];
     beastieSub[1] = beastiesImg->subImageIndex[sym[1]];
 
+    if (xu4.errorMessage)
+        bSkipTitles = true;
+
     if (bSkipTitles)
     {
         // the init() method is called again from within the
@@ -392,19 +395,16 @@ bool IntroController::keyPressed(int key) {
     case INTRO_MENU:
         switch (key) {
         case 'i':
-            errorMessage.erase();
             initiateNewGame();
             break;
         case 'j':
             journeyOnward();
             break;
         case 'r':
-            errorMessage.erase();
             mode = INTRO_MAP;
             updateScreen();
             break;
         case 'c': {
-            errorMessage.erase();
             // Make a copy of our settings so we can change them
             settingsChanged = *xu4.settings;
             screenDisableCursor();
@@ -414,7 +414,6 @@ bool IntroController::keyPressed(int key) {
             break;
         }
         case 'a':
-            errorMessage.erase();
             about();
             break;
         case 'q':
@@ -652,14 +651,15 @@ void IntroController::updateScreen() {
         backgroundArea.draw(BKGD_OPTIONS_BTM, 0, 120);
 
         // if there is an error message to display, show it
-        if (!errorMessage.empty())
+        if (xu4.errorMessage)
         {
-            menuArea.textAt(6, 5, "%s", errorMessage.c_str());
+            menuArea.textAt(6, 5, xu4.errorMessage);
+            xu4.errorMessage = NULL;
+
             drawBeasties();
             // wait for a couple seconds
-            EventHandler::wait_msecs(2000);
+            EventHandler::wait_msecs(3000);
             // clear the screen again
-            errorMessage.erase();
             backgroundArea.draw(BKGD_INTRO);
             backgroundArea.draw(BKGD_OPTIONS_BTM, 0, 120);
         }
@@ -766,10 +766,13 @@ void IntroController::finishInitiateGame(const string &nameBuffer, SexType sex)
 
     // write out save game an segue into game
 
+    delete xu4.saveGame;
+    xu4.saveGame = NULL;    // Make GameController::init() reload the game.
+
     FILE *saveGameFile = fopen((xu4.settings->getUserPath() + PARTY_SAV).c_str(), "wb");
     if (!saveGameFile) {
         questionArea.disableCursor();
-        errorMessage = "Unable to create save game!";
+        xu4.errorMessage = "Unable to create save game!";
         updateScreen();
         return;
     }
@@ -941,35 +944,12 @@ string IntroController::getQuestion(int v1, int v2) {
  * Starts the game.
  */
 void IntroController::journeyOnward() {
-    FILE *saveGameFile;
-    bool validSave = false;
-
-    /*
-     * ensure a party.sav file exists, otherwise require user to
-     * initiate game
-     */
-    saveGameFile = fopen((xu4.settings->getUserPath() + PARTY_SAV).c_str(), "rb");
-    if (saveGameFile) {
-        SaveGame *saveGame = new SaveGame;
-
-        // Make sure there are players in party.sav --
-        // In the Ultima Collection CD, party.sav exists, but does
-        // not contain valid info to journey onward
-        saveGame->read(saveGameFile);
-        if (saveGame->members > 0)
-            validSave = true;
-        delete saveGame;
-        fclose(saveGameFile);
+    if (saveGameLoad()) {
+        xu4.stage = StagePlay;
+        xu4.eventHandler->setControllerDone();
+    } else {
+        updateScreen();     // Shows errorMessage set by saveGameLoad().
     }
-
-    if (!validSave) {
-        errorMessage = "Initiate a new game first!";
-        updateScreen();
-        return;
-    }
-
-    xu4.stage = StagePlay;
-    xu4.eventHandler->setControllerDone();
 }
 
 /**

src/context.cpp
@@ -27,7 +27,6 @@ Context::Context()
 
 Context::~Context() {
     delete party;
-    delete saveGame;
     delete location;
     delete stats;
     delete aura;

src/game.cpp
@@ -66,7 +66,6 @@ void gameInnHandler(void);
 void gameLostEighth(Virtue virtue);
 void gamePartyStarving(void);
 time_t gameTimeSinceLastCommand(void);
-int gameSave(const char* path);
 
 /* spell functions */
 void gameCastSpell(unsigned int spell, int caster, int param);
@@ -213,13 +212,13 @@ public:
 };
 
 /**
- * Loads the saved game.
+ * Loads the saved game (if not already loaded) and create global game context.
+ *
+ * Return true if loading is successful.
  */
-void GameController::init() {
+bool GameController::init() {
     Debug gameDbg("debug/game.txt", "Game");
-    FILE *fp;
     const Settings& settings = *xu4.settings;
-    MonstersSav mons;
 
     TRACE(gameDbg, "gameInit() running.");
 
@@ -232,11 +231,19 @@ void GameController::init() {
 
     screenTextAt(13, 11, "%s", "Loading Game...");
 
-    /* initialize the global game context */
-    c = new Context;
-    c->saveGame = new SaveGame;
+    /* load in the save game (if not done by intro) */
+    if (! xu4.saveGame) {
+        if (! saveGameLoad()) {
+            xu4.stage = StageIntro;     // Go back to intro.
+            return false;
+        }
+    }
+    TRACE_LOCAL(gameDbg, "Save game loaded."); ++pb;
 
-    TRACE_LOCAL(gameDbg, "Global context initialized.");
+    /* initialize the global game context */
+    delete c;
+    c = new Context;
+    c->saveGame = xu4.saveGame;
 
     /* initialize conversation and game state variables */
     c->line = TEXT_AREA_H - 1;
@@ -252,15 +259,7 @@ void GameController::init() {
     c->lastCommandTime = time(NULL);
     c->lastShip = NULL;
 
-    /* load in the save game */
-    fp = fopen((settings.getUserPath() + PARTY_SAV).c_str(), "rb");
-    if (fp) {
-        c->saveGame->read(fp);
-        fclose(fp);
-    } else
-        errorFatal("no savegame found!");
-
-    TRACE_LOCAL(gameDbg, "Save game loaded."); ++pb;
+    TRACE_LOCAL(gameDbg, "Global context initialized.");
 
     /* initialize our party */
     c->party = new Party(c->saveGame);
@@ -308,7 +307,9 @@ void GameController::init() {
     TRACE_LOCAL(gameDbg, "Loading monsters."); ++pb;
 
     /* load in monsters.sav */
-    fp = fopen((settings.getUserPath() + MONSTERS_SAV).c_str(), "rb");
+    {
+    MonstersSav mons;
+    FILE* fp = fopen((settings.getUserPath() + MONSTERS_SAV).c_str(), "rb");
     if (fp) {
         saveGameMonstersRead(mons.table, fp);
         fclose(fp);
@@ -323,6 +324,7 @@ void GameController::init() {
             fclose(fp);
             gameFixupObjects(c->location->prev->map, mons.table);
         }
+    }
     }
 
     spellSetEffectCallback(&gameSpellEffect);
@@ -344,6 +346,7 @@ void GameController::init() {
 
     initScreenWithoutReloadingState();
     TRACE(gameDbg, "gameInit() completed successfully.");
+    return true;
 }
 
 /**

src/game.h
@@ -98,7 +98,7 @@ public:
     virtual void timerFired();
 
     /* main game functions */
-    void init();
+    bool init();
     void initScreen();
     void initScreenWithoutReloadingState();
     void setMap(Map *map, bool saveLocation, const Portal *portal, TurnCompleter *turnCompleter = NULL);

src/intro.h
@@ -216,7 +216,6 @@ private:
     IntroBinData *binData;
 
     // additional introduction state data
-    std::string errorMessage;
     int answerInd;
     int questionRound;
     int questionTree[15];

src/savegame.cpp
@@ -397,6 +397,42 @@ int saveGameMonstersRead(SaveGameMonsterRecord *monsterTable, FILE *f) {
     return 1;
 }
 
+#ifndef SAVE_UTIL
+#include "settings.h"
+#include "xu4.h"
+
+/*
+ * Set xu4.saveGame to a new loaded game.  If loading fails or there are no
+ * players defined then set xu4.errorMessage and return NULL.
+ */
+SaveGame* saveGameLoad() {
+    SaveGame* sg = NULL;
+    FILE* fp = fopen((xu4.settings->getUserPath() + PARTY_SAV).c_str(), "rb");
+    if (fp) {
+        sg = new SaveGame;
+        sg->read(fp);
+        fclose(fp);
+
+        // Make sure there are players in party.sav --
+        // In the Ultima Collection CD, party.sav exists, but does
+        // not contain valid info to journey onward
+
+        if (sg->members < 1) {
+            delete sg;
+            sg = NULL;
+        }
+    }
+
+    if (sg) {
+        delete xu4.saveGame;
+        xu4.saveGame = sg;
+    } else {
+        xu4.errorMessage = "Initiate a new game first!";
+    }
+    return sg;
+}
+#endif
+
 //--------------------------------------
 
 #ifndef SAVE_UTIL

src/savegame.h
@@ -246,6 +246,7 @@ struct SaveGame {
 
 int saveGameMonstersWrite(const SaveGameMonsterRecord *monsterTable, FILE *f);
 int saveGameMonstersRead(SaveGameMonsterRecord *monsterTable, FILE *f);
+SaveGame* saveGameLoad();
 
 class Config;
 class Tileset;

src/u4.cpp
@@ -189,6 +189,7 @@ void servicesInit(XU4GameServices* gs, Options* opt) {
 void servicesFree(XU4GameServices* gs) {
     delete gs->game;
     delete gs->intro;
+    delete gs->saveGame;
     delete gs->eventHandler;
     soundDelete();
     screenDelete();
@@ -260,10 +261,12 @@ int main(int argc, char *argv[]) {
 
             if (! xu4.game) {
                 xu4.game = new GameController();
-                xu4.game->init();
+                if (! xu4.game->init())
+                    continue;
             } else if (xu4.intro && xu4.intro->hasInitiatedNewGame()) {
                 //Loads current savegame
-                xu4.game->init();
+                if (! xu4.game->init())
+                    continue;
             } else {
                 //Inits screen stuff without renewing game
                 xu4.game->initScreen();

commit d4f6035a69d89fbf29e7c9af3057a1e34cc9b0d3

intro.cpp: Fix Journey Onward to return to a running game.
This was broken in commit afd308b7.  The c->party->saveGame pointer
becomes invalid when saveGameLoad() deletes xu4.saveGame.

src/intro.cpp
@@ -944,7 +944,8 @@ string IntroController::getQuestion(int v1, int v2) {
  * Starts the game.
  */
 void IntroController::journeyOnward() {
-    if (saveGameLoad()) {
+    // Return to a running game or attempt to load a saved one.
+    if (xu4.saveGame || saveGameLoad()) {
         xu4.stage = StagePlay;
         xu4.eventHandler->setControllerDone();
     } else {