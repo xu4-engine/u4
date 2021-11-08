xu4-engine/u4

commit 2e190824144fca7905ea713d8b1ea3d0d4abb171

Remove Object::getMap() as a preparatory step for removing Object::maps.

src/combat.cpp
@@ -190,7 +190,7 @@ void CombatController::applyCreatureTileEffects() {
     for (i = creatures.begin(); i != creatures.end(); i++) {
         Creature *m = *i;
         TileEffect effect = map->tileTypeAt(m->getCoords(), WITH_GROUND_OBJECTS)->getEffect();
-        m->applyTileEffect(effect);
+        m->applyTileEffect(map, effect);
     }
 }
 
@@ -554,7 +554,7 @@ bool CombatController::attackAt(const Coords &coords, PartyMember *attacker, int
         GameController::flashTile(coords, hittile, 3);
 
         /* apply the damage to the creature */
-        if (!attacker->dealDamage(creature, attacker->getDamage()))
+        if (!attacker->dealDamage(map, creature, attacker->getDamage()))
         {
             creature = NULL;
             GameController::flashTile(coords, hittile, 1);
@@ -592,7 +592,7 @@ bool CombatController::rangedAttack(const Coords &coords, Creature *attacker) {
         /* FIXME: are there any special effects here? */
         soundPlay(SOUND_PC_STRUCK, false);
         screenMessage("\n%s %cElectrified%c!\n", target->getName().c_str(), FG_BLUE, FG_WHITE);
-        attacker->dealDamage(target, attacker->getDamage());
+        attacker->dealDamage(map, target, attacker->getDamage());
         break;
 
     case EFFECT_POISON:
@@ -626,7 +626,7 @@ bool CombatController::rangedAttack(const Coords &coords, Creature *attacker) {
         soundPlay(SOUND_PC_STRUCK, false);
         screenMessage("\n%s %c%s Hit%c!\n", target->getName().c_str(), FG_RED,
                       effect == EFFECT_LAVA ? "Lava" : "Fiery", FG_WHITE);
-        attacker->dealDamage(target, attacker->getDamage());
+        attacker->dealDamage(map, target, attacker->getDamage());
         break;
 
     default:
@@ -635,7 +635,7 @@ bool CombatController::rangedAttack(const Coords &coords, Creature *attacker) {
         if (hittile == Tileset::findTileByName(Tile::sym.magicFlash)->getId())
             screenMessage("\n%s %cMagical Hit%c!\n", target->getName().c_str(), FG_BLUE, FG_WHITE);
         else screenMessage("\n%s Hit!\n", target->getName().c_str());
-        attacker->dealDamage(target, attacker->getDamage());
+        attacker->dealDamage(map, target, attacker->getDamage());
         break;
     }
     GameController::flashTile(coords, hittile, 1);

src/player.cpp
@@ -375,10 +375,10 @@ bool PartyMember::applyDamage(int damage, bool) {
     player->hp = newHp;
     notifyOfChange();
 
-    if (isCombatMap(c->location->map) && isDead()) {
-        Coords p = getCoords();
-        Map *map = getMap();
-        map->annotations->add(p, Tileset::findTileByName(Tile::sym.corpse)->getId())->setTTL(party->size() * 2);
+    Map* map = c->location->map;
+    if (isCombatMap(map) && isDead()) {
+        TileId corpseId = Tileset::findTileByName(Tile::sym.corpse)->getId();
+        map->annotations->add(coords, corpseId)->setTTL(party->size() * 2);
 
         if (party) {
             party->setChanged();
@@ -405,12 +405,12 @@ int PartyMember::getDefense() const {
     return xu4.config->armor(player->armor)->defense;
 }
 
-bool PartyMember::dealDamage(Creature *m, int damage) {
+bool PartyMember::dealDamage(Map* map, Creature *m, int damage) {
     /* we have to record these now, because if we
        kill the target, it gets destroyed */
     int m_xp = m->getXp();
 
-    if (!Creature::dealDamage(m, damage)) {
+    if (!Creature::dealDamage(map, m, damage)) {
         /* half the time you kill an evil creature you get a karma boost */
         awardXp(m_xp);
         return false;

src/player.h
@@ -128,7 +128,7 @@ public:
     virtual bool applyDamage(int damage, bool byplayer = false);
     virtual int getAttackBonus() const;
     virtual int getDefense() const;
-    virtual bool dealDamage(Creature *m, int damage);
+    virtual bool dealDamage(Map*, Creature *m, int damage);
     int getDamage();
     virtual Symbol getHitTile() const;
     virtual Symbol getMissTile() const;

src/creature.cpp
@@ -277,6 +277,7 @@ void Creature::act(CombatController *controller) {
     int dist;
     CombatAction action;
     Creature *target;
+    CombatMap* map = controller->getMap();
 
     /* see if creature wakes up if it is asleep */
     if ((getStatus() == STAT_SLEEPING) && (xu4_random(8) == 0))
@@ -314,7 +315,7 @@ void Creature::act(CombatController *controller) {
      * now find out who to do it to
      */
 
-    target = nearestOpponent(&dist, action == CA_RANGED);
+    target = nearestOpponent(map, &dist, action == CA_RANGED);
     if (target == NULL)
         return;
 
@@ -322,7 +323,7 @@ void Creature::act(CombatController *controller) {
         action = CA_ADVANCE;
 
     /* let's see if the creature blends into the background, or if he appears... */
-    if (camouflages() && !hideOrShow())
+    if (camouflages() && !hideOrShow(map))
         return; /* creature is hidden -- no action! */
 
     switch(action) {
@@ -334,7 +335,7 @@ void Creature::act(CombatController *controller) {
             GameController::flashTile(target->getCoords(), Tile::sym.hitFlash, 4);
 
 
-            if (!dealDamage(target, getDamage()))
+            if (!dealDamage(map, target, getDamage()))
                 target = NULL;
 
             if (target && isPartyMember(target)) {
@@ -362,7 +363,7 @@ void Creature::act(CombatController *controller) {
 
         /* Apply the sleep spell to party members still in combat */
         if (!isPartyMember(this)) {
-            PartyMemberVector party = controller->getMap()->getPartyMembers();
+            PartyMemberVector party = map->getPartyMembers();
             PartyMemberVector::iterator j;
 
             for (j = party.begin(); j != party.end(); j++) {
@@ -379,7 +380,6 @@ void Creature::act(CombatController *controller) {
         bool firstTry = true;
 
         while (!valid) {
-            Map *map = getMap();
             new_c = Coords(xu4_random(map->width), xu4_random(map->height), c->location->coords.z);
 
             const Tile *tile = map->tileTypeAt(new_c, WITH_OBJECTS);
@@ -430,7 +430,6 @@ void Creature::act(CombatController *controller) {
 
     case CA_FLEE:
     case CA_ADVANCE: {
-        Map *map = getMap();
         if (moveCombatObject(action, map, this, target->getCoords())) {
             Coords coords = getCoords();
 
@@ -451,7 +450,7 @@ void Creature::act(CombatController *controller) {
     this->animateMovement();
 }
 
-void Creature::applyTileEffect(TileEffect effect) {
+void Creature::applyTileEffect(Map* map, TileEffect effect) {
     if (effect != EFFECT_NONE) {
         gameUpdateScreen();
 
@@ -467,13 +466,13 @@ void Creature::applyTileEffect(TileEffect effect) {
         case EFFECT_FIRE:
             /* deal 0 - 127 damage to the creature if it is not immune to fire damage */
             if ((resists != EFFECT_FIRE) && (resists != EFFECT_LAVA))
-                applyDamage(xu4_random(0x7F), false);
+                applyDamage(map, xu4_random(0x7F), false);
             break;
 
         case EFFECT_POISONFIELD:
             /* deal 0 - 127 damage to the creature if it is not immune to poison field damage */
             if (resists != EFFECT_POISONFIELD)
-                applyDamage(xu4_random(0x7F), false);
+                applyDamage(map, xu4_random(0x7F), false);
             break;
 
         case EFFECT_POISON:
@@ -490,8 +489,7 @@ int Creature::getDefense() const {
     return 128;
 }
 
-bool Creature::divide() {
-    Map *map = getMap();
+bool Creature::divide(Map* map) {
     int dirmask = map->getValidMoves(getCoords(), getTile());
     Direction d = dirRandomDir(dirmask);
 
@@ -519,16 +517,11 @@ bool Creature::divide() {
     return false;
 }
 
-bool Creature::spawnOnDeath() {
-    Map *map = getMap();
-
+bool Creature::spawnOnDeath(Map* map) {
     /* this is a game enhancement, make sure it's turned on! */
     if (!xu4.settings->enhancements ||
         !xu4.settings->enhancementsOptions.gazerSpawnsInsects)
         return false;
-
-    /* make sure there's a place to put the divided creature! */
-    MapCoords coords(getCoords());
 
     /* create our new creature! */
     map->addCreature(xu4.config->creature(spawn), coords);
@@ -539,12 +532,12 @@ bool Creature::spawnOnDeath() {
  * Hides or shows a camouflaged creature, depending on its distance from
  * the nearest opponent
  */
-bool Creature::hideOrShow() {
+bool Creature::hideOrShow(Map* map) {
     /* find the nearest opponent */
     int dist;
 
     /* ok, now we've got the nearest party member.  Now, see if they're close enough */
-    if (nearestOpponent(&dist, false) != NULL) {
+    if (nearestOpponent(map, &dist, false) != NULL) {
         if ((dist < 5) && !isVisible())
             setVisible(); /* show yourself */
         else if (dist >= 5)
@@ -554,12 +547,11 @@ bool Creature::hideOrShow() {
     return isVisible();
 }
 
-Creature *Creature::nearestOpponent(int *dist, bool ranged) {
+Creature *Creature::nearestOpponent(Map* map, int *dist, bool ranged) {
     Creature *opponent = NULL;
     int d, leastDist = 0xFFFF;
     ObjectDeque::iterator i;
     bool jinx = (*c->aura == Aura::JINX);
-    Map *map = getMap();
 
     for (i = map->objects.begin(); i < map->objects.end(); i++) {
         if (!isCreature(*i))
@@ -679,7 +671,7 @@ void Creature::wakeUp() {
  * fire or poison, or as a result of jinx) we don't report experience
  * on death
  */
-bool Creature::applyDamage(int damage, bool byplayer) {
+bool Creature::applyDamage(Map* map, int damage, bool byplayer) {
     /* deal the damage */
     if (id != LORDBRITISH_ID)
         AdjustValueMin(hp, -damage, 0);
@@ -700,7 +692,7 @@ bool Creature::applyDamage(int damage, bool byplayer) {
          * then remove it
          */
         if (spawnsOnDeath())
-            spawnOnDeath();
+            spawnOnDeath(map);
 
         // Remove yourself from the map
         removeFromMaps();
@@ -729,13 +721,13 @@ bool Creature::applyDamage(int damage, bool byplayer) {
 
     /* creature is still alive and has the chance to divide - xu4 enhancement */
     if (divides() && xu4_random(2) == 0)
-        divide();
+        divide(map);
 
     return true;
 }
 
-bool Creature::dealDamage(Creature *m, int damage) {
-    return m->applyDamage(damage, isPartyMember(this));
+bool Creature::dealDamage(Map* map, Creature *m, int damage) {
+    return m->applyDamage(map, damage, isPartyMember(this));
 }
 
 //--------------------------------------

src/creature.h
@@ -199,25 +199,25 @@ public:
     /* combat methods */
     void act(CombatController *controller);
     virtual void addStatus(StatusType status);
-    void applyTileEffect(TileEffect effect);
+    void applyTileEffect(Map*, TileEffect effect);
     virtual int getAttackBonus() const;
     virtual int getDefense() const;
-    bool divide();
-    bool spawnOnDeath();
+    bool divide(Map*);
+    bool spawnOnDeath(Map*);
     virtual CreatureStatus getState() const;
     StatusType getStatus() const;
     bool isAsleep() const;
     bool isDisabled() const;
     bool isDead() const;
-    bool hideOrShow();
-    Creature *nearestOpponent(int *dist, bool ranged);
+    bool hideOrShow(Map*);
+    Creature *nearestOpponent(Map*, int *dist, bool ranged);
     virtual void putToSleep();
     virtual void removeStatus(StatusType status);
     virtual void setStatus(StatusType status);
     virtual void wakeUp();
 
-    virtual bool applyDamage(int damage, bool byplayer = true);
-    virtual bool dealDamage(Creature *m, int damage);
+    virtual bool applyDamage(Map*, int damage, bool byplayer = true);
+    virtual bool dealDamage(Map*, Creature *m, int damage);
 
     // Properties
 //protected:

src/object.cpp
@@ -22,12 +22,6 @@ void Object::placeOnMap(Map* map, const Coords& coords) {
     setCoords(coords);
 }
 
-Map *Object::getMap() {
-    if (maps.empty())
-        return NULL;
-    return maps.back();
-}
-
 /*
  * Remove object from any maps that it is a part of.
  *

src/object.h
@@ -63,7 +63,6 @@ public:
     void setAnimated(bool a = true)         { animated = a; }
 
     void placeOnMap(Map*, const Coords&);
-    Map *getMap();
     void removeFromMaps();
 
     bool setDirection(Direction d);

src/spell.cpp
@@ -373,7 +373,7 @@ bool spellMagicAttackAt(const Coords &coords, MapTile attackTile, int attackDama
 
         /* apply the damage to the creature */
         CombatController *controller = spellCombatController();
-        controller->getCurrentPlayer()->dealDamage(creature, attackDamage);
+        controller->getCurrentPlayer()->dealDamage(cm, creature, attackDamage);
         GameController::flashTile(coords, attackTile, 1);
     }
 
@@ -655,7 +655,8 @@ static int spellSleep(int unused) {
 
 static int spellTremor(int unused) {
     CombatController *ct = spellCombatController();
-    CreatureVector creatures = ct->getMap()->getCreatures();
+    CombatMap* map = ct->getMap();
+    CreatureVector creatures = map->getCreatures();
     CreatureVector::iterator i;
 
     for (i = creatures.begin(); i != creatures.end(); i++) {
@@ -675,14 +676,14 @@ static int spellTremor(int unused) {
             if (xu4_random(2) == 0) {
                 soundPlay(SOUND_NPC_STRUCK);
                 GameController::flashTile(coords, Tile::sym.hitFlash, 3);
-                ct->getCurrentPlayer()->dealDamage(m, 0xFF);
+                ct->getCurrentPlayer()->dealDamage(map, m, 0xFF);
             }
             /* Deal enough damage to creature to make it flee */
             else if (xu4_random(2) == 0) {
                 soundPlay(SOUND_NPC_STRUCK);
                 GameController::flashTile(coords, Tile::sym.hitFlash, 2);
                 if (m->getHp() > 23)
-                    ct->getCurrentPlayer()->dealDamage(m, m->getHp()-23);
+                    ct->getCurrentPlayer()->dealDamage(map, m, m->getHp()-23);
             }
             else
             {

commit 5829110f03f2f56ea09eea164c95a735860c7a9e

Fix player characters taking damage (broken in commit 2e190824).
The Map argument was not added to the PartyMember::applyDamage() virtual
function, so Creature::applyDamage() was called instead.

src/combat.cpp
@@ -679,10 +679,12 @@ void CombatController::finishTurn() {
         return;
     }
 
+    Map* map = c->location->map;
+
     /* make sure the player with the focus is still in battle (hasn't fled or died) */
     if (player) {
         /* apply effects from tile player is standing on */
-        player->applyEffect(c->location->map->tileTypeAt(player->getCoords(), WITH_GROUND_OBJECTS)->getEffect());
+        player->applyEffect(map, map->tileTypeAt(player->getCoords(), WITH_GROUND_OBJECTS)->getEffect());
     }
 
     quick = (*c->aura == Aura::QUICKNESS) && player && (xu4_random(2) == 0) ? 1 : 0;
@@ -691,7 +693,7 @@ void CombatController::finishTurn() {
     if (!quick || player->isDisabled()){
 
         do {
-            c->location->map->annotations->passTurn();
+            map->annotations->passTurn();
 
             /* put a sleeping person in place of the player,
                or restore an awakened member to their original state */
@@ -760,7 +762,8 @@ void CombatController::finishTurn() {
                   !party[c->party->getActivePlayer()]->isDisabled() && /* and the active player is not disabled */
                   (c->party->getActivePlayer() != focus)));
     }
-    else c->location->map->annotations->passTurn();
+    else
+        map->annotations->passTurn();
 
 #if 0
     if (focus != 0) {

src/dungeon.cpp
@@ -137,6 +137,7 @@ void dungeonDrinkFountain() {
     if (player == -1)
         return;
 
+    PartyMember* pc = c->party->member(player);
     Dungeon *dungeon = dynamic_cast<Dungeon *>(c->location->map);
     FountainType type = (FountainType) dungeon->currentSubToken();
 
@@ -148,33 +149,36 @@ void dungeonDrinkFountain() {
 
     /* healing fountain */
     case FOUNTAIN_HEALING:
-        if (c->party->member(player)->heal(HT_FULLHEAL))
+        if (pc->heal(HT_FULLHEAL))
             screenMessage("\nAhh-Refreshing!\n");
-        else screenMessage("\nHmmm--No Effect!\n");
+        else
+            screenMessage("\nHmmm--No Effect!\n");
         break;
 
     /* acid fountain */
     case FOUNTAIN_ACID:
-        c->party->member(player)->applyDamage(100); /* 100 damage to drinker */
+        pc->applyDamage(dungeon, 100);      /* 100 damage to drinker */
         screenMessage("\nBleck--Nasty!\n");
         break;
 
     /* cure fountain */
     case FOUNTAIN_CURE:
-        if (c->party->member(player)->heal(HT_CURE))
+        if (pc->heal(HT_CURE))
             screenMessage("\nHmmm--Delicious!\n");
-        else screenMessage("\nHmmm--No Effect!\n");
+        else
+            screenMessage("\nHmmm--No Effect!\n");
         break;
 
     /* poison fountain */
     case FOUNTAIN_POISON:
-        if (c->party->member(player)->getStatus() != STAT_POISONED) {
+        if (pc->getStatus() != STAT_POISONED) {
             soundPlay(SOUND_POISON_DAMAGE);
-            c->party->member(player)->applyEffect(EFFECT_POISON);
-            c->party->member(player)->applyDamage(100); /* 100 damage to drinker also */
+            pc->applyEffect(dungeon, EFFECT_POISON);
+            pc->applyDamage(dungeon, 100);  /* 100 damage to drinker also */
             screenMessage("\nArgh-Choke-Gasp!\n");
         }
-        else screenMessage("\nHmm--No Effect!\n");
+        else
+            screenMessage("\nHmm--No Effect!\n");
         break;
 
     default:
@@ -227,7 +231,7 @@ void dungeonTouchOrb() {
     }
 
     /* deal damage to the party member who touched the orb */
-    c->party->member(player)->applyDamage(damage);
+    c->party->member(player)->applyDamage(loc->map, damage);
 
     /* remove the orb from the map */
     loc->map->setTileAt(loc->coords,
@@ -248,11 +252,11 @@ bool dungeonHandleTrap(TrapType trap) {
         // Treat falling rocks and pits like bomb traps
         // XXX: That's a little harsh.
         screenMessage("\nFalling Rocks!\n");
-        c->party->applyEffect(EFFECT_LAVA);
+        c->party->applyEffect(dungeon, EFFECT_LAVA);
         break;
     case TRAP_PIT:
         screenMessage("\nPit!\n");
-        c->party->applyEffect(EFFECT_LAVA);
+        c->party->applyEffect(dungeon, EFFECT_LAVA);
         break;
     default: break;
     }

src/game.cpp
@@ -573,6 +573,7 @@ void GameController::finishTurn() {
     Creature *attacker = NULL;
 
     while (xu4.stage == StagePlay) {
+        Map* map = c->location->map;
 
         /* adjust food and moves */
         c->party->endTurn();
@@ -592,10 +593,10 @@ void GameController::finishTurn() {
         if (!c->party->isFlying()) {
 
             // apply effects from tile avatar is standing on
-            c->party->applyEffect(c->location->map->tileTypeAt(c->location->coords, WITH_GROUND_OBJECTS)->getEffect());
+            c->party->applyEffect(map, map->tileTypeAt(c->location->coords, WITH_GROUND_OBJECTS)->getEffect());
 
             // Move creatures and see if something is attacking the avatar
-            attacker = c->location->map->moveObjects(c->location->coords);
+            attacker = map->moveObjects(c->location->coords);
 
             // Something's attacking!  Start combat!
             if (attacker) {
@@ -610,7 +611,7 @@ void GameController::finishTurn() {
         }
 
         /* update map annotations */
-        c->location->map->annotations->passTurn();
+        map->annotations->passTurn();
 
         if (!c->party->isImmobilized())
             break;
@@ -692,7 +693,7 @@ void GameController::update(Party *party, PartyEvent &event) {
 
         // 2 damage to each party member for starving!
         for (i = 0; i < c->saveGame->members; i++)
-            c->party->member(i)->applyDamage(2);
+            c->party->member(i)->applyDamage(c->location->map, 2);
         break;
     default:
         break;
@@ -1999,9 +2000,11 @@ bool getChestTrapHandler(int player) {
         if ((player >= 0) &&
             (c->saveGame->players[player].dex + 25 < xu4_random(100)))
         {
+            Map* map = c->location->map;
             if (trapType == EFFECT_LAVA) /* bomb trap */
-                c->party->applyEffect(trapType);
-            else c->party->member(player)->applyEffect(trapType);
+                c->party->applyEffect(map, trapType);
+            else
+                c->party->member(player)->applyEffect(map, trapType);
         }
         else screenMessage("Evaded!\n");
 
@@ -3280,7 +3283,7 @@ void gameDamageParty(int minDamage, int maxDamage) {
             damage = ((minDamage >= 0) && (minDamage < maxDamage)) ?
                 xu4_random((maxDamage + 1) - minDamage) + minDamage :
                 maxDamage;
-            c->party->member(i)->applyDamage(damage);
+            c->party->member(i)->applyDamage(c->location->map, damage);
             c->stats->highlightPlayer(i);
             lastdmged = i;
             EventHandler::wait_msecs(50);

src/player.cpp
@@ -193,7 +193,7 @@ void PartyMember::advanceLevel() {
 /**
  * Apply an effect to the party member
  */
-void PartyMember::applyEffect(TileEffect effect) {
+void PartyMember::applyEffect(Map* map, TileEffect effect) {
     if (isDead())
         return;
 
@@ -202,7 +202,7 @@ void PartyMember::applyEffect(TileEffect effect) {
         break;
     case EFFECT_LAVA:
     case EFFECT_FIRE:
-        applyDamage(16 + (xu4_random(32)));
+        applyDamage(map, 16 + (xu4_random(32)));
 
         /*else if (player == ALL_PLAYERS && xu4_random(2) == 0)
             playerApplyDamage(&(c->saveGame->players[i]), 10 + (xu4_random(25)));*/
@@ -359,7 +359,7 @@ EquipError PartyMember::setWeapon(const Weapon *w) {
  * there anything special about being killed by a party member in U5?)  Also
  * keeps interface consistent for virtual base function Creature::applydamage()
  */
-bool PartyMember::applyDamage(int damage, bool) {
+bool PartyMember::applyDamage(Map* map, int damage, bool) {
     int newHp = player->hp;
 
     if (isDead())
@@ -375,7 +375,6 @@ bool PartyMember::applyDamage(int damage, bool) {
     player->hp = newHp;
     notifyOfChange();
 
-    Map* map = c->location->map;
     if (isCombatMap(map) && isDead()) {
         TileId corpseId = Tileset::findTileByName(Tile::sym.corpse)->getId();
         map->annotations->add(coords, corpseId)->setTTL(party->size() * 2);
@@ -675,25 +674,25 @@ void Party::adjustKarma(KarmaAction action) {
 /**
  * Apply effects to the entire party
  */
-void Party::applyEffect(TileEffect effect) {
+void Party::applyEffect(Map* map, TileEffect effect) {
     int i;
 
     for (i = 0; i < size(); i++) {
         switch(effect) {
         case EFFECT_NONE:
         case EFFECT_ELECTRICITY:
-            members[i]->applyEffect(effect);
+            members[i]->applyEffect(map, effect);
             break;
         case EFFECT_LAVA:
         case EFFECT_FIRE:
         case EFFECT_SLEEP:
             if (xu4_random(2) == 0)
-                members[i]->applyEffect(effect);
+                members[i]->applyEffect(map, effect);
             break;
         case EFFECT_POISONFIELD:
         case EFFECT_POISON:
             if (xu4_random(5) == 0)
-                members[i]->applyEffect(effect);
+                members[i]->applyEffect(map, effect);
             break;
         }
     }
@@ -787,6 +786,7 @@ bool Party::donate(int quantity) {
  * Ends the party's turn
  */
 void Party::endTurn() {
+    Location* loc = c->location;
     int i;
 
     saveGame->moves++;
@@ -794,7 +794,7 @@ void Party::endTurn() {
     for (i = 0; i < size(); i++) {
 
         /* Handle player status (only for non-combat turns) */
-        if ((c->location->context & CTX_NON_COMBAT) == c->location->context) {
+        if ((loc->context & CTX_NON_COMBAT) == loc->context) {
 
             /* party members eat food (also non-combat) */
             if (!members[i]->isDead())
@@ -814,7 +814,7 @@ void Party::endTurn() {
                  * screen appears
                  */
                 soundPlay(SOUND_POISON_DAMAGE, false);
-                members[i]->applyDamage(2);
+                members[i]->applyDamage(loc->map, 2);
                 break;
 
             default:
@@ -828,14 +828,14 @@ void Party::endTurn() {
     }
 
     /* The party is starving! */
-    if ((saveGame->food == 0) && ((c->location->context & CTX_NON_COMBAT) == c->location->context)) {
+    if ((saveGame->food == 0) && ((loc->context & CTX_NON_COMBAT) == loc->context)) {
         setChanged();
         PartyEvent event(PartyEvent::STARVING, 0);
         notifyObservers(event);
     }
 
     /* heal ship (25% chance it is healed each turn) */
-    if ((c->location->context == CTX_WORLDMAP) && (saveGame->shiphull < 50) && xu4_random(4) == 0)
+    if ((loc->context == CTX_WORLDMAP) && (saveGame->shiphull < 50) && xu4_random(4) == 0)
         healShip(1);
 }
 

src/player.h
@@ -116,7 +116,7 @@ public:
     virtual void addStatus(StatusType status);
     void adjustMp(int pts);
     void advanceLevel();
-    void applyEffect(TileEffect effect);
+    void applyEffect(Map*, TileEffect effect);
     void awardXp(int xp);
     bool heal(HealType type);
     virtual void removeStatus(StatusType status);
@@ -125,7 +125,7 @@ public:
     EquipError setArmor(const Armor *a);
     EquipError setWeapon(const Weapon *w);
 
-    virtual bool applyDamage(int damage, bool byplayer = false);
+    virtual bool applyDamage(Map*, int damage, bool byplayer = false);
     virtual int getAttackBonus() const;
     virtual int getDefense() const;
     virtual bool dealDamage(Map*, Creature *m, int damage);
@@ -180,7 +180,7 @@ public:
     void adjustFood(int food);
     void adjustGold(int gold);
     void adjustKarma(KarmaAction action);
-    void applyEffect(TileEffect effect);
+    void applyEffect(Map*, TileEffect effect);
     bool attemptElevation(Virtue virtue);
     bool burnTorch(int turns = 1);
     bool canEnterShrine(Virtue virtue);

src/script_boron.cpp
@@ -141,7 +141,7 @@ CFUNC(cf_damagePc)
 
     if (player > 0 && hp > 0) {
         PartyMember* p = c->party->member(player - 1);
-        p->applyDamage(hp);
+        p->applyDamage(c->location->map, hp);
     }
 
     ur_setId(res, UT_UNSET);