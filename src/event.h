xu4-engine/u4

commit 107bcb9b864e813335e961ab8139b37c839f3c37

Added new classes for event handling:     - KeyHandler     - TimedEvent     - TimedEventMgr     - EventHandler

src/event.h
@@ -5,14 +5,11 @@
 #ifndef EVENT_H
 #define EVENT_H
 
+#include <list>
 #include <string>
 #include "types.h"
 
 using std::string;
-
-#ifdef __cplusplus
-extern "C" {
-#endif
 
 #define U4_UP           '['
 #define U4_DOWN         '/'
@@ -39,58 +36,166 @@ extern int eventTimerGranularity;
 
 struct _MouseArea;
 
-typedef bool (*KeyHandler)(int, void *);
-typedef void (*TimerCallback)(void *);
+/**
+ * A class for handling keystrokes. 
+ */
+class KeyHandler {
+public:
+    /* Typedefs */
+    typedef bool (*Callback)(int, void*);
 
-/** Additional information to be passed as data param for read buffer key handler */
-typedef struct ReadBufferActionInfo {
-    int (*handleBuffer)(string*);
-    string *buffer;
-    int bufferLen;
-    int screenX, screenY;
-} ReadBufferActionInfo;
+    /** Additional information to be passed as data param for read buffer key handler */
+    typedef struct ReadBuffer {        
+        int (*handleBuffer)(string*);
+        string *buffer;
+        int bufferLen;
+        int screenX, screenY;
+    } ReadBuffer;
 
-/** Additional information to be passed as data param for get choice key handler */
-typedef struct GetChoiceActionInfo {
-    string choices;
-    int (*handleChoice)(int);
-} GetChoiceActionInfo;
+    /** Additional information to be passed as data param for get choice key handler */
+    typedef struct GetChoice {
+        string choices;
+        int (*handleChoice)(int);
+    } GetChoice;
 
-void eventHandlerInit(void);
-void eventHandlerDelete(void);
-void eventHandlerSleep(int usec);
-void eventHandlerMain(void (*updateScreen)(void));
-bool eventHandlerTimerQueueEmpty(void);
-void eventHandlerSetExitFlag(bool flag);
-bool eventHandlerGetExitFlag();
-void eventHandlerAddTimerCallback(TimerCallback callback, int interval);
-void eventHandlerAddTimerCallbackData(TimerCallback callback, void *data, int interval);
-void eventHandlerRemoveTimerCallback(TimerCallback callback);
-void eventHandlerRemoveTimerCallbackData(TimerCallback callback, void *data);
-void eventHandlerCallTimerCallbacks();
-void eventHandlerResetTimerCallbacks();
-void eventHandlerPushKeyHandler(KeyHandler kh);
-void eventHandlerPushKeyHandlerWithData(KeyHandler kh, void *data);
-void eventHandlerPopKeyHandler();
-void eventHandlerPopKeyHandlerData();
-void eventHandlerSetKeyHandler(KeyHandler kh);
-KeyHandler eventHandlerGetKeyHandler();
-void *eventHandlerGetKeyHandlerData();
-bool eventHandlerIsKeyIgnored(int key);
-bool eventHandlerUniversalKeyHandler(int key);
-int eventKeyboardSetKeyRepeat(int delay, int interval);
+    /* Constructors */
+    KeyHandler(Callback func, void *data = NULL, bool asyncronous = true);
+    
+    /* Static functions */    
+    static int setKeyRepeat(int delay, int interval);
+    static bool globalHandler(int key);    
 
-bool keyHandlerDefault(int key, void *data);
-bool keyHandlerIgnoreKeys(int key, void *data);
-bool keyHandlerReadBuffer(int key, void *data);
+    /* Static default key handler functions */
+    static bool defaultHandler(int key, void *data);
+    static bool ignoreKeys(int key, void *data);
+
+    /* Operators */
+    bool operator==(Callback cb) const;
+    
+    /* Member functions */    
+    bool handle(int key); 
+    virtual bool isKeyIgnored(int key);
+
+protected:
+    Callback handler;
+    bool async;
+    void *data;
+};
+
+/**
+ * A class for handling timed events.
+ */ 
+class TimedEvent {
+public:
+    /* Typedefs */
+    typedef std::list<TimedEvent*> List;
+    typedef void (*Callback)(void *);
+
+    /* Constructors */
+    TimedEvent(Callback callback, int interval, void *data = NULL);
+
+    /* Member functions */
+    Callback getCallback() const;
+    void *getData();
+    void tick();
+    
+    /* Properties */
+protected:    
+    Callback callback;
+    void *data;
+    int interval;
+    int current;
+};
+
+/**
+ * A class for managing timed events
+ */ 
+class TimedEventMgr {
+public:
+    /* Typedefs */
+    typedef TimedEvent::List List;    
+
+    /* Constructors */
+    TimedEventMgr(int baseInterval);
+    ~TimedEventMgr();
+
+    /* Static functions */
+    static unsigned int callback(unsigned int interval, void *param);
+
+    /* Member functions */
+    bool isLocked() const;      /**< Returns true if the event list is locked (in use) */    
+
+    void add(TimedEvent::Callback callback, int interval, void *data = NULL);
+    List::iterator remove(List::iterator i);                          
+    void remove(TimedEvent* event);
+    void remove(TimedEvent::Callback callback, void *data = NULL);
+    void tick();
+    
+    void reset(unsigned int interval);     /**< Re-initializes the event manager to a new base interval */
+
+private:
+    void lock();                /**< Locks the event list */
+    void unlock();              /**< Unlocks the event list */
+
+    /* Properties */
+protected:
+    /* Static properties */
+    static unsigned int instances;
+
+    void *id;
+    int baseInterval;
+    bool locked;
+    List events;
+    List deferredRemovals;
+};
+
+
+/**
+ * A class for handling game events. 
+ */
+class EventHandler {
+public:    
+    /* Typedefs */
+    typedef std::list<KeyHandler*> KeyHandlerList;
+    typedef std::list<_MouseArea*> MouseAreaList;    
+
+    /* Constructors */
+    EventHandler();    
+
+    /* Static functions */    
+    static void sleep(unsigned int usec);    
+    static void setExitFlag(bool exit = true);
+    static bool getExitFlag();
+    static bool timerQueueEmpty();
+
+    /* Member functions */
+    TimedEventMgr* getTimer();
+
+    /* Event functions */
+    void main(void (*updateScreen)(void));    
+
+    /* Key handler functions */
+    void pushKeyHandler(KeyHandler kh);
+    KeyHandler *popKeyHandler();
+    KeyHandler *getKeyHandler() const;
+    void setKeyHandler(KeyHandler kh);
+
+    /* Mouse area functions */
+    void pushMouseAreaSet(_MouseArea *mouseAreas);
+    void popMouseAreaSet();
+    _MouseArea* getMouseAreaSet() const;
+    _MouseArea* mouseAreaForPoint(int x, int y);
+
+protected:    
+    static bool exit;
+    TimedEventMgr timer;
+    KeyHandlerList keyHandlers;
+    MouseAreaList mouseAreaSets;
+};
+
 bool keyHandlerGetChoice(int key, void *data);
+bool keyHandlerReadBuffer(int key, void *data);
 
-void eventHandlerPushMouseAreaSet(struct _MouseArea *mouseAreas);
-void eventHandlerPopMouseAreaSet(void);
-struct _MouseArea *eventHandlerGetMouseAreaSet(void);
-
-#ifdef __cplusplus
-}
-#endif
+extern EventHandler eventHandler;
 
 #endif

src/camp.cpp
@@ -48,16 +48,16 @@ void campBegin(void) {
     c->combat->initCamping();
     c->combat->begin();    
     
-    eventHandlerPushKeyHandler(&keyHandlerIgnoreKeys);
-    eventHandlerAddTimerCallback(&campTimer, (eventTimerGranularity * settings.gameCyclesPerSecond) * settings.campTime);
+    eventHandler.pushKeyHandler(&KeyHandler::ignoreKeys);
+    eventHandler.getTimer()->add(&campTimer, settings.gameCyclesPerSecond * settings.campTime);
 
     screenMessage("Resting...\n");
     screenDisableCursor();
 }
 
 void campTimer(void *data) {
-    eventHandlerRemoveTimerCallback(&campTimer);
-    eventHandlerPopKeyHandler();
+    eventHandler.getTimer()->remove(&campTimer);
+    eventHandler.popKeyHandler();
     screenEnableCursor();
 
     /* Is the party ambushed during their rest? */
@@ -83,7 +83,7 @@ void campTimer(void *data) {
 void campEnd(void) {
     int i, healed = 0;    
 
-    eventHandlerPopKeyHandler();
+    eventHandler.popKeyHandler();
     gameExitToParentMap();
     musicFadeIn(CAMP_FADE_IN_TIME, 1);
     
@@ -127,27 +127,27 @@ void innBegin(void) {
     if (settings.enhancements)
         musicFadeOut(INN_FADE_OUT_TIME); /* Fade volume out to ease into rest */
 
-    eventHandlerSleep(INN_FADE_OUT_TIME);
+    EventHandler::sleep(INN_FADE_OUT_TIME);
 
     /* show the sleeping avatar */
     c->party->setTransport(Tileset::findTileByName("corpse")->id);
     gameUpdateScreen();
 
-    eventHandlerPushKeyHandler(&keyHandlerIgnoreKeys);
-    eventHandlerAddTimerCallback(&innTimer, (eventTimerGranularity * settings.gameCyclesPerSecond) * settings.innTime);
+    eventHandler.pushKeyHandler(&KeyHandler::ignoreKeys);
+    eventHandler.getTimer()->add(&innTimer, settings.gameCyclesPerSecond * settings.innTime);
 
     screenDisableCursor();
 }
 
 void innTimer(void *data) {
-    eventHandlerRemoveTimerCallback(&innTimer);    
+    eventHandler.getTimer()->remove(&innTimer);    
 
     /**
      * FIXME: normally we would "pop" the key handler
      * here, but for some reason the "ignore key"
      * handler doesn't pop every time.  Weird...
      **/
-    eventHandlerSetKeyHandler(&gameBaseKeyHandler);
+    eventHandler.setKeyHandler(&gameBaseKeyHandler);
     screenEnableCursor();
 
     /* restore the avatar to normal */

src/codex.cpp
@@ -95,7 +95,7 @@ void codexStart() {
     gameSetViewMode(VIEW_CODEX);
 
     screenMessage("\n\n\n\nThere is a sudden darkness, and you find yourself alone in an empty chamber.\n");    
-    eventHandlerSleep(4000);
+    EventHandler::sleep(4000);
 
     /**
      * check to see if you have the 3-part key
@@ -109,7 +109,7 @@ void codexStart() {
     screenRedrawMapArea();
 
     screenMessage("\nYou use your key of Three Parts.\n");
-    eventHandlerSleep(3000);
+    EventHandler::sleep(3000);
 
     screenMessage("\nA voice rings out:\n\"What is the Word of Passage?\"\n\n");    
     
@@ -143,7 +143,7 @@ void codexEject(CodexEjectCode code) {
         break;    
     case CODEX_EJECT_NO_FULL_AVATAR:
         screenMessage("\nThou art not ready.\n");
-        eventHandlerSleep(2000);
+        EventHandler::sleep(2000);
         screenMessage("\nPassage is not granted.\n\n");
         break;
     case CODEX_EJECT_BAD_WOP:        
@@ -170,7 +170,7 @@ void codexEject(CodexEjectCode code) {
         break;
     }
 
-    eventHandlerSleep(2000);
+    EventHandler::sleep(2000);
 
     /* free memory associated with the Codex */
     codexDelete();
@@ -205,12 +205,12 @@ int codexHandleWOP(string *word) {
     static int tries = 1;
     int i;
 
-    eventHandlerPopKeyHandler();    
+    eventHandler.popKeyHandler();    
 
     /* slight pause before continuing */
     screenMessage("\n");    
     screenDisableCursor();
-    eventHandlerSleep(1000);    
+    EventHandler::sleep(1000);    
         
     /* entered correctly */
     if (strcasecmp(word->c_str(), "veramocor") == 0) {        
@@ -225,14 +225,14 @@ int codexHandleWOP(string *word) {
         }        
         
         screenMessage("\nPassage is granted.\n");
-        eventHandlerSleep(4000);
+        EventHandler::sleep(4000);
 
         screenEraseMapArea();
         screenRedrawMapArea();
 
         /* Ask the Virtue questions */
         screenMessage("\n\nThe voice asks:\n");
-        eventHandlerSleep(2000);
+        EventHandler::sleep(2000);
         screenMessage("\n%s\n\n", codexVirtueQuestions[0].c_str());
 
         gameGetInput(&codexHandleVirtues, &codexInputBuffer);
@@ -270,12 +270,12 @@ int codexHandleVirtues(string *virtue) {
     static int current = 0;
     static int tries = 1;
 
-    eventHandlerPopKeyHandler();
+    eventHandler.popKeyHandler();
 
     /* slight pause before continuing */    
     screenMessage("\n");
     screenDisableCursor();    
-    eventHandlerSleep(1000);    
+    EventHandler::sleep(1000);    
         
     /* answered with the correct one of eight virtues */
     if ((current < VIRT_MAX) && 
@@ -287,15 +287,15 @@ int codexHandleVirtues(string *virtue) {
         current++;
         tries = 1;
 
-        eventHandlerSleep(2000);
+        EventHandler::sleep(2000);
 
         if (current == VIRT_MAX) {
             screenMessage("\nThou art well versed in the virtues of the Avatar.\n");
-            eventHandlerSleep(5000);
+            EventHandler::sleep(5000);
         }
 
         screenMessage("\n\nThe voice asks:\n");
-        eventHandlerSleep(2000);
+        EventHandler::sleep(2000);
         screenMessage("\n%s\n\n", codexVirtueQuestions[current].c_str());
 
         gameGetInput(&codexHandleVirtues, &codexInputBuffer);
@@ -313,20 +313,20 @@ int codexHandleVirtues(string *virtue) {
 
         if (current < VIRT_MAX+3) {
             screenMessage("\n\nThe voice asks:\n");
-            eventHandlerSleep(2000);
+            EventHandler::sleep(2000);
             screenMessage("\n%s\n\n", codexVirtueQuestions[current].c_str());
     
             gameGetInput(&codexHandleVirtues, &codexInputBuffer);
         }
         else {
             screenMessage("\nThe ground rumbles beneath your feet.\n");
-            eventHandlerSleep(1000);
+            EventHandler::sleep(1000);
             screenShake(10);
 
-            eventHandlerSleep(3000);
+            EventHandler::sleep(3000);
             screenEnableCursor();
             screenMessage("\nAbove the din, the voice asks:\n\nIf all eight virtues of the Avatar combine into and are derived from the Three Principles of Truth, Love and Courage...");
-            eventHandlerPushKeyHandler(&codexHandleInfinityAnyKey);
+            eventHandler.pushKeyHandler(&codexHandleInfinityAnyKey);
         }
     }
     
@@ -350,7 +350,7 @@ int codexHandleVirtues(string *virtue) {
 }
 
 bool codexHandleInfinityAnyKey(int key, void *data) {
-    eventHandlerPopKeyHandler();
+    eventHandler.popKeyHandler();
 
     screenMessage("\n\nThen what is the one thing which encompasses and is the whole of all undeniable Truth, unending Love, and unyielding Courage?\n\n");
     gameGetInput(&codexHandleInfinity, &codexInputBuffer);
@@ -360,25 +360,25 @@ bool codexHandleInfinityAnyKey(int key, void *data) {
 int codexHandleInfinity(string *answer) {
     static int tries = 1;
 
-    eventHandlerPopKeyHandler();
+    eventHandler.popKeyHandler();
 
     /* slight pause before continuing */    
     screenMessage("\n");
     screenDisableCursor();
-    eventHandlerSleep(1000);
+    EventHandler::sleep(1000);
 
     if (strcasecmp(answer->c_str(), "infinity") == 0) {        
-        eventHandlerSleep(2000);
+        EventHandler::sleep(2000);
         screenShake(10);
         
         screenEnableCursor();
         screenMessage("\n%s", codexEndgameText1[0].c_str());
-        eventHandlerPushKeyHandler(&codexHandleEndgameAnyKey); 
+        eventHandler.pushKeyHandler(&codexHandleEndgameAnyKey); 
     }
     else if (tries++ < 3) {
         codexImpureThoughts();
         screenMessage("\nAbove the din, the voice asks:\n\nIf all eight virtues of the Avatar combine into and are derived from the Three Principles of Truth, Love and Courage...");
-        eventHandlerPushKeyHandler(&codexHandleInfinityAnyKey);
+        eventHandler.pushKeyHandler(&codexHandleInfinityAnyKey);
     }
     else codexEject(CODEX_EJECT_BAD_INFINITY);
 
@@ -388,7 +388,7 @@ int codexHandleInfinity(string *answer) {
 bool codexHandleEndgameAnyKey(int key, void *data) {
     static int index = 1;
 
-    eventHandlerPopKeyHandler();
+    eventHandler.popKeyHandler();
 
     if (index < 10) {
 
@@ -408,13 +408,13 @@ bool codexHandleEndgameAnyKey(int key, void *data) {
             screenMessage(codexEndgameText2[index-7].c_str());
     
         index++;
-        eventHandlerPushKeyHandler(&codexHandleEndgameAnyKey);
+        eventHandler.pushKeyHandler(&codexHandleEndgameAnyKey);
     }
     else {
         /* CONGRATULATIONS!... you have completed the game in x turns */    
         screenDisableCursor();
         screenMessage("%s%d%s", codexEndgameText2[index-7].c_str(), c->saveGame->moves, codexEndgameText2[index-6].c_str());
-        eventHandlerPushKeyHandler(&keyHandlerIgnoreKeys);        
+        eventHandler.pushKeyHandler(&KeyHandler::ignoreKeys);        
     }
 
     return true;
@@ -425,5 +425,5 @@ bool codexHandleEndgameAnyKey(int key, void *data) {
  */
 void codexImpureThoughts() {
     screenMessage("\nThy thoughts are not pure.\nI ask again.\n");
-    eventHandlerSleep(2000);
+    EventHandler::sleep(2000);
 }

src/combat.cpp
@@ -229,7 +229,7 @@ void CombatController::begin() {
     }
 
     /* Use the combat key handler */
-    eventHandlerPushKeyHandler(&CombatController::baseKeyHandler);
+    eventHandler.pushKeyHandler(&CombatController::baseKeyHandler);
  
     /* if there are creatures around, start combat! */    
     if (showMessage && placeCreaturesOnMap && winOrLose)
@@ -564,7 +564,7 @@ bool CombatController::attackAtCoord(MapCoords coords, int distance, void *data)
                 /* Based on attack speed setting in setting struct, make a delay for
                    the attack annotation */
                 if (attackdelay > 0)
-                    eventHandlerSleep(attackdelay * 2);
+                    EventHandler::sleep(attackdelay * 2);
             }       
 
             return 0;
@@ -648,7 +648,7 @@ bool CombatController::rangedAttack(MapCoords coords, int distance, void *data) 
             /* Based on attack speed setting in setting struct, make a delay for
                the attack annotation */
             if (attackdelay > 0)
-                eventHandlerSleep(attackdelay * 2);
+                EventHandler::sleep(attackdelay * 2);
 
             return 0;
         }
@@ -748,7 +748,7 @@ bool CombatController::returnWeaponToOwner(MapCoords coords, int distance, void 
         /* Based on attack speed setting in setting struct, make a delay for
            the attack annotation */
         if (attackdelay > 0)
-            eventHandlerSleep(attackdelay * 2);
+            EventHandler::sleep(attackdelay * 2);
         
         cm->annotations->remove(new_coords, misstile);
     }
@@ -773,7 +773,7 @@ void CombatController::attackFlash(Coords coords, MapTile tile, int timeFactor) 
             screenCycle();
 
         gameUpdateScreen();       
-        eventHandlerSleep(eventTimerGranularity/divisor);
+        EventHandler::sleep(eventTimerGranularity/divisor);
     }
     c->location->map->annotations->remove(coords, tile);
 }
@@ -787,7 +787,7 @@ void CombatController::finishTurn(void) {
     c->stats->showPartyView();    
 
     if (ct->isWon() && ct->winOrLose) {
-        eventHandlerPopKeyHandler();
+        eventHandler.popKeyHandler();
         ct->end(true);
         return;
     }
@@ -831,7 +831,7 @@ void CombatController::finishTurn(void) {
                 player = ct->getCurrentPlayer();
 
                 gameUpdateScreen();
-                eventHandlerSleep(50); /* give a slight pause in case party members are asleep for awhile */
+                EventHandler::sleep(50); /* give a slight pause in case party members are asleep for awhile */
 
                 /* adjust moves */
                 c->party->endTurn();
@@ -853,14 +853,14 @@ void CombatController::finishTurn(void) {
 
                 /* check to see if combat is over */
                 if (ct->isLost()) {                    
-                    eventHandlerPopKeyHandler();
+                    eventHandler.popKeyHandler();
                     ct->end(true);
                     return;
                 }
 
                 /* end combat immediately if the enemy has fled */
                 else if (ct->isWon() && ct->winOrLose) {
-                    eventHandlerPopKeyHandler();
+                    eventHandler.popKeyHandler();
                     ct->end(true);
                     return;
                 }
@@ -937,7 +937,7 @@ bool CombatController::baseKeyHandler(int key, void *data) {
 
     case U4_ESC:
         if (settings.debug) {
-            eventHandlerPopKeyHandler();
+            eventHandler.popKeyHandler();
             ct->end(false); /* don't adjust karma */
         }
         else screenMessage("Bad command\n");        
@@ -973,9 +973,8 @@ bool CombatController::baseKeyHandler(int key, void *data) {
         info->blockBefore = 1;
         info->firstValidDistance = 0;
         
-        eventHandlerPushKeyHandlerWithData(&CombatController::chooseWeaponDir, info);        
-
-        screenMessage("Dir: ");
+        screenMessage("Dir: ");        
+        eventHandler.pushKeyHandler(KeyHandler(&CombatController::chooseWeaponDir, info));
         break;
 
     case 'c':
@@ -1009,7 +1008,7 @@ bool CombatController::baseKeyHandler(int key, void *data) {
 
             screenMessage(alphaInfo->prompt.c_str());
 
-            eventHandlerPushKeyHandlerWithData(&gameGetAlphaChoiceKeyHandler, alphaInfo);
+            eventHandler.pushKeyHandler(KeyHandler(&gameGetAlphaChoiceKeyHandler, alphaInfo));
         }
         break;
 
@@ -1065,7 +1064,7 @@ bool CombatController::baseKeyHandler(int key, void *data) {
                and hide reagents that you don't have */            
             gameResetSpellMixing();
 
-            eventHandlerPushKeyHandler(&gameZtatsKeyHandler);
+            eventHandler.pushKeyHandler(&gameZtatsKeyHandler);
             screenMessage("Ztats\n");        
         }
         break;    
@@ -1113,7 +1112,7 @@ bool CombatController::baseKeyHandler(int key, void *data) {
 
     if (valid) {
         c->lastCommandTime = time(NULL);
-        if (eventHandlerGetKeyHandler() == &CombatController::baseKeyHandler &&
+        if (*eventHandler.getKeyHandler() == &CombatController::baseKeyHandler &&
             c->location->finishTurn == &CombatController::finishTurn)
             (*c->location->finishTurn)();
     }
@@ -1132,8 +1131,8 @@ bool CombatController::chooseWeaponRange(int key, void *data) {
         screenMessage("%d\n", info->range);
         gameDirectionalAction(info);
 
-        eventHandlerPopKeyHandler();
-        eventHandlerPopKeyHandlerData();
+        eventHandler.popKeyHandler();
+        //eventHandler.popKeyHandlerData();
 
         return true;
     }
@@ -1146,26 +1145,26 @@ bool CombatController::chooseWeaponRange(int key, void *data) {
  */
 bool CombatController::chooseWeaponDir(int key, void *data) {
     CombatController *ct = c->combat;
-    CoordActionInfo *info = (CoordActionInfo *)eventHandlerGetKeyHandlerData();
+    CoordActionInfo *info = (CoordActionInfo *)data;
     Direction dir = keyToDirection(key);
     bool valid = (dir != DIR_NONE) ? true : false;
     WeaponType weapon = ct->party[info->player]->getWeapon();
 
-    eventHandlerPopKeyHandler();
+    eventHandler.popKeyHandler();
     info->dir = MASK_DIR(dir);
 
     if (valid) {
         screenMessage("%s\n", getDirectionName(dir));
         if (Weapon::get(weapon)->canChooseDistance()) {
             screenMessage("Range: ");
-            eventHandlerPushKeyHandlerWithData(&CombatController::chooseWeaponRange, info);
+            eventHandler.pushKeyHandler(KeyHandler(&CombatController::chooseWeaponRange, info));
         }
         else gameDirectionalAction(info);        
     }
 
-    eventHandlerPopKeyHandlerData();
+    //eventHandler.popKeyHandlerData();
     
-    return valid || keyHandlerDefault(key, NULL);
+    return valid || KeyHandler::defaultHandler(key, NULL);
 }
 
 /**

src/death.cpp
@@ -60,14 +60,14 @@ void deathStart(int delay) {
     timerCount = 0;
     timerMsg = 0;    
 
-    eventHandlerSleep(delay * 1000);
+    EventHandler::sleep(delay * 1000);
     
     gameSetViewMode(VIEW_DEAD);
     
-    eventHandlerPushKeyHandler(&keyHandlerIgnoreKeys);
+    eventHandler.pushKeyHandler(&KeyHandler::ignoreKeys);
     screenDisableCursor();
 
-    eventHandlerAddTimerCallback(&deathTimer, eventTimerGranularity * settings.gameCyclesPerSecond);
+    eventHandler.getTimer()->add(&deathTimer, settings.gameCyclesPerSecond);
 }
 
 void deathTimer(void *data) {
@@ -82,7 +82,7 @@ void deathTimer(void *data) {
         timerMsg++;
 
         if (timerMsg >= N_MSGS) {
-            eventHandlerRemoveTimerCallback(&deathTimer);
+            eventHandler.getTimer()->remove(&deathTimer);
             deathRevive();
         }
     }
@@ -96,8 +96,8 @@ void deathRevive() {
     deathSequenceRunning = 0;
     gameSetViewMode(VIEW_NORMAL);
 
-    eventHandlerSetKeyHandler(&keyHandlerDefault);
-    eventHandlerPushKeyHandler(&gameBaseKeyHandler);
+    eventHandler.setKeyHandler(&KeyHandler::defaultHandler);
+    eventHandler.pushKeyHandler(&gameBaseKeyHandler);
 
     /* Move our world map location to Lord British's Castle */
     c->location->coords = c->location->map->portals[0]->coords;

commit dc5c1a5c370e8e37301dfa60f6716835f1e3348a

xu4.cpp: Fix --test-save segfaults.
 - Initialize TimedEventMgr::locked (broken since commit 107bcb9b).

src/event.h
@@ -206,6 +206,7 @@ class TimedEventMgr {
 public:
     typedef std::list<TimedEvent*> List;
 
+    TimedEventMgr() : locked(false) {}
     ~TimedEventMgr();
 
     /** Returns true if the event list is locked (in use) */

src/xu4.cpp
@@ -261,11 +261,18 @@ int main(int argc, char *argv[]) {
 
 #ifdef DEBUG
     if (opt.flags & OPT_TEST_SAVE) {
+        int status;
         xu4.game = new GameController();
-        xu4.game->initContext();
-        gameSave("/tmp/xu4/");
+        if (xu4.game->initContext()) {
+            gameSave("/tmp/xu4/");
+            status = 0;
+        } else {
+            printf("initContext failed!\n");
+            status = 1;
+        }
+        xu4.stage = StageExitGame;
         servicesFree(&xu4);
-        return 0;
+        return status;
     }
 #endif
     }