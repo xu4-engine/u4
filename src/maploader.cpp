xu4-engine/u4

commit 3be19603733d16b8f4ecf7474ae07e5343ecefab

map: Implement city grass borders for GPU rendering.
Add Map::boundMaxX & boundMaxY to define a boundary separate from the data.
This will allow for unique border content around the entire map.

 - gpu_drawMap: Fix drawing of wrapped map edges.
 - MAP_IS_OOB now calls map_outOfBounds().

src/maploader.cpp
@@ -29,8 +29,9 @@ static bool isChunkCompressed(Map *map, int chunk) {
 /**
  * Loads raw data from the given file.
  */
-static bool loadMapData(Map *map, U4FILE *uf) {
+static bool loadMapData(Map *map, U4FILE *uf, Symbol borderTile) {
     unsigned int x, xch, y, ych, di;
+    unsigned int chunkCols, chunkRows;
     uint8_t* chunk;
     uint8_t* cp;
     const UltimaSaveIds* usaveIds = xu4.config->usaveIds();
@@ -39,13 +40,41 @@ static bool loadMapData(Map *map, U4FILE *uf) {
     Symbol sym_sea = SYM_UNSET;
 #endif
 
-    /* allocate the space we need for the map data */
-    map->data = new TileId[map->width * map->height];
-
     if (map->chunk_height == 0)
         map->chunk_height = map->height;
     if (map->chunk_width == 0)
         map->chunk_width = map->width;
+
+    map->boundMaxX = map->width;
+    map->boundMaxY = map->height;
+
+    chunkCols = map->width / map->chunk_width;
+    chunkRows = map->height / map->chunk_height;
+
+#ifdef GPU_RENDER
+    bool addBorder = false;
+    if (map->border_behavior == Map::BORDER_EXIT2PARENT && borderTile) {
+        // gpu_drawMap() always wraps the map so adding border chunks to the
+        // bottom & right edges will render these borders on all edges.
+        map->width  += map->chunk_width;
+        map->height += map->chunk_height;
+        addBorder = true;
+    }
+#endif
+
+    /* allocate the space we need for the map data */
+    map->data = new TileId[map->width * map->height];
+
+#ifdef GPU_RENDER
+    if (addBorder) {
+        // Fill the entire map with borderTile.
+        TileId borderId = Tileset::findTileByName(borderTile)->id;
+        TileId* it = map->data;
+        TileId* end = it + map->width * map->height;
+        while (it != end)
+            *it++ = borderId;
+    }
+#endif
 
     size_t chunkLen = map->chunk_width * map->chunk_height;
     chunk = new uint8_t[chunkLen];
@@ -53,8 +82,8 @@ static bool loadMapData(Map *map, U4FILE *uf) {
     if (map->offset)
         u4fseek(uf, map->offset, SEEK_CUR);
 
-    for(ych = 0; ych < (map->height / map->chunk_height); ++ych) {
-        for(xch = 0; xch < (map->width / map->chunk_width); ++xch) {
+    for(ych = 0; ych < chunkRows; ++ych) {
+        for(xch = 0; xch < chunkCols; ++xch) {
             di = (xch * map->chunk_width) +
                  (ych * map->chunk_height * map->width);
 
@@ -132,7 +161,7 @@ static bool loadCityMap(Map *map, U4FILE *ult) {
     ASSERT(city->width == CITY_WIDTH, "map width is %d, should be %d", city->width, CITY_WIDTH);
     ASSERT(city->height == CITY_HEIGHT, "map height is %d, should be %d", city->height, CITY_HEIGHT);
 
-    if (! loadMapData(city, ult))
+    if (! loadMapData(city, ult, Tile::sym.grass))
         return false;
 
     data = new uint8_t[PD_SIZE];
@@ -260,7 +289,7 @@ static bool loadCombatMap(Map *map, U4FILE *uf) {
         u4fseek(uf, 16L, SEEK_CUR);
     }
 
-    return loadMapData(map, uf);
+    return loadMapData(map, uf, SYM_UNSET);
 }
 
 /**
@@ -464,7 +493,7 @@ bool loadMap(Map *map, FILE* sav) {
                 break;
 
             case Map::WORLD:
-                ok = loadMapData(map, uf);
+                ok = loadMapData(map, uf, SYM_UNSET);
                 break;
         }
         u4fclose(uf);

src/gpu_opengl.cpp
@@ -775,7 +775,7 @@ void gpu_resetMap(void* res, const Map* map)
     }
 
     // Clear chunk cache.
-    memset(gr->mapChunkLoc, 0xff, 4*sizeof(uint16_t));
+    memset(gr->mapChunkId, 0xff, 4*sizeof(uint16_t));
 }
 
 #define VIEW_TILE_SIZE  (2.0f / VIEWPORT_W)
@@ -833,21 +833,32 @@ static void _buildChunkGeo(GLuint vbo, const TileId* chunk, int dim,
     glUnmapBuffer(GL_ARRAY_BUFFER);
 }
 
+struct ChunkLoc {
+    int16_t x, y;
+};
+
 struct ChunkInfo {
     const TileId* mapData;
     const TileRenderData* renderData;
     const float* uvs;
     const GLuint* vbo;
-    uint16_t* mapChunkLoc;
+    uint16_t* mapChunkId;
+    ChunkLoc* chunkLoc;
     int mapW;
     int mapH;
     int cdim;
     int geoUsedMask;
 };
 
-#define WRAP(x,w) \
-    if (x < 0) x += w; \
-    else if (x >= w) x -= w;
+#define WRAP(x,loc,limit) \
+    if (x < 0) { \
+        x += limit; \
+        loc = -limit; \
+    } else if (x >= limit) { \
+        x -= limit; \
+        loc = limit; \
+    } else \
+        loc = 0;
 
 // LIMIT: Maximum of 256x256 chunks.
 #define CHUNK_ID(c,r)       (c<<8 | r)
@@ -866,10 +877,12 @@ static int _obtainChunkGeo(ChunkInfo* ci, int x, int y, int bumpPass)
 {
     int i;
     int ccol, crow;
+    int wx, wy;
     uint16_t chunkId;
+    ChunkLoc* loc;
 
-    WRAP(x, ci->mapW);
-    WRAP(y, ci->mapH);
+    WRAP(x, wx, ci->mapW);
+    WRAP(y, wy, ci->mapH);
     ccol = x / ci->cdim;
     crow = y / ci->cdim;
     chunkId = CHUNK_ID(ccol, crow);
@@ -890,13 +903,13 @@ static int _obtainChunkGeo(ChunkInfo* ci, int x, int y, int bumpPass)
     {
         // Check if already made.
         for (i = 0; i < CHUNK_CACHE_SIZE; ++i) {
-            if (ci->mapChunkLoc[i] == chunkId)
+            if (ci->mapChunkId[i] == chunkId)
                 goto used;
         }
 
         // Find unused VBO.
         for (i = 0; i < CHUNK_CACHE_SIZE; ++i) {
-            if (ci->mapChunkLoc[i] == 0xffff)
+            if (ci->mapChunkId[i] == 0xffff)
                 goto build;
         }
 
@@ -905,11 +918,14 @@ static int _obtainChunkGeo(ChunkInfo* ci, int x, int y, int bumpPass)
     }
 
 build:
-    ci->mapChunkLoc[i] = chunkId;
+    ci->mapChunkId[i] = chunkId;
     _buildChunkGeo(ci->vbo[i],
                    ci->mapData + (crow * ci->mapW + ccol) * ci->cdim,
                    ci->cdim, ci->mapW, ci->renderData, ci->uvs);
 used:
+    loc = ci->chunkLoc + i;
+    loc->x = wx + (ccol * ci->cdim);
+    loc->y = wy + (crow * ci->cdim);
     ci->geoUsedMask |= 1 << i;
     return i;
 }
@@ -928,6 +944,7 @@ void gpu_drawMap(void* res, const Map* map, const float* tileUVs,
                  int cx, int cy, int viewRadius)
 {
     OpenGLResources* gr = (OpenGLResources*) res;
+    ChunkLoc cloc[4];   // Tile location of chunks on the map.
     int i, usedMask;
 
 #if 1
@@ -971,7 +988,8 @@ void gpu_drawMap(void* res, const Map* map, const float* tileUVs,
     ci.renderData = map->tileset->render;
     ci.uvs    = tileUVs;
     ci.vbo    = gr->vbo + GLOB_MAP_CHUNK0;
-    ci.mapChunkLoc = gr->mapChunkLoc;
+    ci.mapChunkId = gr->mapChunkId;
+    ci.chunkLoc = cloc;
     ci.mapW   = map->width;
     ci.mapH   = map->height;
     ci.cdim   = gr->mapChunkDim;
@@ -1003,8 +1021,6 @@ void gpu_drawMap(void* res, const Map* map, const float* tileUVs,
 
     {
     float matrix[16];
-    int wx, wy;         // Tile location of chunk on the map.
-    uint16_t chunkId;
 
     gr->time = ((float) getTicks()) * 0.001;
     memcpy(matrix, unitMatrix, sizeof(matrix));
@@ -1029,11 +1045,8 @@ void gpu_drawMap(void* res, const Map* map, const float* tileUVs,
     for (i = 0; i < 4; ++i) {
         if (usedMask & (1 << i)) {
             // Position chunk in viewport.
-            chunkId = gr->mapChunkLoc[i];
-            wx = (chunkId >> 8)  * gr->mapChunkDim;
-            wy = (chunkId & 255) * gr->mapChunkDim;
-            matrix[ MAT_X ] = (float) (wx - cx) * VIEW_TILE_SIZE;
-            matrix[ MAT_Y ] = (float) (cy - wy) * VIEW_TILE_SIZE;
+            matrix[ MAT_X ] = (float) (cloc[i].x - cx) * VIEW_TILE_SIZE;
+            matrix[ MAT_Y ] = (float) (cy - cloc[i].y) * VIEW_TILE_SIZE;
             glUniformMatrix4fv(gr->slocTrans, 1, GL_FALSE, matrix);
 
             glBindVertexArray(gr->vao[ GLOB_MAP_CHUNK0 + i ]);

src/gpu_opengl.h
@@ -67,5 +67,5 @@ struct OpenGLResources {
     int    blockCount;
     GLsizei mapChunkVertCount;
     uint16_t mapChunkDim;       // Size in tiles (width & height are the same).
-    uint16_t mapChunkLoc[4];    // Chunk X,Y of associated GLOB_MAP_CHUNK.
+    uint16_t mapChunkId[4];     // Chunk X,Y of associated GLOB_MAP_CHUNK.
 };

src/map.cpp
@@ -201,6 +201,12 @@ int map_distance(const Coords& a, const Coords& b, const Map *map) {
     return dist;
 }
 
+bool map_outOfBounds(const Map* map, const Coords& c) {
+    return (c.x < 0 || c.x >= (int) map->boundMaxX ||
+            c.y < 0 || c.y >= (int) map->boundMaxY ||
+            c.z < 0 || c.z >= (int) map->levels);
+}
+
 /**
  * Map Class Implementation
  */
@@ -213,6 +219,7 @@ Map::Map() {
     levels = 1;
     chunk_width = 0;
     chunk_height = 0;
+    boundMaxX = boundMaxY = 0;
     flags = 0;
     offset = 0;
     id = 0;

src/map.h
@@ -15,8 +15,6 @@
 #include "types.h"
 #include "u4file.h"
 
-#define MAP_IS_OOB(mapptr, c) (((c).x) < 0 || ((c).x) >= (static_cast<int>((mapptr)->width)) || ((c).y) < 0 || ((c).y) >= (static_cast<int>((mapptr)->height)) || ((c).z) < 0 || ((c).z) >= (static_cast<int>((mapptr)->levels)))
-
 class AnnotationMgr;
 class Object;
 class Creature;
@@ -110,6 +108,8 @@ public:
                     levels;
     uint16_t        chunk_width,
                     chunk_height;
+    uint16_t        boundMaxX,
+                    boundMaxY;
     uint16_t        flags;
     uint16_t        music;
     unsigned int    offset;
@@ -146,5 +146,8 @@ void map_move(Coords&, int dx, int dy, const Map *map = NULL);
 int  map_getRelativeDirection(const Coords& a, const Coords& b, const Map* map = NULL);
 int  map_movementDistance(const Coords& a, const Coords &b, const Map *map = NULL);
 int  map_distance(const Coords& a, const Coords& b, const Map* map = NULL);
+bool map_outOfBounds(const Map* map, const Coords& c);
+
+#define MAP_IS_OOB(M,C) map_outOfBounds(M, C)
 
 #endif

commit a443b237a2460cc5dfde94114e56982c51bda3a8

Fix rendering of dungeon maps (broken in commit 3be196037).

src/maploader.cpp
@@ -323,6 +323,7 @@ static void initDungeonRoom(Dungeon *dng, int room) {
     cmap->id = 0;
     cmap->border_behavior = Map::BORDER_FIXED;
     cmap->width = cmap->height = 11;
+    cmap->boundMaxX = cmap->boundMaxY = 11;
     cmap->music = MUSIC_COMBAT;
     cmap->type = Map::COMBAT;
     cmap->flags |= NO_LINE_OF_SIGHT;
@@ -347,6 +348,9 @@ static bool loadDungeonMap(Map *map, U4FILE *uf, FILE *sav) {
     /* the map must be 11x11 to be read from an .CON file */
     ASSERT(dungeon->width == DNG_WIDTH, "map width is %d, should be %d", dungeon->width, DNG_WIDTH);
     ASSERT(dungeon->height == DNG_HEIGHT, "map height is %d, should be %d", dungeon->height, DNG_HEIGHT);
+
+    map->boundMaxX = map->width;
+    map->boundMaxY = map->height;
 
     /* load the dungeon map */
     bytes = DNG_HEIGHT * DNG_WIDTH * dungeon->levels;