xu4-engine/u4

commit b23d9b7415bff77e419089832a9b8e5ac12c3c16

Replace Observable class with notify system. Add XU4GameServices::notifyBus.
Using a central message bus reduces memory usage and allows listeners to
connect without reference to any sending object.

Party & PartyMember constructors are modified to avoid notification.

The Linux release binary is now 30912 bytes smaller. The following
class byte sizes have been reduced:
 - Aura               40 to 8
 - Menu              120 to 88
 - IntroController  2000 to 1704
 - Location           80 to 48

src/imagemgr.cpp
@@ -30,8 +30,8 @@ ImageMgr::ImageMgr() : vgaColors(NULL), resGroup(0) {
     logger = new Debug("debug/imagemgr.txt", "ImageMgr");
     TRACE(*logger, "creating ImageMgr");
 
-    update(xu4.settings);
-    xu4.settings->addObserver(this);
+    notice(SENDER_SETTINGS, xu4.settings, this);
+    gs_listen(1<<SENDER_SETTINGS, notice, this);
 
     xu4.config->internSymbols(&sym.tiles, 45,
         "tiles charset borders title options_top\n"
@@ -46,8 +46,6 @@ ImageMgr::ImageMgr() : vgaColors(NULL), resGroup(0) {
 }
 
 ImageMgr::~ImageMgr() {
-    xu4.settings->deleteObserver(this);
-
     std::map<Symbol, ImageSet *>::iterator it;
     foreach (it, imageSets)
         delete it->second;
@@ -768,10 +766,13 @@ const RGBA* ImageMgr::vgaPalette() {
 /**
  * Find the new base image set when settings have changed.
  */
-void ImageMgr::update(Settings *newSettings) {
-    string setname = newSettings->videoType;
+void ImageMgr::notice(int sender, void* eventData, void* user) {
+    ImageMgr* mgr = (ImageMgr*) user;
+    (void) sender;
+
+    string setname = ((Settings*) eventData)->videoType;
     TRACE(*logger, string("base image set is '") + setname + string("'"));
-    baseSet = scheme( xu4.config->intern(setname.c_str()) );
+    mgr->baseSet = mgr->scheme( xu4.config->intern(setname.c_str()) );
 }
 
 ImageSet::~ImageSet() {

src/imagemgr.h
@@ -10,7 +10,6 @@
 
 #include "config.h"
 #include "image.h"
-#include "observer.h"
 #include "u4file.h"
 
 #define errorLoadImage(Sym) \
@@ -198,7 +197,7 @@ public:
 /**
  * The image manager singleton that keeps track of all the images.
  */
-class ImageMgr : Observer<Settings *> {
+class ImageMgr {
 public:
     static ImageSymbols sym;
 
@@ -214,6 +213,7 @@ public:
     const RGBA* vgaPalette();
 
 private:
+    static void notice(int, void*, void*);
     const SubImage* getSubImage(Symbol name, ImageInfo** infoPtr);
     ImageInfo* load(ImageInfo* info, bool returnUnscaled);
     U4FILE * getImageFile(ImageInfo *info);
@@ -225,8 +225,6 @@ private:
     void fixupAbacus(Image *im, int prescale);
     void fixupDungNS(Image *im);
     void fixupFMTowns(Image *im);
-
-    void update(Settings *newSettings);
 
     std::map<Symbol, ImageSet *> imageSets;
     ImageSet *baseSet;

project.b
@@ -173,6 +173,8 @@ exe %u4 [
 		%lzw/lzw.c
 		%lzw/u6decode.cpp
 		%lzw/u4decode.cpp
+
+		%support/notify.c
 	]
 ]
 

src/Makefile.common
@@ -7,6 +7,7 @@ CSRCS=\
         anim.c \
         lzw/hash.c \
         lzw/lzw.c \
+        support/notify.c \
         unzip.c \
         $(NULL)
 

src/aura.cpp
@@ -3,26 +3,26 @@
  */
 
 #include "aura.h"
+#include "xu4.h"
+
+#define NOTIFY  gs_emitMessage(SENDER_AURA, this)
 
 Aura::Aura() : type(NONE), duration(0) {}
 
 void Aura::setDuration(int d) {
     duration = d;
-    setChanged();
-    notifyObservers(NULL);
+    NOTIFY;
 }
 
 void Aura::set(Type t, int d) {
     type = t;
     duration = d;
-    setChanged();
-    notifyObservers(NULL);
+    NOTIFY;
 }
 
 void Aura::setType(Type t) {
     type = t;
-    setChanged();
-    notifyObservers(NULL);
+    NOTIFY;
 }
 
 void Aura::passTurn() {
@@ -31,9 +31,7 @@ void Aura::passTurn() {
 
         if (duration == 0) {
             type = NONE;
-
-            setChanged();
-            notifyObservers(NULL);
+            NOTIFY;
         }
     }
 }

src/aura.h
@@ -5,12 +5,10 @@
 #ifndef AURA_H
 #define AURA_H
 
-#include "observable.h"
-
 /**
  * Aura class
  */
-class Aura : public Observable<Aura *> {
+class Aura {
 public:
     enum Type {
         NONE,

src/combat.cpp
@@ -67,14 +67,14 @@ CombatController::CombatController(CombatMap* cmap) : map(cmap) {
     camping = false;
     forceStandardEncounterSize = false;
     showMessage = true;
-    c->party->addObserver(this);
+    listenerId = gs_listen(1<<SENDER_PARTY, combatNotice, this);
 
     if (cmap)
         xu4.game->setMap(cmap, true, NULL, this);
 }
 
 CombatController::~CombatController() {
-    c->party->deleteObserver(this);
+    gs_unplug(listenerId);
 }
 
 /**
@@ -1226,9 +1226,13 @@ void CombatController::attack() {
         returnWeaponToOwner(targetCoords, targetDistance, MASK_DIR(dir), weapon);
 }
 
-void CombatController::update(Party *party, PartyEvent &event) {
-    if (event.type == PartyEvent::PLAYER_KILLED)
-        screenMessage("\n%c%s is Killed!%c\n", FG_RED, event.player->getName().c_str(), FG_WHITE);
+void CombatController::combatNotice(int sender, void* eventData, void* user) {
+    PartyEvent* event = (PartyEvent*) eventData;
+    (void) sender;
+    (void) user;
+    if (event->type == PartyEvent::PLAYER_KILLED)
+        screenMessage("\n%c%s is Killed!%c\n",
+                      FG_RED, event->player->getName().c_str(), FG_WHITE);
 }
 
 /**

src/combat.h
@@ -29,7 +29,7 @@ typedef enum {
 /**
  * CombatController class
  */
-class CombatController : public Controller, public Observer<Party *, PartyEvent &>, public TurnCompleter {
+class CombatController : public Controller, public TurnCompleter {
 public:
     static bool attackHit(const Creature *attacker, const Creature *defender);
     static void engage(MapId mid, const Creature* creatures);
@@ -67,7 +67,6 @@ public:
 
 protected:
     virtual void awardLoot();
-    virtual void update(Party *party, PartyEvent &event);
 
     void initCreature(const Creature *m);
     void fillCreatureTable(const Creature *creature);
@@ -89,8 +88,10 @@ protected:
     bool winOrLose;
     bool showMessage;
     Direction exitDir;
+    int listenerId;
 
 private:
+    static void combatNotice(int, void*, void*);
     CombatController(const CombatController&);
     const CombatController &operator=(const CombatController&);
 

commit 8b5ec9b4bd0815ab86c07e6813f9b114a71cad4d

ImageMgr: Fix crash when video options change (broken in commit b23d9b74).
Settings was sending a notify message to the deleted ImageMgr, so now the
destructor calls notify_unplug().

src/imagemgr.cpp
@@ -21,17 +21,19 @@ using std::string;
 
 Image *screenScale(Image *src, int scale, int n, int filter);
 
-//#define dprint  printf
-
 
 ImageSymbols ImageMgr::sym;
 
 ImageMgr::ImageMgr() : vgaColors(NULL), resGroup(0) {
+#ifdef TRACE_ON
     logger = new Debug("debug/imagemgr.txt", "ImageMgr");
     TRACE(*logger, "creating ImageMgr");
+#else
+    logger = NULL;
+#endif
 
     notice(SENDER_SETTINGS, xu4.settings, this);
-    gs_listen(1<<SENDER_SETTINGS, notice, this);
+    listenerId = gs_listen(1<<SENDER_SETTINGS, notice, this);
 
     xu4.config->internSymbols(&sym.tiles, 45,
         "tiles charset borders title options_top\n"
@@ -46,6 +48,8 @@ ImageMgr::ImageMgr() : vgaColors(NULL), resGroup(0) {
 }
 
 ImageMgr::~ImageMgr() {
+    gs_unplug(listenerId);
+
     std::map<Symbol, ImageSet *>::iterator it;
     foreach (it, imageSets)
         delete it->second;
@@ -537,7 +541,7 @@ ImageInfo* ImageMgr::load(ImageInfo* info, bool returnUnscaled) {
     Image *unscaled = NULL;
     if (file) {
         TRACE(*logger, string("loading image from file '") + info->filename + string("'"));
-        //dprint( "ImageMgr load %d:%s\n", resGroup, info->filename.c_str() );
+        //printf( "ImageMgr load %d:%s\n", resGroup, info->filename.c_str() );
 
         unscaled = loadImage(file, info->filetype, info->width, info->height,
                              info->depth);
@@ -734,7 +738,7 @@ void ImageMgr::freeResourceGroup(uint16_t group) {
         foreach (j, si->second->info) {
             ImageInfo *info = j->second;
             if (info->image && (info->resGroup == group)) {
-                //dprint("ImageMgr::freeRes %s\n", info->filename.c_str());
+                //printf("ImageMgr::freeRes %s\n", info->filename.c_str());
 #ifdef USE_GL
                 if (info->tex) {
                     gpu_freeTexture(info->tex);

src/imagemgr.h
@@ -230,6 +230,7 @@ private:
     ImageSet *baseSet;
     RGBA* vgaColors;
     Debug *logger;
+    int listenerId;
     uint16_t resGroup;
 };
 