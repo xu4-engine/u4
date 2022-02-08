xu4-engine/u4

commit c1ccf0d36c52ccf5b24a74460706763b448b2a08

Created screenWait function to allow animation frames to pass. Eliminated much of the black refresh flashing.

src/game.cpp
@@ -623,6 +623,7 @@ void GameController::finishTurn() {
     Creature *attacker = NULL;    
 
     while (1) {
+
         /* adjust food and moves */
         c->party->endTurn();
 
@@ -632,9 +633,10 @@ void GameController::finishTurn() {
         gameCheckHullIntegrity();
 
         /* update party stats */
-        c->stats->setView(STATS_PARTY_OVERVIEW);
+        //c->stats->setView(STATS_PARTY_OVERVIEW);
 
         screenUpdate(&this->mapArea, true, false);
+        screenWait(1);
 
         /* Creatures cannot spawn, move or attack while the avatar is on the balloon */        
         if (!c->party->isFlying()) {
@@ -668,6 +670,7 @@ void GameController::finishTurn() {
             return;
         } else {            
             screenMessage("Zzzzzz\n");
+            screenWait(4);
         }
     }
 
@@ -694,7 +697,7 @@ void GameController::finishTurn() {
 
     /* draw a prompt */
     screenPrompt();
-    screenRedrawTextArea(TEXT_AREA_X, TEXT_AREA_Y, TEXT_AREA_W, TEXT_AREA_H);
+    //screenRedrawTextArea(TEXT_AREA_X, TEXT_AREA_Y, TEXT_AREA_W, TEXT_AREA_H);
 }
 
 /**
@@ -705,11 +708,9 @@ void GameController::finishTurn() {
 void GameController::flashTile(const Coords &coords, MapTile tile, int frames) {
     c->location->map->annotations->add(coords, tile, true);
 
-    int frameDuration = 1000 / settings.screenAnimationFramesPerSecond + 1; //Screen refresh period.
-    //TODO reduce repetitive recalculation to whenever settings.gameCyclesPerSecond is changed
-
     screenTileUpdate(&game->mapArea, coords);
-   	EventHandler::wait_msecs(frameDuration * frames);
+
+    screenWait(frames);
     c->location->map->annotations->remove(coords, tile);
 
     screenTileUpdate(&game->mapArea, coords, false);

src/object.cpp
@@ -43,6 +43,6 @@ void Object::animateMovement()
 {
 	//TODO abstract movement - also make screen.h and game.h not required
     screenTileUpdate(&game->mapArea, prevCoords, false);
-    if (screenTileUpdate(&game->mapArea, coords, true))
-    	EventHandler::wait_msecs(11);
+    if (screenTileUpdate(&game->mapArea, coords, false))
+    	screenWait(1);
 }

src/screen.cpp
@@ -421,9 +421,9 @@ bool screenTileUpdate(TileView *view, const Coords &coords, bool redraw)
 
 		if (redraw)
 		{
-			screenRedrawMapArea();
-			return true;
+			//screenRedrawMapArea();
 		}
+		return true;
 	}
 	return false;
 }
@@ -1331,4 +1331,5 @@ void screenGemUpdate() {
 //Unsure if implementation required in iOS.
 void inline screenLock(){};
 void inline screenUnlock(){};
+void inline screenWait(int numberOfAnimationFrames){};
 #endif

src/screen.h
@@ -86,6 +86,7 @@ void screenDelete(void);
 void screenReInit(void);
 void screenLock();
 void screenUnlock();
+void screenWait(int numberOfAnimationFrames);
 
 void screenIconify(void);
 

src/screen_sdl.cpp
@@ -206,6 +206,7 @@ int screenLoadImageCga(Image **image, int width, int height, U4FILE *file, Compr
  */
 
 SDL_mutex *screenLockMutex = NULL;
+int frameDuration = 0;
 
 void screenLock() {
 	SDL_mutexP(screenLockMutex);
@@ -227,13 +228,14 @@ void screenRedrawTextArea(int x, int y, int width, int height) {
 	screenUnlock();
 }
 
+void screenWait(int numberOfAnimationFrames) {
+	SDL_Delay(numberOfAnimationFrames * frameDuration);
+}
 
 bool continueScreenRefresh = true;
 SDL_Thread *screenRefreshThread = NULL;
 
 int screenRefreshThreadFunction(void *unused) {
-
-	int frameDuration = 1000 / settings.screenAnimationFramesPerSecond;
 
 	while (continueScreenRefresh) {
 		SDL_Delay(frameDuration);
@@ -243,6 +245,8 @@ int screenRefreshThreadFunction(void *unused) {
 
 void screenRefreshThreadInit() {
 	screenLockMutex = SDL_CreateMutex();;
+
+	frameDuration = 1000 / settings.screenAnimationFramesPerSecond;
 
 	continueScreenRefresh = true;
 	if (screenRefreshThread) {

commit 4003a82d035d4169dcdd1a861bc91d865d482b88

game.cpp: Restore party overview after using an item.
This may have been broken in commit c1ccf0d3 when setView() was removed
from finishTurn().

src/game.cpp
@@ -1207,6 +1207,8 @@ bool GameController::keyPressed(int key) {
             U4IOS::IOSConversationHelper::setIntroString("Use which item?");
 #endif
             itemUse(gameGetInput().c_str());
+            if (settings.enhancements)
+                c->stats->setView(STATS_PARTY_OVERVIEW);
             break;
         }
 