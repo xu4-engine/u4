xu4-engine/u4

commit ef07dd455beebdd6081dd870107e5f53beba4be8

Designate original game files with "u4/" path ("u4u/" for the VGA upgrade).
Eliminate the xu4Graphic flag.

src/imagemgr.cpp
@@ -3,6 +3,7 @@
  */
 
 #include <vector>
+#include <string.h>
 
 #include "config.h"
 #include "debug.h"
@@ -354,36 +355,39 @@ ImageInfo *ImageMgr::getInfoFromSet(const string &name, ImageSet *imageset) {
 
 U4FILE * ImageMgr::getImageFile(ImageInfo *info)
 {
+    U4FILE *file;
     string filename = info->filename;
+    const char* fn = filename.c_str();
 
-    /*
-     * If the u4 VGA upgrade is installed (i.e. setup has been run and
-     * the u4dos files have been renamed), we need to use VGA names
-     * for EGA and vice versa, but *only* when the upgrade file has a
-     * .old extention.  The charset and tiles have a .vga extention
-     * and are not renamed in the upgrade installation process
-     */
-    if (u4isUpgradeInstalled()) {
-        string& vgaFile = getInfoFromSet(info->name, scheme("VGA"))->filename;
-        if (vgaFile.find(".old") != string::npos) {
-            if (xu4.settings->videoType == "EGA")
-                filename = vgaFile;
-            else
-                filename = getInfoFromSet(info->name, scheme("EGA"))->filename;
+    if (strncmp(fn, "u4/", 3) == 0 ||
+        strncmp(fn, "u4u/", 4) == 0) {
+        // Original game data - strip off path.
+        string basename = fn + ((fn[2] == '/') ? 3 : 4);
+
+        /*
+         * If the u4 VGA upgrade is installed (i.e. setup has been run and
+         * the u4dos files have been renamed), we need to use VGA names
+         * for EGA and vice versa, but *only* when the upgrade file has a
+         * .old extention.  The charset and tiles have a .vga extention
+         * and are not renamed in the upgrade installation process
+         */
+        if (u4isUpgradeInstalled()) {
+            string& vgaFile = getInfoFromSet(info->name, scheme("VGA"))->filename;
+            if (vgaFile.find(".old") != string::npos) {
+                if (xu4.settings->videoType == "EGA")
+                    basename = vgaFile;
+                else
+                    basename = getInfoFromSet(info->name, scheme("EGA"))->filename;
+            }
         }
-    }
 
-    if (filename == "")
-        return NULL;
-
-    U4FILE *file = NULL;
-    if (info->xu4Graphic) {
+        file = u4fopen(basename);
+    } else {
         string pathname(u4find_graphics(filename));
-        if (!pathname.empty())
+        if (pathname.empty())
+            file = NULL;
+        else
             file = u4fopen_stdio(pathname);
-    }
-    else {
-        file = u4fopen(filename);
     }
     return file;
 }

conf/dtd/graphics.dtd
@@ -23,7 +23,6 @@
                 tiles             NMTOKEN  #IMPLIED
                 introOnly         %bool;   #IMPLIED
                 transparentIndex  NMTOKEN  #IMPLIED
-                xu4Graphic        %bool;   #IMPLIED
                 fixup             %fixup;  #IMPLIED>
 
 <!ELEMENT subimage EMPTY >

src/config.cpp
@@ -480,8 +480,6 @@ static ImageInfo* loadImageInfo(const ConfigElement &conf) {
     info->tiles = conf.getInt("tiles");
     info->introOnly = conf.getBool("introOnly");
     info->transparentIndex = conf.getInt("transparentIndex", -1);
-
-    info->xu4Graphic = conf.getBool("xu4Graphic");
     info->fixup = static_cast<ImageFixup>(conf.getEnum("fixup", fixupEnumStrings));
     info->image = NULL;
 

src/imagemgr.h
@@ -91,7 +91,6 @@ public:
     uint8_t prescale;
     uint8_t filetype;
     bool introOnly;             /**< whether can be freed after the intro */
-    bool xu4Graphic;            /**< an original xu4 graphic not part of u4dos or the VGA upgrade */
     uint8_t transparentIndex;   /**< color index to consider transparent */
     uint8_t fixup;              /**< a routine to do miscellaneous fixes to the image */
     Image *image;               /**< the image we're describing */
@@ -121,9 +120,9 @@ public:
     ImageInfo *get(const std::string &name, bool returnUnscaled=false);
     SubImage *getSubImage(const std::string &name);
     void freeIntroBackgrounds();
-    U4FILE * getImageFile(ImageInfo *info);
 
 private:
+    U4FILE * getImageFile(ImageInfo *info);
     ImageSet* scheme(const std::string &setname);
     ImageInfo* getInfoFromSet(const std::string &name, ImageSet *set);
 

commit 9ce4699783f708911a4a7e2124b8cb638612c714

imagemgr.cpp: Allow u4upgrad.zip to be used with unzipped ultima4/ data.
Broken in commit ef07dd45.

src/imagemgr.cpp
@@ -394,6 +394,11 @@ U4FILE * ImageMgr::getImageFile(ImageInfo *info)
                     basename = vgaFile;
                 else
                     basename = getInfoFromSet(info->name, scheme(sname[1]))->getFilename();
+
+                // Remove u4/ or u4u/ path again.
+                size_t pos = basename.find('/');
+                if (pos != string::npos)
+                    basename.erase(0,pos+1);
             }
         }
 