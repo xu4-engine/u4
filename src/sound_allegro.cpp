xu4-engine/u4

commit 18533cc74de65598e8b5c465899190d1cf31a368

Load core render assets from render.pak.  Add module.* files.

src/sound_allegro.cpp
@@ -196,7 +196,7 @@ static bool sound_load(Sound sound) {
         const CDIEntry* ent = config_soundFile(sound);
         if (ent) {
             ALLEGRO_FILE* slice;
-            ALLEGRO_FILE* af = al_fopen(xu4.config->modulePath(), "rb");
+            ALLEGRO_FILE* af = al_fopen(xu4.config->modulePath(ent), "rb");
             if (af) {
                 al_fseek(af, ent->offset, ALLEGRO_SEEK_SET);
                 slice = al_fopen_slice(af, ent->bytes, "r");
@@ -301,7 +301,7 @@ static bool music_load(int music, float newGain) {
     const CDIEntry* ent = config_musicFile(music);
     if (ent) {
         if (! moduleFile)
-            moduleFile = al_fopen(xu4.config->modulePath(), "rb");
+            moduleFile = al_fopen(xu4.config->modulePath(ent), "rb");
 
         if (moduleFile) {
             ALLEGRO_FILE* slice;

Makefile
@@ -7,12 +7,15 @@ else
 MFILE_OS=Makefile
 endif
 
-all: src/xu4
+all: src/xu4 render.pak Ultima-IV.mod
 
 src/xu4:
 	make -C src -f $(MFILE_OS)
 
-Ultima-IV.mod: module/Ultima-IV/*.b module/Ultima-IV/shader/*.glsl
+render.pak: module/render/shader/*.glsl
+	boron -s tools/pack-xu4.b -f module/render -o $@
+
+Ultima-IV.mod: module/Ultima-IV/*.b
 	boron -s tools/pack-xu4.b -o $@
 
 .PHONY: clean download snapshot
@@ -29,7 +32,7 @@ ultima4.zip:
 u4upgrad.zip:
 	curl -sSL -o $@ http://sourceforge.net/projects/xu4/files/Ultima%204%20VGA%20Upgrade/1.3/u4upgrad.zip
 
-snapshot: Ultima-IV.mod
+snapshot: render.pak Ultima-IV.mod
 	@rm -f project.tar.gz
 	copr -c
 	tools/cbuild windows

commit 5b691739aa417fefffc29e79e1a665465fb6d9a6

sound_allegro: Fix audioExt() to handle CDI changed by module layering.
Broken in commit 18533cc74.

src/sound_allegro.cpp
@@ -178,12 +178,14 @@ void soundDelete(void)
 
 #ifdef CONF_MODULE
 static const char* audioExt(const CDIEntry* entry) {
-    switch (entry->cdi) {
-        case DA7A_AUDIO_WAVE:
+    // NOTE: Using CDI_MASK_FORMAT since mod_addLayer() changes the 0xDA byte.
+
+    switch (entry->cdi & CDI_MASK_FORMAT) {
+        case DA7A_AUDIO_WAVE & CDI_MASK_FORMAT:
             return ".wav";
-        case DA7A_AUDIO_MP3:
+        case DA7A_AUDIO_MP3 & CDI_MASK_FORMAT:
             return ".mp3";
-        case DA7A_AUDIO_OGG_VORBIS:
+        case DA7A_AUDIO_OGG_VORBIS & CDI_MASK_FORMAT:
             return ".ogg";
     }
     return NULL;