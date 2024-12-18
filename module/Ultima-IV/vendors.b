xu4-engine/u4

commit e67f45d0d27c60db2097d295ead830e3bdad1581

Fix karma adjustments from buying reagents to match DOS version.
KA_DIDNT_CHEAT_REAGENTS was time limited in commit db5db7cf for some
unknown reason.

Add Party::adjustVirtues() and change 'karma function to allow scripts to
control the amount of adjustment.  The calculations are from
u4-decompiled/SRC/U4_SHOPS.C SHP_reagent().

module/Ultima-IV/vendors.b
@@ -39,14 +39,6 @@ enum 9 [
     stats-items     stats-reagents  stats-mixtures
 ]
 
-; Matches KarmaAction in player.h
-enum 15 [
-    gave_blood
-    didnt_give_blood
-    cheated_merchant
-    honest_to_merchant
-]
-
 ; Shared vendor variables.
 shop: owner: price: quant: none
 shop-vars: ['@' shop '%' owner '$' price '#' quant]
@@ -942,9 +934,9 @@ reagents: context [
                 vo 4
                 >> "^/Very good. "
                 add-items type quant
-                karma either lt? paying price
-                    cheated_merchant
-                    honest_to_merchant
+                karma [honesty justice honor] either lt? paying price
+                    [either lt? d: sub price paying 12 -4 [div d -3]]
+                    2
             ][
                 vo 5
                 >> "^/It seems you have not the gold! "
@@ -1017,13 +1009,13 @@ healer: context [
             input-choice [
                 'y' [
                     damage-pc 1 100
-                    karma gave_blood
+                    karma sacrifice 5
                     >> "^/Thou art a great help. We are in dire need!^/"
                     game-wait 1000
                     adieu
                 ]
                 'n' [
-                    karma didnt_give_blood
+                    karma sacrifice -5
                     adieu
                 ]
                 end

src/party.cpp
@@ -516,138 +516,149 @@ void Party::adjustGold(int gold) {
     notifyOfChange();
 }
 
+/*
+ * Check if enough time has passed since last virtue award if a karma
+ * action is time limited.
+ */
+bool Party::virtueIncreaseTimeout() {
+    if (((saveGame->moves / 16) >= 0x10000) ||
+        (((saveGame->moves / 16) & 0xFFFF) != saveGame->lastvirtue)) {
+        saveGame->lastvirtue = (saveGame->moves / 16) & 0xFFFF;
+        return true;
+    }
+    return false;
+}
+
 /**
  * Adjusts the avatar's karma level for the given action.  Notify
- * observers with a lost eighth event if the player has lost
- * avatarhood.
+ * observers with a lost eighth event if the player has lost avatarhood.
  */
 void Party::adjustKarma(KarmaAction action) {
-    int timeLimited = 0;
-    int v, newKarma[VIRT_MAX], maxVal[VIRT_MAX];
-
-    /*
-     * make a local copy of all virtues, and adjust it according to
-     * the game rules
-     */
-    for (v = 0; v < VIRT_MAX; v++) {
-        newKarma[v] = saveGame->karma[v] == 0 ? 100 : saveGame->karma[v];
-        maxVal[v] = saveGame->karma[v] == 0 ? 100 : 99;
-    }
+#define VMASK_1(a)      (1<<a)
+#define VMASK_2(a,b)    ((1<<a) | (1<<b))
+#define VMASK_3(a,b,c)  ((1<<a) | (1<<b) | (1<<c))
 
     switch (action) {
     case KA_FOUND_ITEM:
-        AdjustValueMax(newKarma[VIRT_HONOR], 5, maxVal[VIRT_HONOR]);
+        adjustVirtues(VMASK_1(VIRT_HONOR), 5);
         break;
     case KA_STOLE_CHEST:
-        AdjustValueMin(newKarma[VIRT_HONESTY], -1, 1);
-        AdjustValueMin(newKarma[VIRT_JUSTICE], -1, 1);
-        AdjustValueMin(newKarma[VIRT_HONOR], -1, 1);
+        adjustVirtues(VMASK_3(VIRT_HONESTY, VIRT_JUSTICE, VIRT_HONOR), -1);
         break;
     case KA_GAVE_ALL_TO_BEGGAR:
         //  When donating all, you get +3 HONOR in Apple 2, but not in in U4DOS.
         //  TODO: Make this a configuration option.
-        //  AdjustValueMax(newKarma[VIRT_HONOR], 3, maxVal[VIRT_HONOR]);
+        //  adjustVirtues(VMASK_1(VIRT_HONOR), 3);
     case KA_GAVE_TO_BEGGAR:
         //  In U4DOS, we only get +2 COMPASSION, no HONOR or SACRIFICE even if
         //  donating all.
-        timeLimited = 1;
-        AdjustValueMax(newKarma[VIRT_COMPASSION], 2, maxVal[VIRT_COMPASSION]);
+        if (virtueIncreaseTimeout())
+            adjustVirtues(VMASK_1(VIRT_COMPASSION), 2);
         break;
     case KA_BRAGGED:
-        AdjustValueMin(newKarma[VIRT_HUMILITY], -5, 1);
+        adjustVirtues(VMASK_1(VIRT_HUMILITY), -5);
         break;
     case KA_HUMBLE:
-        timeLimited = 1;
-        AdjustValueMax(newKarma[VIRT_HUMILITY], 10, maxVal[VIRT_HUMILITY]);
+        if (virtueIncreaseTimeout())
+            adjustVirtues(VMASK_1(VIRT_HUMILITY), 10);
         break;
     case KA_HAWKWIND:
     case KA_MEDITATION:
-        timeLimited = 1;
-        AdjustValueMax(newKarma[VIRT_SPIRITUALITY], 3, maxVal[VIRT_SPIRITUALITY]);
+        if (virtueIncreaseTimeout())
+            adjustVirtues(VMASK_1(VIRT_SPIRITUALITY), 3);
         break;
     case KA_BAD_MANTRA:
-        AdjustValueMin(newKarma[VIRT_SPIRITUALITY], -3, 1);
+        adjustVirtues(VMASK_1(VIRT_SPIRITUALITY), -3);
         break;
     case KA_ATTACKED_GOOD:
-        AdjustValueMin(newKarma[VIRT_COMPASSION], -5, 1);
-        AdjustValueMin(newKarma[VIRT_JUSTICE], -5, 1);
-        AdjustValueMin(newKarma[VIRT_HONOR], -5, 1);
+        adjustVirtues(VMASK_3(VIRT_COMPASSION, VIRT_JUSTICE, VIRT_HONOR), -5);
         break;
     case KA_FLED_EVIL:
-        AdjustValueMin(newKarma[VIRT_VALOR], -2, 1);
+        adjustVirtues(VMASK_1(VIRT_VALOR), -2);
         break;
     case KA_HEALTHY_FLED_EVIL:
-        AdjustValueMin(newKarma[VIRT_VALOR], -2, 1);
-        AdjustValueMin(newKarma[VIRT_SACRIFICE], -2, 1);
+        adjustVirtues(VMASK_2(VIRT_VALOR, VIRT_SACRIFICE), -2);
         break;
     case KA_KILLED_EVIL:
-        AdjustValueMax(newKarma[VIRT_VALOR], xu4_random(2), maxVal[VIRT_VALOR]); /* gain one valor half the time, zero the rest */
+         /* gain one valor half the time, zero the rest */
+        if (xu4_random(2))
+            adjustVirtues(VMASK_1(VIRT_VALOR), 1);
         break;
     case KA_FLED_GOOD:
-        AdjustValueMax(newKarma[VIRT_COMPASSION], 2, maxVal[VIRT_COMPASSION]);
-        AdjustValueMax(newKarma[VIRT_JUSTICE], 2, maxVal[VIRT_JUSTICE]);
+        adjustVirtues(VMASK_2(VIRT_COMPASSION, VIRT_JUSTICE), 2);
         break;
     case KA_SPARED_GOOD:
-        AdjustValueMax(newKarma[VIRT_COMPASSION], 1, maxVal[VIRT_COMPASSION]);
-        AdjustValueMax(newKarma[VIRT_JUSTICE], 1, maxVal[VIRT_JUSTICE]);
+        adjustVirtues(VMASK_2(VIRT_COMPASSION, VIRT_JUSTICE), 1);
         break;
+#if 0
+    // These are handled by the vendor script.
     case KA_DONATED_BLOOD:
-        AdjustValueMax(newKarma[VIRT_SACRIFICE], 5, maxVal[VIRT_SACRIFICE]);
+        adjustVirtues(VMASK_1(VIRT_SACRIFICE), 5);
         break;
     case KA_DIDNT_DONATE_BLOOD:
-        AdjustValueMin(newKarma[VIRT_SACRIFICE], -5, 1);
+        adjustVirtues(VMASK_1(VIRT_SACRIFICE), -5);
         break;
     case KA_CHEAT_REAGENTS:
-        AdjustValueMin(newKarma[VIRT_HONESTY], -10, 1);
-        AdjustValueMin(newKarma[VIRT_JUSTICE], -10, 1);
-        AdjustValueMin(newKarma[VIRT_HONOR], -10, 1);
+        // This doesn't match the penalty in U4DOS, which is based on price.
+        adjustVirtues(VMASK_3(VIRT_HONESTY, VIRT_JUSTICE, VIRT_HONOR), -10);
         break;
     case KA_DIDNT_CHEAT_REAGENTS:
-        timeLimited = 1;
-        AdjustValueMax(newKarma[VIRT_HONESTY], 2, maxVal[VIRT_HONESTY]);
-        AdjustValueMax(newKarma[VIRT_JUSTICE], 2, maxVal[VIRT_JUSTICE]);
-        AdjustValueMax(newKarma[VIRT_HONOR], 2, maxVal[VIRT_HONOR]);
+        adjustVirtues(VMASK_3(VIRT_HONESTY, VIRT_JUSTICE, VIRT_HONOR), 2);
         break;
+#endif
     case KA_USED_SKULL:
         /* using the skull is very, very bad... */
-        for (v = 0; v < VIRT_MAX; v++)
-            AdjustValueMin(newKarma[v], -5, 1);
+        adjustVirtues(0xff, -5);
         break;
     case KA_DESTROYED_SKULL:
         /* ...but destroying it is very, very good */
-        for (v = 0; v < VIRT_MAX; v++)
-            AdjustValueMax(newKarma[v], 10, maxVal[v]);
+        adjustVirtues(0xff, 10);
+        break;
+    default:
         break;
     }
+}
 
-    /*
-     * check if enough time has passed since last virtue award if
-     * action is time limited -- if not, throw away new values
-     */
-    if (timeLimited) {
-        if (((saveGame->moves / 16) >= 0x10000) || (((saveGame->moves / 16) & 0xFFFF) != saveGame->lastvirtue))
-            saveGame->lastvirtue = (saveGame->moves / 16) & 0xFFFF;
-        else
-            return;
+/**
+ * Adjusts the avatar's virtue levels by a fixed amount.
+ * Notify observers with a lost eighth event if the player has lost avatarhood.
+ */
+void Party::adjustVirtues(uint32_t virtueMask, int adjust) {
+    const int enlightened = 0;  // DOS save game enlightenment value.
+    int v, level;
+    int modified = 0;
+
+    for (v = 0; v < VIRT_MAX && virtueMask; v++) {
+        if (virtueMask & 1) {
+            level = saveGame->karma[v];
+            if (adjust > 0) {
+                if (level != enlightened) {
+                    level += adjust;
+                    if (level > 99)
+                        level = 99;
+                    saveGame->karma[v] = level;
+                    ++modified;
+                }
+            } else {
+                if (level == enlightened) {
+                    level = 99;
+                    PartyEvent event(PartyEvent::LOST_EIGHTH, 0);
+                    gs_emitMessage(SENDER_PARTY, &event);
+                }
+                level += adjust;
+                if (level < 1)
+                    level = 1;
+                saveGame->karma[v] = level;
+                ++modified;
+            }
+            //printf("AV %d %d:%d\n", saveGame->moves, v, saveGame->karma[v]);
+        }
+        virtueMask >>= 1;
     }
 
     /* something changed */
-    notifyOfChange();
-
-    /*
-     * return to u4dos compatibility and handle losing of eighths
-     */
-    for (v = 0; v < VIRT_MAX; v++) {
-        if (maxVal[v] == 100) { /* already an avatar */
-            if (newKarma[v] < 100) { /* but lost it */
-                saveGame->karma[v] = newKarma[v];
-                PartyEvent event(PartyEvent::LOST_EIGHTH, 0);
-                gs_emitMessage(SENDER_PARTY, &event);
-            }
-            else saveGame->karma[v] = 0; /* return to u4dos compatibility */
-        }
-        else saveGame->karma[v] = newKarma[v];
-    }
+    if (modified)
+        notifyOfChange();
 }
 
 /**

src/party.h
@@ -169,6 +169,7 @@ public:
     void adjustFood(int food);
     void adjustGold(int gold);
     void adjustKarma(KarmaAction action);
+    void adjustVirtues(uint32_t virtues, int adjust);
     void applyEffect(int player, Map*, TileEffect effect);
     bool attemptElevation(Virtue virtue);
     bool burnTorch(int turns = 1);
@@ -210,6 +211,7 @@ public:
     int memberIndex(Creature* person) const;
 
 private:
+    bool virtueIncreaseTimeout();
     void initTransport(const MapTile& tile);
     void syncMembers();
     PartyMemberVector members;

src/script_boron.cpp
@@ -15,7 +15,8 @@ enum ItemClass {
     IC_ARMOR,
     IC_WEAPON,
     IC_REAGENT,
-    IC_PARTY
+    IC_PARTY,
+    IC_VIRTUE
 };
 
 enum ItemMiscId {
@@ -182,15 +183,42 @@ CFUNC(cf_damagePc)
 
 /*-cf-
     karma
-        action  int!
+        virtue  word!/block!
+        amount  int!
     return: unset!
 
-    Adjust avatar's karma level for an action.
+    Adjust avatar's karma levels.
 */
 CFUNC(cf_karma)
 {
-    (void) ut;
-    c->party->adjustKarma((KarmaAction) ur_int(a1));
+    UBlockIt bi;
+    uint32_t virtues = 0;
+    int adj = ur_int(a1+1);
+
+    if (! adj)
+        goto done;
+
+    if (ur_is(a1, UT_BLOCK)) {
+        ur_blockIt(ut, &bi, a1);
+    } else {
+        bi.it  = a1;
+        bi.end = a1 + 1;
+    }
+
+    // Convert virtue names to bitmask.
+    ur_foreach(bi) {
+        if (ur_is(bi.it, UT_WORD)) {
+            int id = xu4.config->scriptItemId(ur_atom(bi.it));
+            int ic = SCRIPT_ITEM_CLASS(id);
+            if (ic != IC_VIRTUE)
+                goto done;
+            virtues |= 1 << SCRIPT_ITEM_INDEX(id);
+        }
+    }
+
+    c->party->adjustVirtues(virtues, adj);
+
+done:
     ur_setId(res, UT_UNSET);
     return UR_OK;
 }
@@ -461,7 +489,7 @@ CFUNC(cf_inputPlayer)
         'attr        word!
     return: Specified value.
 
-    Get a party attribute (food, gold, keys, members, or transport).
+    Get a party attribute (food, gold, keys, members, transport, or virtue).
 */
 CFUNC(cf_party)
 {
@@ -525,6 +553,8 @@ CFUNC(cf_party)
                 return ur_error(ut, UR_ERR_SCRIPT,
                                 "Invalid party attribute '%s", ur_wordCStr(a1));
         }
+    } else if (ic == IC_VIRTUE) {
+        n = c->saveGame->karma[id];
     }
 
     ur_setId(res, UT_INT);
@@ -842,7 +872,7 @@ static const char pfFuncSpecs[] =
     "vo n\n"
     "relocate a coord!\n"
     "damage-pc n int! hp int!\n"
-    "karma n int!\n"
+    "karma a word!/block! n int!\n"
     "inn-sleep\n"
     "cursor n logic!\n"
     "view-stats n int!\n"
@@ -898,9 +928,10 @@ static UIndex script_init(UThread* ut, const UCell* blkC)
     }
 
     // Create a context which will map item names to internal script ids.
-    // This simplifies the implementation of party, add-items & remove-items.
+    // This simplifies the implementation of karma, party, add-items &
+    // remove-items.
 
-    const int icount = 38;
+    const int icount = 46;
     UAtom atoms[icount];
     UIndex itemIdN = ur_makeContext(ut, icount);
     ur_hold(itemIdN);   // Hold forever.
@@ -918,7 +949,10 @@ static UIndex script_init(UThread* ut, const UCell* blkC)
         "ash ginseng garlic silk moss\n"
         "pearl nightshade mandrake\n"
 
-        "members transport\n",
+        "members transport\n"
+
+        "honesty compassion valor justice sacrifice honor\n"
+        "spirituality humility\n",
         atoms);
 
     UBuffer* ctx = ur_buffer(itemIdN);
@@ -927,6 +961,7 @@ static UIndex script_init(UThread* ut, const UCell* blkC)
     enumItems(ctx, atoms+13, IC_WEAPON,  1, 15);
     enumItems(ctx, atoms+28, IC_REAGENT, 0,  8);
     enumItems(ctx, atoms+36, IC_PARTY,   0,  2);
+    enumItems(ctx, atoms+38, IC_VIRTUE,  0,  8);
     ur_ctxSort(ctx);
     assert(ctx->used == icount);
 

commit 8e4833d1f2d96b5ceeeae70d17b83062ef2377f0

vendors.b: Fix karma word! argument (broken in commit e67f45d0).

module/Ultima-IV/vendors.b
@@ -1009,13 +1009,13 @@ healer: context [
             input-choice [
                 'y' [
                     damage-pc 1 100
-                    karma sacrifice 5
+                    karma 'sacrifice 5
                     >> "^/Thou art a great help. We are in dire need!^/"
                     game-wait 1000
                     adieu
                 ]
                 'n' [
-                    karma sacrifice -5
+                    karma 'sacrifice -5
                     adieu
                 ]
                 end